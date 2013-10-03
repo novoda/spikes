3cf2ec37c3e11ebcd3f2a1cb013c43de9c170345
PaulThomas <paul.thomas@novoda.com>
2013-10-03 19:43:23 +0100

    Modified after Frankie's suggestions.
    Also put everything in a proper subdirectory.

--- ListRefreshSpike2Project/ListRefreshSpike2/src/main/java/com/novoda/spike/listrefreshspike2/MainActivity.java
+++ ListRefreshSpike2Project/ListRefreshSpike2/src/main/java/com/novoda/spike/listrefreshspike2/MainActivity.java
@@ -5,35 +5,28 @@ import android.app.FragmentManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.widget.ListView;
 
 public class MainActivity extends Activity implements Handler.Callback {
 
     public static final int WHAT = 99;
     private Handler handler;
     private SimpleListFragment slf1;
-    private SimpleListFragment slf2;
-    private ListView listView1;
-    private ListView listView2;
     private boolean noMoreBgUpdates;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        noMoreBgUpdates = false;
         handler = new Handler(this);
         handler.sendEmptyMessageDelayed(MainActivity.WHAT, 400L);
         setContentView(R.layout.main);
         FragmentManager fragmentManager = getFragmentManager();
         slf1 = (SimpleListFragment) fragmentManager.findFragmentById(R.id.simple_list_fragment1);
-        slf2 = (SimpleListFragment) fragmentManager.findFragmentById(R.id.simple_list_fragment2);
-        listView1 = (ListView) slf1.getView();
-        listView2 = (ListView) slf2.getView();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        noMoreBgUpdates = false;
     }
 
     @Override
@@ -46,7 +39,7 @@ public class MainActivity extends Activity implements Handler.Callback {
     @Override
     public boolean handleMessage(Message msg) {
         if (msg.what == WHAT && !noMoreBgUpdates) {
-            new DataUpdateTask(getApplicationContext(), slf1, handler).execute((Void) null);
+            new DataUpdateTask(getApplicationContext(), slf1, handler).execute();
             return true;
         }
         return false;
