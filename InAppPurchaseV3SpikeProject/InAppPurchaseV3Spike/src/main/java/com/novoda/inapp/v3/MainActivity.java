798b423cf12fd1292f401ce0e702c5cf3c50b710
Blundell <blundell.app.dev@gmail.com>
2013-11-06 12:02:25 +0000

    Pulled fragment up n out

--- InAppPurchaseV3SpikeProject/InAppPurchaseV3Spike/src/main/java/com/novoda/inapp/v3/MainActivity.java
+++ InAppPurchaseV3SpikeProject/InAppPurchaseV3Spike/src/main/java/com/novoda/inapp/v3/MainActivity.java
@@ -1,15 +1,7 @@
 package com.novoda.inapp.v3;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.support.v7.app.ActionBarActivity;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -22,43 +14,6 @@ public class MainActivity extends ActionBarActivity {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
-        }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
         }
     }
 
---
+++ InAppPurchaseV3SpikeProject/InAppPurchaseV3Spike/src/main/java/com/novoda/inapp/v3/PlaceholderFragment.java
@@ -0,0 +1,16 @@
+package com.novoda.inapp.v3;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PlaceholderFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
