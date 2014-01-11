1170118484133df065f58f1b360e9953fac7bd78
Volker Leck <volker.leck@gmail.com>
2014-01-11 22:38:42 +0100

    sqliteAnalyzer: add groovy task for code generation

---
+++ sqliteAnalyzer/src/main/groovy/tv/arte/plus7/sql/GenerateDatabaseInfo.groovy
@@ -0,0 +1,49 @@
+package tv.arte.plus7.sql
+
+import com.novoda.sqlite.Analyzer
+import com.novoda.sqlite.ColumnsPrinter
+import com.novoda.sqlite.Generator
+import com.novoda.sqlite.MigrationsInDir
+import com.novoda.sqlite.Migrator
+import com.novoda.sqlite.Printer
+import com.novoda.sqlite.TablesPrinter
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.TaskAction
+
+/**
+ * This task runs the SqliteAnalyzer to generate code to describe the database tables and columns.
+ *
+ * We use input and output directory annotations to ensure the task is run on any changes in the migrations directory
+ * or when the output is removed.
+ */
+class GenerateDatabaseInfo extends DefaultTask {
+
+    @InputDirectory
+    File migrationsDir
+
+    @OutputDirectory
+    File outputDir
+
+    String packageName = "com.novoda.database"
+
+    private File makeFileDir() {
+        String packageAsDir = packageName.replaceAll(~/\./, "/")
+        def fileDir = new File(outputDir, packageAsDir)
+        fileDir.mkdirs()
+        return fileDir
+    }
+
+    @TaskAction
+    void generate() {
+        def arteMigrations = new MigrationsInDir(migrationsDir)
+        def connection = new Migrator(arteMigrations).runMigrations()
+        def database = new Analyzer(connection).analyze()
+        connection.close()
+        Printer[] printers = [new ColumnsPrinter(database), new TablesPrinter(database)]
+        def codeGenerator = new Generator(makeFileDir(), packageName, printers)
+        codeGenerator.print()
+    }
+}
+
