cb83c649866f5947d0c8c5f00e6976f0a1497edb
Blundell <blundell.app.dev@gmail.com>
2014-06-20 12:45:31 +0100

    Documented

--- LeanPlumLeakSpike/app/src/main/java/blundell/leanplumleakspike/MyActivity.java
+++ LeanPlumLeakSpike/app/src/main/java/blundell/leanplumleakspike/MyActivity.java
@@ -14,12 +14,21 @@ public class MyActivity extends LeanplumActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Show a loading view
         setContentView(R.layout.activity_loading);
+        // Setup leanplum
         Leanplum.setAppIdForDevelopmentMode(APP_ID, DEV_KEY);
+        // Ask lean plum to go fetch the latest AB variables
         Leanplum.start(this);
+        // Add a response handler to be told when the AB variable has been updated for the first time
+        // When they are updated consequent times we won't get a callback
+        // and if they variable was updated before we set this callback we get called back instantly
         Leanplum.addStartResponseHandler(startCallback);
     }
 
+    // Passing this annonymous class as reference leaks our outer class (Activity) into Leanplum
+    // Leanplum seems to be holding a list of these and therefore all Activities cannot be
+    // cleared by the garbage collector MEMORY LEAK
     private final StartCallback startCallback = new StartCallback() {
         @Override
         public void onResponse(boolean b) {
