9e05a07b9f476111305098a8e3a92f251e83fb0a
Adam Brown <adam@novoda.com>
2014-06-27 19:19:07 +0100

    initial commit, needs a git ignore & love

---
+++ SwipeableView/src/main/java/com/ouchadam/swipeableview/MainActivity.java
@@ -0,0 +1,6 @@
+package com.ouchadam.swipeableview;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
---
+++ SwipeableView/src/main/java/com/ouchadam/swipeableview/SwipeableView.java
@@ -0,0 +1,20 @@
+package com.ouchadam.swipeableview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+public class SwipeableView extends RelativeLayout {
+    public SwipeableView(Context context) {
+        super(context);
+    }
+
+    public SwipeableView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SwipeableView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
