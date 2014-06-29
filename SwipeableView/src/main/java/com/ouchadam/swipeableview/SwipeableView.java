ef445c79913e4a3998d8a4c8c5aee8f4799a3a4b
Adam Brown <adam@novoda.com>
2014-06-29 14:02:10 +0100

    adding more the child layouts and creating a working solution

--- SwipeableView/src/main/java/com/ouchadam/swipeableview/SwipeableView.java
+++ SwipeableView/src/main/java/com/ouchadam/swipeableview/SwipeableView.java
@@ -3,20 +3,17 @@ package com.ouchadam.swipeableview;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-public class SwipeableView extends RelativeLayout implements View.OnClickListener {
+public class SwipeableView extends RelativeLayout implements View.OnClickListener, ValueAnimator.AnimatorUpdateListener {
 
-    private ViewGroup contentParent;
-    private View contentView;
-
-    public SwipeableView(Context context) {
-        super(context);
-        init();
-    }
+    private ViewGroup firstPage;
 
     public SwipeableView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -34,41 +31,48 @@ public class SwipeableView extends RelativeLayout implements View.OnClickListene
 
     private void validateUsage() {
         int childCount = getChildCount();
-        if (childCount > 1) {
-            throw new RuntimeException("Can only have one child at the moment");
+        if (childCount > 2) {
+            throw new RuntimeException("Can only have two children at the moment");
         }
     }
 
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-        contentParent = (ViewGroup) getChildAt(0);
-        contentView = contentParent.getChildAt(0);
+        firstPage = (ViewGroup) getChildAt(1);
         setOnClickListener(this);
     }
 
     public void slide() {
         Toast.makeText(getContext(), "Sliding", Toast.LENGTH_SHORT).show();
-        ValueAnimator va = ValueAnimator.ofInt(getWidth() + contentView.getWidth(), contentView.getWidth());
-        va.setDuration(1000);
-        va.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            public void onAnimationUpdate(ValueAnimator animation) {
-                Integer value = (Integer) animation.getAnimatedValue();
-                int i = value.intValue();
-                contentParent.getLayoutParams().width = i;
-                contentParent.requestLayout();
-            }
-        });
+        ValueAnimator va = ValueAnimator.ofInt(getWidth() / 2, 0);
+        va.setDuration(500);
+        va.setStartDelay(0L);
+        va.addUpdateListener(this);
         va.start();
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec + contentView.getWidth(), heightMeasureSpec);
+        super.onMeasure(widthMeasureSpec * 2, heightMeasureSpec);
+        int wspec = MeasureSpec.makeMeasureSpec(getMeasuredWidth() / 2, MeasureSpec.EXACTLY);
+        int hspec = MeasureSpec.makeMeasureSpec(getMeasuredHeight(), MeasureSpec.EXACTLY);
+        for (int i = 0; i < getChildCount(); i++) {
+            View v = getChildAt(i);
+            v.measure(wspec, hspec);
+        }
     }
 
     @Override
     public void onClick(View view) {
         slide();
+    }
+
+    @Override
+    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+        Integer value = (Integer) valueAnimator.getAnimatedValue();
+        int i = value.intValue();
+        firstPage.getLayoutParams().width = i;
+        requestLayout();
     }
 }
