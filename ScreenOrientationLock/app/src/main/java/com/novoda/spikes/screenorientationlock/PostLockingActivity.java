b999e8935be03c0e0d71bcfc118e6c9f2b9e16c9
Ataul Munim <timberwo7ves@gmail.com>
2014-10-06 09:55:14 +0100

    Add skeleton for screenlocking spike

---
+++ ScreenOrientationLock/app/src/main/java/com/novoda/spikes/screenorientationlock/PostLockingActivity.java
@@ -0,0 +1,14 @@
+package com.novoda.spikes.screenorientationlock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PostLockingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_locking);
+    }
+
+}
