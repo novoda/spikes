5bcd750e8580deaa5c97989bda7521216707d8be
Ataul Munim <timberwo7ves@gmail.com>
2015-04-27 15:17:41 +0100

    Add NoFragments simple text tab activity

---
+++ landing-strip/demo/src/main/java/com/novoda/landingstrip/NoFragmentsSimpleTextTabActivity.java
@@ -0,0 +1,24 @@
+package com.novoda.landingstrip;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+
+import com.novoda.landing_strip.R;
+import com.novoda.landingstrip.setup.view.DemoViewPagerAdapter;
+
+public class NoFragmentsSimpleTextTabActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_usage);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        viewPager.setAdapter(new DemoViewPagerAdapter(getLayoutInflater()));
+
+        LandingStrip landingStrip = (LandingStrip) findViewById(R.id.landing_strip);
+        landingStrip.attach(viewPager, viewPager.getAdapter());
+    }
+
+}
