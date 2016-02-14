cb93ffb8454cf99359dff578ff73dfad63321d89
Ataul Munim <hello@ataulm.com>
2016-02-14 20:16:05 +0000

    Add ActionsAccessibilityDelegate

--- accessibility/demo/src/main/java/com/novoda/accessibility/demo/custom_actions/TweetView.java
+++ accessibility/demo/src/main/java/com/novoda/accessibility/demo/custom_actions/TweetView.java
@@ -1,19 +1,27 @@
 package com.novoda.accessibility.demo.custom_actions;
 
 import android.content.Context;
+import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.novoda.accessibility.AccessibilityServices;
+import com.novoda.accessibility.Action;
+import com.novoda.accessibility.Actions;
+import com.novoda.accessibility.ActionsAccessibilityDelegate;
 import com.novoda.accessibility.demo.R;
+
+import java.util.Arrays;
 
 public class TweetView extends LinearLayout {
 
     private TextView tweetTextView;
     private Button replyButton;
     private Button retweetButton;
+    private AccessibilityServices services;
 
     public TweetView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -23,6 +31,8 @@ public class TweetView extends LinearLayout {
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
+        services = AccessibilityServices.newInstance(getContext());
+
         View.inflate(getContext(), R.layout.merge_tweet, this);
         tweetTextView = (TextView) findViewById(R.id.tweet_text);
         replyButton = (Button) findViewById(R.id.tweet_button_reply);
@@ -30,13 +40,20 @@ public class TweetView extends LinearLayout {
     }
 
     public void display(final String tweet, final Listener listener) {
+        Actions actions = createActions(tweet, listener);
+        ViewCompat.setAccessibilityDelegate(this, new ActionsAccessibilityDelegate(getResources(), actions));
+
         tweetTextView.setText(tweet);
 
         setOnClickListener(
                 new OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        listener.onClick(tweet);
+                        if (services.isSpokenFeedbackEnabled()) {
+                            // TODO: show dialog
+                        } else {
+                            listener.onClick(tweet);
+                        }
                     }
                 }
         );
@@ -57,6 +74,31 @@ public class TweetView extends LinearLayout {
                         listener.onClickRetweet(tweet);
                     }
                 }
+        );
+    }
+
+    private Actions createActions(final String tweet, final Listener listener) {
+        return new Actions(
+                Arrays.asList(
+                        new Action(R.id.tweet_action_open, R.string.tweet_action_open, new Runnable() {
+                            @Override
+                            public void run() {
+                                listener.onClick(tweet);
+                            }
+                        }),
+                        new Action(R.id.tweet_action_reply, R.string.tweet_action_reply, new Runnable() {
+                            @Override
+                            public void run() {
+                                listener.onClickReply(tweet);
+                            }
+                        }),
+                        new Action(R.id.tweet_action_retweet, R.string.tweet_action_retweet, new Runnable() {
+                            @Override
+                            public void run() {
+                                listener.onClickRetweet(tweet);
+                            }
+                        })
+                )
         );
     }
 
