aa36b71b873bc1036fbadecd87fa1eeaf2608db3
Daniele Bonaldo <dany.bony@gmail.com>
2016-05-10 18:30:48 +0200

    Display image and author handle

--- ReactNative/ReactTwitter/core/views/tweetsList.js
+++ ReactNative/ReactTwitter/core/views/tweetsList.js
@@ -3,12 +3,14 @@ import {
   Alert,
   StyleSheet,
   View,
+  Image,
   ListView,
   Text,
   TouchableHighlight
 } from 'react-native'
 
 class TweetsList extends Component {
+
   constructor (props) {
     super(props)
     var dataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
@@ -16,17 +18,29 @@ class TweetsList extends Component {
       dataSource: dataSource.cloneWithRows([
         {
           id: '1',
-          user: 'user 1',
+          user: {
+            name: 'User 1',
+            screen_name: 'user_1',
+            profile_image_url: 'http:\/\/pbs.twimg.com\/profile_images\/2215576731\/ars-logo_normal.png'
+          },
           text: 'this is the first tweet'
         },
         {
           id: '2',
-          user: 'user 2',
+          user: {
+            name: 'User 2',
+            screen_name: 'another_user',
+            profile_image_url: 'http:\/\/pbs.twimg.com\/profile_images\/655059892022022144\/Pq3Q_1oU_normal.png'
+          },
           text: 'this is the a cool tweet!!1!'
         },
         {
           id: '3',
-          user: 'user 1',
+          user: {
+            name: 'User 1',
+            screen_name: 'user_1',
+            profile_image_url: 'http:\/\/pbs.twimg.com\/profile_images\/2215576731\/ars-logo_normal.png'
+          },
           text: 'this is the second tweet'
         },
       ]),
@@ -47,9 +61,11 @@ class TweetsList extends Component {
          underlayColor='#dddddd'>
        <View>
         <View style={styles.rowContainer}>
+          <Image style={styles.thumb} source={{ uri: rowData.user.profile_image_url }} />
           <View style={styles.textContainer}>
-            <Text>{rowData.user}</Text>
-            <Text>{rowData.text}</Text>
+            <Text style={styles.tweet_author}>{rowData.user.name}</Text>
+            <Text style={styles.tweet_author_handle}>{rowData.user.screen_name}</Text>
+            <Text style={styles.tweet_text}>{rowData.text}</Text>
           </View>
          </View>
          <View style={styles.separator}/>
@@ -85,6 +101,11 @@ const styles = StyleSheet.create({
     fontSize: 25,
     fontWeight: 'bold',
     color: '#48BBEC'
+  },
+  tweet_author_handle: {
+    fontSize: 20,
+    fontStyle: 'italic',
+    color: '#656565'
   },
   tweet_text: {
     fontSize: 20,
