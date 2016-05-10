88fabf0ee9c8c699a6658fba21700c301ac51500
Daniele Bonaldo <dany.bony@gmail.com>
2016-05-10 15:29:14 +0200

    Create new custom tweets list component to display tweets author and text

---
+++ ReactNative/ReactTwitter/core/views/tweetsList.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react'
+import {
+  Alert,
+  StyleSheet,
+  View,
+  ListView,
+  Text,
+  TouchableHighlight
+} from 'react-native'
+
+class TweetsList extends Component {
+  constructor (props) {
+    super(props)
+    var dataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: dataSource.cloneWithRows([
+        {
+          id: '1',
+          user: 'user 1',
+          text: 'this is the first tweet'
+        },
+        {
+          id: '2',
+          user: 'user 2',
+          text: 'this is the a cool tweet!!1!'
+        },
+        {
+          id: '3',
+          user: 'user 1',
+          text: 'this is the second tweet'
+        },
+      ]),
+    };
+  }
+
+
+  renderRow (rowData, sectionID, rowID) {
+    return (
+      <TouchableHighlight 
+         underlayColor='#dddddd'>
+       <View>
+        <View style={styles.rowContainer}>
+          <View style={styles.textContainer}>
+            <Text>{rowData.user}</Text>
+            <Text>{rowData.text}</Text>
+          </View>
+         </View>
+         <View style={styles.separator}/>
+       </View>
+     </TouchableHighlight>
+    );
+  }
+
+  render () {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderRow.bind(this)}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+  textContainer: {
+    flex: 1
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  tweet_author: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  tweet_text: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  }
+})
+
+module.exports = TweetsList
