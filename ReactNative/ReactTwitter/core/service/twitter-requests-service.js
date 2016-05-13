95527034f5f7208464206f0fe57252a6684e9dab
Eduard Bolos and Giuseppe Basile <eduard@novoda.com>
2016-05-13 11:16:07 +0200

    Extract configuration values to separate JSON file

--- ReactNative/ReactTwitter/core/service/twitter-requests-service.js
+++ ReactNative/ReactTwitter/core/service/twitter-requests-service.js
@@ -1,22 +1,17 @@
 var OauthHelper = require('../service/oauth-helper.js')
 
-const CONSUMER_KEY = 'aqPSTs1FT2ndP7qXi247BtbXd'
-const CONSUMER_SECRET = 'pTvKlESMnsmc7DqP70CfmJypGgk9oaHPxgC7DkUVbxVZXtIGp6'
-
-const BASE_URL = 'https://api.twitter.com'
-const REQUEST_TOKEN = '/oauth/request_token'
-const CALLBACK_URL = 'react-twitter-oauth://callback'
+const config = require('./config.json')
 
 class TwitterRequestsService {
 
   requestToken () {
-    let ouathHelper = new OauthHelper(CONSUMER_SECRET)
+    let ouathHelper = new OauthHelper(config.CONSUMER_SECRET)
     let time = new Date().getTime() / 1000 | 0
     let nonce = OauthHelper.generateNonce()
-    let url = BASE_URL + REQUEST_TOKEN
+    let url = config.BASE_URL + config.REQUEST_TOKEN
     const params = {
-      'oauth_callback': CALLBACK_URL,
-      'oauth_consumer_key': CONSUMER_KEY,
+      'oauth_callback': config.CALLBACK_URL,
+      'oauth_consumer_key': config.CONSUMER_KEY,
       'oauth_nonce': nonce,
       'oauth_signature_method': 'HMAC-SHA1',
       'oauth_timestamp': time,
