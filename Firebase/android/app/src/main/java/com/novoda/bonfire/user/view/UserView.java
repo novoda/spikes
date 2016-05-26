bb6969d1ac6edc93ca503d0c5fc8ac0b9ecbe3e6
Jozef Celuch <jozef@novoda.com>
2016-05-26 15:22:37 +0100

    Make list of channel members a bit more pretty

--- Firebase/android/app/src/main/java/com/novoda/bonfire/user/view/UserView.java
+++ Firebase/android/app/src/main/java/com/novoda/bonfire/user/view/UserView.java
@@ -1,10 +1,10 @@
 package com.novoda.bonfire.user.view;
 
 import android.content.Context;
-import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -12,7 +12,7 @@ import com.novoda.bonfire.R;
 import com.novoda.bonfire.view.CircleCropImageTransformation;
 import com.novoda.notils.caster.Views;
 
-public class UserView extends LinearLayout {
+public class UserView extends FrameLayout {
 
     private final TextView name;
     private final ImageView image;
@@ -20,21 +20,12 @@ public class UserView extends LinearLayout {
 
     public UserView(Context context) {
         super(context);
-        configureViewParams();
+        setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         View.inflate(context, R.layout.merge_user_item_view, this);
         name = Views.findById(this, R.id.userName);
         image = Views.findById(this, R.id.userImage);
         setBackgroundResource(R.drawable.user_item_selector);
         circleCropTransformation = new CircleCropImageTransformation(context);
-    }
-
-    private void configureViewParams() {
-        setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        setOrientation(HORIZONTAL);
-        setGravity(Gravity.CENTER_VERTICAL);
-        int horizontalMargin = getResources().getDimensionPixelSize(R.dimen.list_item_horizontal_margin);
-        int verticalMargin = getResources().getDimensionPixelSize(R.dimen.list_item_vertical_margin);
-        setPadding(horizontalMargin, verticalMargin, horizontalMargin, verticalMargin);
     }
 
     public void display(UsersView.SelectableUser user) {
