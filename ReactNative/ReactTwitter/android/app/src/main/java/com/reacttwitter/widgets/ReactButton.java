2f45124dab5008baf8fef23bfe25230dce07bd01
Eduard Bolos <eduard@novoda.com>
2016-05-27 15:29:11 +0200

    Remove useless init method from ReactButton

--- ReactNative/ReactTwitter/android/app/src/main/java/com/reacttwitter/widgets/ReactButton.java
+++ ReactNative/ReactTwitter/android/app/src/main/java/com/reacttwitter/widgets/ReactButton.java
@@ -4,41 +4,24 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
 import android.util.AttributeSet;
-import android.view.ViewGroup;
 import android.widget.Button;
 
 public class ReactButton extends Button {
 
     public ReactButton(Context context) {
         super(context);
-        init(context);
     }
 
     public ReactButton(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(context);
     }
 
     public ReactButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context);
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public ReactButton(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        init(context);
-    }
-
-    public void init(Context context) {
-        ViewGroup.LayoutParams layoutParams = getLayoutParams();
-        if (layoutParams == null) {
-            layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        } else {
-            layoutParams.width = ViewGroup.LayoutParams.WRAP_CONTENT;
-            layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT;
-        }
-
-        setLayoutParams(layoutParams);
     }
 }
