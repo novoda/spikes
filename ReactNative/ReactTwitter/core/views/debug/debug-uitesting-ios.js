273f553abefcb007253e12a9ff8e7a47404a43ed
Giuseppe Basile <giuseppe@novoda.com>
2016-05-31 18:10:09 +0200

    UITesting using Xcode native tools

---
+++ ReactNative/ReactTwitter/core/views/debug/debug-uitesting-ios.js
@@ -0,0 +1,73 @@
+// @flow
+
+import React from 'react'
+import {
+  StyleSheet,
+  View,
+  Navigator,
+  Text
+} from 'react-native'
+
+var Button = require('react-native-button')
+
+var DebugUITestingScene = React.createClass({
+  statics: {
+    navigatorID () {
+      return 'debug-uitesting-ios-scene-identifier'
+    },
+    navigatorTitle () {
+      return 'iOS UI Testing'
+    }
+  },
+
+  propTypes: {
+    navigator: React.PropTypes.instanceOf(Navigator).isRequired
+  },
+
+  getInitialState () {
+    return {
+      numberOfClicks: 0,
+      text: 'No Clicks'
+    }
+  },
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <Button style={styles.button} testID='button' styleDisabled={styles.button_disabled} onPress={this._updateText}>Update Label</Button>
+        <Text style={styles.label} testID='label'>{this.state.text}</Text>
+      </View>
+    )
+  },
+
+  _updateText () {
+    let newNumberOfClicks = this.state.numberOfClicks + 1
+    this.setState({
+      numberOfClicks: newNumberOfClicks,
+      text: newNumberOfClicks + ' Clicks'
+    })
+  }
+})
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    backgroundColor: '#F5FCFF'
+  },
+  button: {
+    fontSize: 20,
+    color: 'black'
+  },
+  button_disabled: {
+    fontSize: 20,
+    color: 'grey'
+  },
+  label: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  }
+})
+
+module.exports = DebugUITestingScene
---
+++ ReactNative/ReactTwitter/ios/ReactTwitterUITests/ReactTwitterUITests.swift
@@ -0,0 +1,77 @@
+//
+//  ReactTwitterUITests.swift
+//  ReactTwitterUITests
+//
+//  Created by Giuseppe Basile on 31/05/2016.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+import XCTest
+
+class ReactTwitterUITests: XCTestCase {
+
+  override func setUp() {
+    super.setUp()
+    continueAfterFailure = false
+
+    XCUIApplication().launch()
+  }
+
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+
+  func testLabelStartWithInitialState() {
+    let app = XCUIApplication()
+
+    self.goToUITestingScreen()
+
+    let textLabel = app.staticTexts["label"]
+    let text = textLabel.label
+
+    XCTAssertEqual("No Clicks", text)
+  }
+
+  func testLabelStartUpdatesAfterFirstClick() {
+    let app = XCUIApplication()
+
+    self.goToUITestingScreen()
+
+    app.otherElements["button"].tap()
+    sleep(2)
+    let textLabel = app.staticTexts["label"]
+    let text = textLabel.label
+
+    XCTAssertEqual("1 Clicks", text)
+  }
+
+  func goToUITestingScreen() {
+    let app = XCUIApplication()
+    if (app.alerts.count != 0) {
+      app.alerts["“ReactTwitter” Would Like to Send You Notifications"].collectionViews.buttons["OK"].tap()
+    }
+
+    sleep(2) //wait for the splash screen
+
+    //Go To Debug Screen
+    app.otherElements["debug"].tap()
+
+    //Go To UITesting Screen
+    let debugList = app.otherElements["debug-list"]
+    self.waitForElementToExist(debugList)
+    debugList.otherElements["debug-uitesting-ios-scene-identifier"].tap()
+
+    let textLabel = app.staticTexts["label"]
+    self.waitForElementToExist(textLabel)
+
+    let button = app.otherElements["button"]
+    self.waitForElementToExist(button)
+  }
+
+  func waitForElementToExist(element: XCUIElement) {
+
+    expectationForPredicate(NSPredicate(format: "exists == true"), evaluatedWithObject: element, handler: nil)
+    waitForExpectationsWithTimeout(2, handler: nil)
+  }
+}
