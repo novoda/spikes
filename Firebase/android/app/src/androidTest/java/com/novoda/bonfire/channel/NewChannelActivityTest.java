177a78fa102696083c872e168e45433e3dd5ba22
Jozef Celuch <jozef@novoda.com>
2016-05-31 11:27:24 +0100

    Test private channel creation

--- Firebase/android/app/src/androidTest/java/com/novoda/bonfire/channel/NewChannelActivityTest.java
+++ Firebase/android/app/src/androidTest/java/com/novoda/bonfire/channel/NewChannelActivityTest.java
@@ -15,6 +15,7 @@ import com.novoda.bonfire.chat.service.ChatService;
 import com.novoda.bonfire.database.DatabaseResult;
 import com.novoda.bonfire.login.data.model.Authentication;
 import com.novoda.bonfire.login.service.LoginService;
+import com.novoda.bonfire.user.data.model.User;
 import com.novoda.bonfire.user.service.UserService;
 
 import java.util.ArrayList;
@@ -23,18 +24,21 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 import rx.Observable;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.action.ViewActions.*;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.*;
-import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.not;
 import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(AndroidJUnit4.class)
@@ -42,41 +46,64 @@ public class NewChannelActivityTest {
 
     @Rule
     public ActivityTestRule<NewChannelActivity> activity = new ActivityTestRule<>(NewChannelActivity.class, false, false);
+
+    private final Answer<Observable<DatabaseResult<Channel>>> observableWithInsertedChannel = new Answer<Observable<DatabaseResult<Channel>>>() {
+        @Override
+        public Observable<DatabaseResult<Channel>> answer(InvocationOnMock invocation) throws Throwable {
+            return Observable.just(new DatabaseResult<>((Channel) invocation.getArguments()[0]));
+        }
+    };
     private ChannelService channelService;
-    private ChatService chatService;
 
     @Before
     public void setUp() throws Exception {
         channelService = Mockito.mock(ChannelService.class);
-        chatService = Mockito.mock(ChatService.class);
+        ChatService chatService = Mockito.mock(ChatService.class);
         LoginService loginService = Mockito.mock(LoginService.class);
         when(loginService.getAuthentication()).thenReturn(Observable.just(Mockito.mock(Authentication.class)));
 
+        when(chatService.getChat(any(Channel.class)))
+                .thenReturn(Observable.just(new DatabaseResult<>(new Chat(new ArrayList<Message>()))));
+        when(channelService.createPublicChannel(any(Channel.class))).thenAnswer(observableWithInsertedChannel);
+        when(channelService.createPrivateChannel(any(Channel.class), any(User.class))).thenAnswer(observableWithInsertedChannel);
         Dependencies.INSTANCE.setLoginService(loginService);
         Dependencies.INSTANCE.setUserService(Mockito.mock(UserService.class));
         Dependencies.INSTANCE.setChatService(chatService);
         Dependencies.INSTANCE.setChannelService(channelService);
         Dependencies.INSTANCE.setAnalytics(Mockito.mock(Analytics.class));
+
+        activity.launchActivity(new Intent());
     }
 
     @Test
     public void channelWithEmptyNameCannotBeCreated() throws Exception {
-        activity.launchActivity(new Intent());
-
         onView(withId(R.id.createButton)).check(matches(not(isEnabled())));
     }
 
     @Test
-    public void channelWithANameCanBeCreated() throws Exception {
-        String name = "new channel";
-        activity.launchActivity(new Intent());
+    public void canCreatePublicChannel() throws Exception {
+        String channelName = "public channel";
+        ArgumentCaptor<Channel> channelArgumentCaptor = ArgumentCaptor.forClass(Channel.class);
 
-        when(channelService.createPublicChannel(any(Channel.class))).thenReturn(Observable.just(new DatabaseResult<>(new Channel(name, false))));
-        when(chatService.getChat(any(Channel.class))).thenReturn(Observable.just(new DatabaseResult<>(new Chat(new ArrayList<Message>()))));
-        onView(withId(R.id.newChannelName)).perform(typeText(name));
+        onView(withId(R.id.newChannelName)).perform(typeText(channelName), closeSoftKeyboard());
         onView(withId(R.id.createButton)).perform(click());
 
+        verify(channelService).createPublicChannel(channelArgumentCaptor.capture());
+        assertThat(channelArgumentCaptor.getValue().isPrivate(), equalTo(false));
+        assertThat(channelArgumentCaptor.getValue().getName(), equalTo(channelName));
+    }
 
-        onView(allOf(isDescendantOfA(withId(R.id.toolbar)), withText(name))).check(matches(isDisplayed()));
+    @Test
+    public void canCreatePrivateChannelWhen() throws Exception {
+        String channelName = "private channel";
+        ArgumentCaptor<Channel> channelArgumentCaptor = ArgumentCaptor.forClass(Channel.class);
+
+        onView(withId(R.id.newChannelName)).perform(typeText(channelName), closeSoftKeyboard());
+        onView(withId(R.id.privateChannel)).perform(click());
+        onView(withId(R.id.createButton)).perform(click());
+
+        verify(channelService).createPrivateChannel(channelArgumentCaptor.capture(), any(User.class));
+        assertThat(channelArgumentCaptor.getValue().isPrivate(), equalTo(true));
+        assertThat(channelArgumentCaptor.getValue().getName(), equalTo(channelName));
     }
 }
