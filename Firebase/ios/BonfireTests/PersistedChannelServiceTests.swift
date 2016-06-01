8816f998246a008b5b88269efd31fae841644c35
Dominic Freeston <dominic.freeston@gmail.com>
2016-06-01 11:37:08 +0100

    adding a test that should totally compile but for some reason doesnt

---
+++ Firebase/ios/BonfireTests/PersistedChannelServiceTests.swift
@@ -0,0 +1,103 @@
+import XCTest
+import RxSwift
+import RxTests
+@testable import Bonfire
+
+class PersistedChannelServiceTests: XCTestCase {
+
+    var scheduler: TestScheduler!
+    var testableChannelsObserver: TestableObserver<Channel>!
+    var testableUsersObserver: TestableObserver<[User]>!
+
+    let testUser = User(name: "TestUser", id: "1", photoURL: nil)
+
+    override func setUp() {
+        super.setUp()
+        scheduler = TestScheduler(initialClock: 0)
+        testableChannelsObserver = scheduler.createObserver(Channel)
+        testableUsersObserver = scheduler.createObserver([User])
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    struct MockUserDatabase: UserDatabase {
+        func observeUsers() -> Observable<[User]> {
+            return Observable.empty()
+        }
+
+        func readUserFrom(userID: String) -> Observable<User> {
+            return Observable.just(User(name: "TestUser", id: "1", photoURL: nil))
+        }
+
+        func writeCurrentUser(user: User) {}
+    }
+
+    struct MockChannelDatabase: ChannelsDatabase {
+        func observePublicChannelIds() -> Observable<[String]> {
+            return Observable.empty()
+        }
+
+        func observePrivateChannelIdsFor(user: User) -> Observable<[String]> {
+            return Observable.empty()
+        }
+
+        func readChannelFor(channelName: String) -> Observable<Channel> {
+            return Observable.empty()
+        }
+
+        func writeChannel(newChannel: Channel) -> Observable<Channel> {
+            return Observable.empty()
+        }
+
+        func writeChannelToPublicChannelIndex(newChannel: Channel) -> Observable<Channel> {
+            return Observable.empty()
+        }
+
+        func addOwnerToPrivateChannel(user: User, channel: Channel) -> Observable<Channel> {
+            return Observable.empty()
+        }
+
+        func removeOwnerFromPrivateChannel(user: User, channel: Channel) -> Observable<Channel> {
+            return Observable.empty()
+        }
+
+        func addChannelToUserPrivateChannelIndex(user: User, channel: Channel) -> Observable<Channel> {
+            return Observable.empty()
+        }
+
+        func removeChannelFromUserPrivateChannelIndex(user: User, channel: Channel) -> Observable<Channel> {
+            return Observable.empty()
+        }
+
+        func observeOwnerIdsFor(channel: Channel) -> Observable<[String]> {
+            return Observable.empty()
+        }
+
+    }
+
+    func testThatItReturnsUsersForChannel() {
+        // Given
+        let channel = Channel(name: "Test Channel", access: .Public)
+        let mockChannelDatabase = MockChannelDatabase()
+        let mockUserDatabase = MockUserDatabase()
+
+        let channelService = PersistedChannelsService(channelsDatabase: mockChannelDatabase, userDatabase: mockUserDatabase)
+
+        // When
+        channelService.users(forChannel: channel).subscribe(testableUsersObserver)
+        scheduler.start()
+
+        // Then
+        let users = [testUser]
+        let expectedEvents = [
+            next(1, users)
+        ]
+
+        XCTAssertEqual(testableUsersObserver.events, expectedEvents)
+    }
+
+
+    
+}
