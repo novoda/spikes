36363514b362c61ff0bde582ead8c88a488db1db
Dominic Freeston <dominic.freeston@gmail.com>
2016-06-01 15:39:13 +0100

    made test a bit more robust

--- Firebase/ios/BonfireTests/PersistedChannelServiceTests.swift
+++ Firebase/ios/BonfireTests/PersistedChannelServiceTests.swift
@@ -9,7 +9,8 @@ class PersistedChannelServiceTests: XCTestCase {
     var testableChannelsObserver: TestableObserver<Channel>!
     var testableUsersObserver: TestableObserver<Users>!
 
-    let testUser = User(name: "TestUser", id: "1", photoURL: nil)
+    let testUser1 = User(name: "TestUser1", id: "1", photoURL: nil)
+    let testUser2 = User(name: "TestUser2", id: "2", photoURL: nil)
 
     override func setUp() {
         super.setUp()
@@ -17,24 +18,50 @@ class PersistedChannelServiceTests: XCTestCase {
         testableChannelsObserver = scheduler.createObserver(Channel)
         testableUsersObserver = scheduler.createObserver(Users)
     }
-    
+
     override func tearDown() {
         super.tearDown()
     }
 
     struct MockUserDatabase: UserDatabase {
+
+        let scheduler: TestScheduler
+
+        init(scheduler: TestScheduler) {
+            self.scheduler = scheduler
+        }
+
         func observeUsers() -> Observable<Users> {
             return Observable.empty()
         }
 
         func readUserFrom(userID: String) -> Observable<User> {
-            return Observable.just(User(name: "TestUser", id: "1", photoURL: nil))
+            if userID == "1" {
+                return scheduler.createColdObservable([
+                    next(0, User(name: "TestUser1", id: "1", photoURL: nil)),
+                    completed(0)
+                    ]).asObservable()
+            } else if userID == "2" {
+                return scheduler.createColdObservable([
+                    next(0, User(name: "TestUser2", id: "2", photoURL: nil)),
+                    completed(0)
+                    ]).asObservable().debug("wtf")
+            } else {
+                return Observable.empty()
+            }
         }
 
         func writeCurrentUser(user: User) {}
     }
 
     struct MockChannelDatabase: ChannelsDatabase {
+
+        let scheduler: TestScheduler
+
+        init(scheduler: TestScheduler) {
+            self.scheduler = scheduler
+        }
+
         func observePublicChannelIds() -> Observable<[String]> {
             return Observable.empty()
         }
@@ -72,16 +99,30 @@ class PersistedChannelServiceTests: XCTestCase {
         }
 
         func observeOwnerIdsFor(channel: Channel) -> Observable<[String]> {
-            return Observable.empty()
+            if channel.name == "Test Channel" {
+                return scheduler.createHotObservable([
+                    next(0, ["1", "2"])
+                    ]).asObservable()
+            } else {
+                return Observable.empty()
+            }
         }
 
     }
 
+    func testThatItReturnsChannelsForUser() {
+        let user = User(name: "Test User", id: "1", photoURL: nil)
+
+
+    }
+
+
     func testThatItReturnsUsersForChannel() {
         // Given
-        let channel = Channel(name: "Test Channel", access: .Public)
-        let mockChannelDatabase = MockChannelDatabase()
-        let mockUserDatabase = MockUserDatabase()
+        let channel = Channel(name: "Test Channel", access: .Private)
+
+        let mockChannelDatabase = MockChannelDatabase(scheduler: scheduler)
+        let mockUserDatabase = MockUserDatabase(scheduler: scheduler)
 
         let channelService = PersistedChannelsService(channelsDatabase: mockChannelDatabase, userDatabase: mockUserDatabase)
 
@@ -90,11 +131,11 @@ class PersistedChannelServiceTests: XCTestCase {
         scheduler.start()
 
         // Then
-        let users = [testUser]
+        let users = [testUser1, testUser2]
         let expectedEvents = [
-            next(1, Users(users: users))
+            next(0, Users(users: users))
         ]
-
+        
         XCTAssertEqual(testableUsersObserver.events, expectedEvents)
     }
 }
