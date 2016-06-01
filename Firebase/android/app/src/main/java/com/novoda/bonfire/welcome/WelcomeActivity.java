ad7cedc6ba231bd04e0f5222560ee863b1bd9aa4
Jozef Celuch <jozef@novoda.com>
2016-06-01 16:40:41 +0100

    Improve welcome screen layout

--- Firebase/android/app/src/main/java/com/novoda/bonfire/welcome/WelcomeActivity.java
+++ Firebase/android/app/src/main/java/com/novoda/bonfire/welcome/WelcomeActivity.java
@@ -2,37 +2,38 @@ package com.novoda.bonfire.welcome;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.View;
-import android.widget.TextView;
 
 import com.novoda.bonfire.BaseActivity;
+import com.novoda.bonfire.R;
 import com.novoda.bonfire.navigation.AndroidNavigator;
+import com.novoda.bonfire.welcome.displayer.WelcomeDisplayer;
+import com.novoda.bonfire.welcome.presenter.WelcomePresenter;
 import com.novoda.notils.logger.simple.Log;
 
 public class WelcomeActivity extends BaseActivity {
 
-    private AndroidNavigator navigator;
+    private WelcomePresenter welcomePresenter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("Got it!");
-        TextView textView = new TextView(this);
-        setContentView(textView);
+        setContentView(R.layout.activity_welcome);
         String sender = getIntent().getData().getQueryParameter("sender");
-        textView.setText(sender);
-        navigator = new AndroidNavigator(this);
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                navigator.toLogin();
-            }
-        });
+        welcomePresenter = new WelcomePresenter((WelcomeDisplayer) findViewById(R.id.welcomeView),
+                                                new AndroidNavigator(this),
+                                                sender);
     }
 
     @Override
-    public void onBackPressed() {
-        navigator.toLogin();
+    protected void onStart() {
+        super.onStart();
+        welcomePresenter.startPresenting();
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        welcomePresenter.stopPresenting();
+    }
 }
