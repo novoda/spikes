5d8b9233ad9a20b4accbe3a944d07d7d967f049d
Jozef Celuch <jozef@novoda.com>
2016-06-08 15:58:02 +0100

    Extract constants for firebase events and params

--- Firebase/android/app/src/main/java/com/novoda/bonfire/analytics/FirebaseAnalyticsAnalytics.java
+++ Firebase/android/app/src/main/java/com/novoda/bonfire/analytics/FirebaseAnalyticsAnalytics.java
@@ -1,21 +1,34 @@
 package com.novoda.bonfire.analytics;
 
-import android.content.Context;
 import android.os.Bundle;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
 
-import static com.google.firebase.analytics.FirebaseAnalytics.*;
+import static com.google.firebase.analytics.FirebaseAnalytics.Event;
+import static com.google.firebase.analytics.FirebaseAnalytics.Param;
 
 public class FirebaseAnalyticsAnalytics implements Analytics {
 
-    private static final String CHANNEL_NAME = "channel_name";
-    private static final String SENDER = "sender";
-    private static final String USER_ID = "user_id";
+    private static final String PARAM_CHANNEL_NAME = "channel_name";
+    private static final String PARAM_SENDER = "sender";
+    private static final String PARAM_USER_ID = "user_id";
+    private static final String PARAM_ADDED_OWNER = "added_owner";
+    private static final String PARAM_REMOVED_OWNER = "removed_owner";
+    private static final String EVENT_SIGN_UP_SUCCESS = "sign_up_success";
+    private static final String EVENT_MESSAGE_LENGTH = "message_length";
+    private static final String EVENT_INVITE_OPENED = "invite_opened";
+    private static final String EVENT_INVITE_ACCEPTED = "invite_accepted";
+    private static final String EVENT_MANAGE_OWNERS = "manage_owners";
+    private static final String EVENT_ADD_CHANNEL_OWNER = "add_channel_owner";
+    private static final String EVENT_REMOVE_CHANNEL_OWNER = "remove_channel_owner";
+    private static final String EVENT_SEND_INVITES = "send_invites";
+    private static final String EVENT_CREATE_CHANNEL = "create_channel";
+    private static final String CONTENT_TYPE_CHANNEL = "channel";
+
     private final FirebaseAnalytics firebaseAnalytics;
 
-    public FirebaseAnalyticsAnalytics(Context context) {
-        firebaseAnalytics = getInstance(context);
+    public FirebaseAnalyticsAnalytics(FirebaseAnalytics firebaseAnalytics) {
+        this.firebaseAnalytics = firebaseAnalytics;
     }
 
     @Override
@@ -29,13 +42,13 @@ public class FirebaseAnalyticsAnalytics implements Analytics {
     public void trackSignInSuccessful(String method) {
         Bundle bundle = new Bundle();
         bundle.putString(Param.SIGN_UP_METHOD, method);
-        firebaseAnalytics.logEvent("sign_up_success", bundle);
+        firebaseAnalytics.logEvent(EVENT_SIGN_UP_SUCCESS, bundle);
     }
 
     @Override
     public void trackSelectChannel(String channelName) {
         Bundle bundle = new Bundle();
-        bundle.putString(Param.CONTENT_TYPE, "channel");
+        bundle.putString(Param.CONTENT_TYPE, CONTENT_TYPE_CHANNEL);
         bundle.putString(Param.ITEM_ID, channelName);
         firebaseAnalytics.logEvent(Event.SELECT_CONTENT, bundle);
     }
@@ -44,61 +57,61 @@ public class FirebaseAnalyticsAnalytics implements Analytics {
     public void trackMessageLength(int messageLength, String userId, String channelName) {
         Bundle bundle = new Bundle();
         bundle.putInt(Param.VALUE, messageLength);
-        bundle.putString(CHANNEL_NAME, channelName);
-        bundle.putString(USER_ID, userId);
-        firebaseAnalytics.logEvent("message_length", bundle);
+        bundle.putString(PARAM_CHANNEL_NAME, channelName);
+        bundle.putString(PARAM_USER_ID, userId);
+        firebaseAnalytics.logEvent(EVENT_MESSAGE_LENGTH, bundle);
     }
 
     @Override
     public void trackInvitationOpened(String senderId) {
         Bundle bundle = new Bundle();
-        bundle.putString(SENDER, senderId);
-        firebaseAnalytics.logEvent("invite_opened", bundle);
+        bundle.putString(PARAM_SENDER, senderId);
+        firebaseAnalytics.logEvent(EVENT_INVITE_OPENED, bundle);
     }
 
     @Override
     public void trackInvitationAccepted(String senderId) {
         Bundle bundle = new Bundle();
-        bundle.putString(SENDER, senderId);
-        firebaseAnalytics.logEvent("invite_accepted", bundle);
+        bundle.putString(PARAM_SENDER, senderId);
+        firebaseAnalytics.logEvent(EVENT_INVITE_ACCEPTED, bundle);
     }
 
     @Override
     public void trackManageOwners(String userId, String channelName) {
         Bundle bundle = new Bundle();
-        bundle.putString(USER_ID, userId);
-        bundle.putString(CHANNEL_NAME, channelName);
-        firebaseAnalytics.logEvent("manage_owners", bundle);
+        bundle.putString(PARAM_USER_ID, userId);
+        bundle.putString(PARAM_CHANNEL_NAME, channelName);
+        firebaseAnalytics.logEvent(EVENT_MANAGE_OWNERS, bundle);
     }
 
     @Override
     public void trackAddChannelOwner(String channelName, String userId) {
         Bundle bundle = new Bundle();
-        bundle.putString(CHANNEL_NAME, channelName);
-        bundle.putString("added_owner", userId);
-        firebaseAnalytics.logEvent("add_channel_owner", bundle);
+        bundle.putString(PARAM_CHANNEL_NAME, channelName);
+        bundle.putString(PARAM_ADDED_OWNER, userId);
+        firebaseAnalytics.logEvent(EVENT_ADD_CHANNEL_OWNER, bundle);
     }
 
     @Override
     public void trackRemoveChannelOwner(String channelName, String userId) {
         Bundle bundle = new Bundle();
-        bundle.putString(CHANNEL_NAME, channelName);
-        bundle.putString("removed_owner", userId);
-        firebaseAnalytics.logEvent("remove_channel_owner", bundle);
+        bundle.putString(PARAM_CHANNEL_NAME, channelName);
+        bundle.putString(PARAM_REMOVED_OWNER, userId);
+        firebaseAnalytics.logEvent(EVENT_REMOVE_CHANNEL_OWNER, bundle);
     }
 
     @Override
     public void trackSendInvitesSelected(String userId) {
         Bundle bundle = new Bundle();
-        bundle.putString(USER_ID, userId);
-        firebaseAnalytics.logEvent("send_invites", bundle);
+        bundle.putString(PARAM_USER_ID, userId);
+        firebaseAnalytics.logEvent(EVENT_SEND_INVITES, bundle);
     }
 
     @Override
     public void trackCreateChannel(String userId) {
         Bundle bundle = new Bundle();
-        bundle.putString(USER_ID, userId);
-        firebaseAnalytics.logEvent("create_channel", bundle);
+        bundle.putString(PARAM_USER_ID, userId);
+        firebaseAnalytics.logEvent(EVENT_CREATE_CHANNEL, bundle);
     }
 
 }
