aea1024c34c9e5db84620ab67a7bbf6a735865af
Dominic Freeston <dominic.freeston@gmail.com>
2016-06-14 21:35:13 +0100

    set correct dynamic link URL

--- Firebase/ios/Bonfire/Mobile/FirebaseIdentifiers.swift
+++ Firebase/ios/Bonfire/Mobile/FirebaseIdentifiers.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 struct FirebaseIdentifiers {
-    static let dynamicLinkDomain = "https://w5htk.app.goo.gl/"
+    static let dynamicLinkDomain = "https://t6c2e.app.goo.gl/"
     static let bundleIdentifier = "com.novoda.bonfire"
     static let androidPackageName = "com.novoda.bonfire"
     static let deepLinkBaseURL = NSURL(string: "https://novoda.com/bonfire")!
