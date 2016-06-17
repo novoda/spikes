9829954521c68f9ecbfe8869e21186b2af35b744
Blundell <paul@novoda.com>
2016-06-17 14:33:32 +0100

    increase single responsibility

--- MakeFestSimonSays/app/src/main/java/com/novoda/simonsays/game/RoundCreator.java
+++ MakeFestSimonSays/app/src/main/java/com/novoda/simonsays/game/RoundCreator.java
@@ -21,22 +21,27 @@ class RoundCreator {
     }
 
     public Round createRound(int roundNumber) {
-        if (roundNumber == 1) {
-            sequence.clear();
-        }
-        int randomPosition = random.nextInt(this.views.size());
-        sequence.add(randomPosition);
-
+        incrementSequence(roundNumber);
         List<View> orderedViews = new ArrayList<>();
         List<Integer> orderedKeys = new ArrayList<>();
         for (Integer position : sequence) {
             orderedViews.add(this.views.get(position));
             orderedKeys.add(keys[position]);
         }
-
         Round.ViewSequence viewSequence = new Round.ViewSequence(orderedViews);
         Round.KeySequence keySequence = new Round.KeySequence(orderedKeys);
-        long speedInMillis = 3000 / roundNumber;
-        return new Round(roundNumber, viewSequence, keySequence, speedInMillis);
+        return new Round(roundNumber, viewSequence, keySequence, getSpeedInMillis(roundNumber));
+    }
+
+    private void incrementSequence(int roundNumber) {
+        if (roundNumber == 1) {
+            sequence.clear();
+        }
+        int randomPosition = random.nextInt(this.views.size());
+        sequence.add(randomPosition);
+    }
+
+    private long getSpeedInMillis(int roundNumber) {
+        return (long) (3000 / roundNumber);
     }
 }
