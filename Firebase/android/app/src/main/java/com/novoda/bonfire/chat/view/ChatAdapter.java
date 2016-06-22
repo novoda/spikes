dc6be476146c72a24900ec47e737ab18105adde3
Jozef Celuch <jozef@novoda.com>
2016-06-22 10:48:18 +0100

    Extract constants for message view types

--- Firebase/android/app/src/main/java/com/novoda/bonfire/chat/view/ChatAdapter.java
+++ Firebase/android/app/src/main/java/com/novoda/bonfire/chat/view/ChatAdapter.java
@@ -9,6 +9,7 @@ import com.novoda.bonfire.chat.data.model.Chat;
 import com.novoda.bonfire.chat.data.model.Message;
 import com.novoda.bonfire.user.data.model.User;
 import com.novoda.bonfire.view.MessageBubbleDrawable;
+import com.novoda.notils.exception.DeveloperError;
 
 import java.util.ArrayList;
 
@@ -16,6 +17,8 @@ import static com.novoda.bonfire.view.MessageBubbleDrawable.*;
 
 class ChatAdapter extends RecyclerView.Adapter<MessageViewHolder> {
 
+    private static final int VIEW_TYPE_MESSAGE_THIS_USER = 0;
+    private static final int VIEW_TYPE_MESSAGE_OTHER_USERS = 1;
     private Chat chat = new Chat(new ArrayList<Message>());
     private User user = new User("", "", "");
     private final LayoutInflater inflater;
@@ -35,12 +38,14 @@ class ChatAdapter extends RecyclerView.Adapter<MessageViewHolder> {
     public MessageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         MessageBubbleDrawable bubbleDrawable;
         MessageView messageView;
-        if (viewType == 0) {
+        if (viewType == VIEW_TYPE_MESSAGE_THIS_USER) {
             bubbleDrawable = new MessageBubbleDrawable(parent.getContext(), R.color.colorPrimaryLight, Gravity.END);
             messageView = (MessageView) inflater.inflate(R.layout.self_message_item_layout, parent, false);
-        } else {
+        } else if (viewType == VIEW_TYPE_MESSAGE_OTHER_USERS) {
             bubbleDrawable = new MessageBubbleDrawable(parent.getContext(), R.color.bubble_grey, Gravity.START);
             messageView = (MessageView) inflater.inflate(R.layout.message_item_layout, parent, false);
+        } else {
+            throw new DeveloperError("There is an unknown view type, you should inflate a view for it.");
         }
         messageView.setTextBackground(bubbleDrawable);
         return new MessageViewHolder(messageView);
@@ -63,6 +68,6 @@ class ChatAdapter extends RecyclerView.Adapter<MessageViewHolder> {
 
     @Override
     public int getItemViewType(int position) {
-        return chat.get(position).getAuthor().getId().equals(user.getId()) ? 0 : 1;
+        return chat.get(position).getAuthor().getId().equals(user.getId()) ? VIEW_TYPE_MESSAGE_THIS_USER : VIEW_TYPE_MESSAGE_OTHER_USERS;
     }
 }
