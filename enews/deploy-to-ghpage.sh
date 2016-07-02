081988cc2d85760d71c3c9e97e33b0e48e7b720c
Adam Brown <adam@novoda.com>
2016-07-02 11:56:39 +0100

    adding subtree ghpage deployment

--- enews/deploy-to-ghpage.sh
+++ enews/deploy-to-ghpage.sh
@@ -4,6 +4,6 @@ node app.js
 
 mkdir output
 mv enews.html output/index.html
-
+git add output && git commit -m "deploy html"
 cd ../../
 git subtree push --prefix enews/html-generator-node/output origin gh-pages
