59e9b18cfe6b23547e601bf07be58c639678c75d
Adam Brown <adam@novoda.com>
2016-07-02 10:43:23 +0100

    reintroducing the moment lib to create a formatted version of todays date

--- enews/html-generator-node/htmlGenerator.js
+++ enews/html-generator-node/htmlGenerator.js
@@ -2,9 +2,11 @@ module.exports = {
   generateHtml: generateHtml
 };
 
+var moment = require('moment');
+
 function generateHtml(eNews) {
-  var date = /*moment().format('YYYY-MM-DD')*/ '10/10/10';
-  var html = '<h1 style="font-family:Comic Sans MS">#enews ' + date + '</h1><br><table style="width:100%" cellspacing="20">';
+  var formattedTodaysDate = moment().format('YYYY-MM-DD');
+  var html = '<h1 style="font-family:Comic Sans MS">#enews ' + formattedTodaysDate + '</h1><br><table style="width:100%" cellspacing="20">';
   eNews.forEach(function(each) {
     var item = '<tr>' +
     '<td><a href="' + each.link + '"><img src=' + each.imageUrl + ' height="150" width="150"/></td><a/>' +
