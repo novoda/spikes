d74b96b39864ec61562ddb05408d9045329ef0e4
Adam Brown <adam@novoda.com>
2016-07-02 11:46:07 +0100

    moving output html to output dir and pushing to gh-pges

--- enews/deploy-to-ghpage.sh
+++ enews/deploy-to-ghpage.sh
@@ -1,3 +1,9 @@
 (cd enews-node && npm install)
 cd html-generator-node && npm install
 node app.js
+
+mkdir output
+mv enews.html output/index.html
+
+cd ../../
+git subtree push --prefix enews/html-generator-node/output origin gh-pages
