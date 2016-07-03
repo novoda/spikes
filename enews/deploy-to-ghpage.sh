cef8039b6e4456bf2d94506b9f259ba5b87e8f30
Adam Brown <adam@novoda.com>
2016-07-03 13:45:13 +0100

    using git root instead of randomly cd up

--- enews/deploy-to-ghpage.sh
+++ enews/deploy-to-ghpage.sh
@@ -4,6 +4,7 @@ node app.js
 mkdir output
 mv enews.html output/index.html
 git add output && git commit -m "deploy html"
-cd ../../
+
+cd `git rev-parse --show-toplevel`
 
 git subtree push --prefix enews/html-generator-node/output origin gh-pages
