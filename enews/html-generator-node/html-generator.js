0a7d651ffb0707af86d1ba94059ac39389e35a1e
Adam Brown <adam@novoda.com>
2016-07-03 20:20:46 +0100

    separating the text and image html generation

--- enews/html-generator-node/html-generator.js
+++ enews/html-generator-node/html-generator.js
@@ -8,11 +8,26 @@ function generateHtml(eNews) {
   var formattedTodaysDate = moment().format('YYYY-MM-DD');
   var html = '<h1 style="font-family:Comic Sans MS">#enews ' + formattedTodaysDate + '</h1><br><table style="width:100%" cellspacing="20">';
   eNews.forEach(function(each) {
-    var item = '<tr>' +
-    '<td><a href="' + each.link + '"><img src=' + each.imageUrl + ' height="150" width="150"/></td><a/>' +
-    '<td><p style="font-family:Comic Sans MS">' + '<i>' + each.originalMessage + '</i>' + '<br>' + each.title + '</p>' +
-    '<br><p style="font-family:Comic Sans MS">' + each.poster + '</p></td>';
-    html+= item;
+    if (each.imageUrl) {
+      html += createImageItem(each);
+    } else {
+      html += createTextItem(each);
+    }
   });
  return html + '</table><br><br><br>sik news bro';
 };
+
+function createImageItem(each) {
+  return '<tr>' +
+  '<td><a href="' + each.link + '"><img src=' + each.imageUrl + ' height="150" width="150"/></td><a/>' +
+  '<td><p style="font-family:Comic Sans MS">' + '<i>' + each.originalMessage + '</i>' + '<br>' + each.title + '</p>' +
+  '<br><p style="font-family:Comic Sans MS">' + each.poster + '</p></td>' +
+  '</tr>';
+}
+
+function createTextItem(each) {
+  return '<tr>' +
+  '<p style="font-family:Comic Sans MS">' + '<i>' + each.originalMessage + '</i>' + '<br>' + each.title + '</p>' +
+  '<br><p style="font-family:Comic Sans MS">' + each.poster + '</p>' +
+  '</tr>';
+}
