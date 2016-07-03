377e350afe161ebe28e37d4a034c48230188532f
Adam Brown <adam@novoda.com>
2016-07-03 13:50:28 +0100

    moving html deploy script to the html module

--- enews/deploy-to-ghpage.sh
+++
@@ -1,7 +0,0 @@
-mkdir output
-mv enews.html output/index.html
-git add output && git commit -m "deploy enews html"
-
-cd `git rev-parse --show-toplevel`
-
-git subtree push --prefix enews/html-generator-node/output origin gh-pages
