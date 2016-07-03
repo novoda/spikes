b8037b94db4315affba81c0749d776c535e1fa2e
Adam Brown <adam@novoda.com>
2016-07-03 14:18:30 +0100

    force pushing the gh page changes

--- enews/html-generator-node/deploy-to-ghpage.sh
+++ enews/html-generator-node/deploy-to-ghpage.sh
@@ -4,4 +4,4 @@ git add output && git commit -m "deploy enews html"
 
 cd `git rev-parse --show-toplevel`
 
-git subtree push --prefix enews/html-generator-node/output origin gh-pages
+git push origin `git subtree split --prefix enews/html-generator-node/output gh-pages`:gh-pages --force
