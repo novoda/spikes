f17a759844b173b2938fa3a293aa3b44d2ace24e
Adam Brown <adam@novoda.com>
2016-07-03 14:47:31 +0100

    simplifying auto deploy script to avoid subtrees

--- enews/html-generator-node/deploy-to-ghpage.sh
+++ enews/html-generator-node/deploy-to-ghpage.sh
@@ -1,7 +1,5 @@
-mkdir output
-mv enews.html output/index.html
-git add output && git commit -m "deploy enews html"
-
+git checkout gh-pages
 cd `git rev-parse --show-toplevel`
-
-git push origin `git subtree split --prefix enews/html-generator-node/output gh-pages`:gh-pages --force
+mv enews/html-generator-node/enews.html index.html
+git add index.html && git commit -m "deploy enews html"
+git push -f origin gh-pages
