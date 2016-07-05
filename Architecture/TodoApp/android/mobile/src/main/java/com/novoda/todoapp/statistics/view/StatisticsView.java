ee978ff540b63bc98c5ed4f687456ecab6737de2
Dorvaryn <ben@novoda.com>
2016-07-05 15:03:54 +0100

    Adding the app bar to the statistics screen

--- Architecture/TodoApp/android/mobile/src/main/java/com/novoda/todoapp/statistics/view/StatisticsView.java
+++ Architecture/TodoApp/android/mobile/src/main/java/com/novoda/todoapp/statistics/view/StatisticsView.java
@@ -1,24 +1,43 @@
 package com.novoda.todoapp.statistics.view;
 
 import android.content.Context;
+import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.novoda.notils.caster.Views;
+import com.novoda.todoapp.R;
 import com.novoda.todoapp.statistics.data.model.Statistics;
 import com.novoda.todoapp.statistics.displayer.StatisticsDisplayer;
+import com.novoda.todoapp.views.TodoAppBar;
 
-public class StatisticsView extends TextView implements StatisticsDisplayer {
+public class StatisticsView extends LinearLayout implements StatisticsDisplayer {
+
+    private TextView statisticsLabel;
 
     public StatisticsView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setOrientation(VERTICAL);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        View.inflate(getContext(), R.layout.merge_statistics_view, this);
+        statisticsLabel = Views.findById(this, R.id.statistics_label);
+        TodoAppBar todoAppBar = Views.findById(this, R.id.app_bar);
+        Toolbar toolbar = todoAppBar.getToolbar();
+        toolbar.setTitle("Statistics");
     }
 
     @Override
     public void display(Statistics statistics) {
         if (statistics.noTasks()) {
-            setText("No Tasks");
+            statisticsLabel.setText("No Tasks");
         } else {
-            setText("Active tasks: " + statistics.activeTasks() + "\nCompleted tasks: " + statistics.completedTasks());
+            statisticsLabel.setText("Active tasks: " + statistics.activeTasks() + "\nCompleted tasks: " + statistics.completedTasks());
         }
     }
 
