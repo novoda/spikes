78083d9703913552bfca63a2726b3014dc51df09
Joe Timmins <joe@novoda.com>
2016-07-11 11:13:50 +0100

    StatisticsPresenter can operate a NavDrawer

--- Architecture/TodoApp/android/core/src/main/java/com/novoda/todoapp/statistics/presenter/StatisticsPresenter.java
+++ Architecture/TodoApp/android/core/src/main/java/com/novoda/todoapp/statistics/presenter/StatisticsPresenter.java
@@ -1,6 +1,9 @@
 package com.novoda.todoapp.statistics.presenter;
 
 import com.novoda.event.DataObserver;
+import com.novoda.todoapp.navigation.NavDrawerActionListener;
+import com.novoda.todoapp.navigation.NavDrawerDisplayer;
+import com.novoda.todoapp.navigation.Navigator;
 import com.novoda.todoapp.statistics.data.model.Statistics;
 import com.novoda.todoapp.statistics.displayer.StatisticsDisplayer;
 import com.novoda.todoapp.statistics.service.StatisticsService;
@@ -13,12 +16,31 @@ public class StatisticsPresenter {
 
     private final StatisticsService statisticsService;
     private final StatisticsDisplayer statisticsDisplayer;
+    private final NavDrawerDisplayer navDrawerDisplayer;
+    private final Navigator navigator;
 
     private CompositeSubscription subscriptions = new CompositeSubscription();
 
-    public StatisticsPresenter(StatisticsService statisticsService, StatisticsDisplayer statisticsDisplayer) {
+    final NavDrawerActionListener navDrawerActionListener = new NavDrawerActionListener() {
+        @Override
+        public void onToDoListNavDrawerItemSelected() {
+            navigator.toTasksList();
+        }
+
+        @Override
+        public void onStatisticsNavDrawerItemSelected() {
+            navDrawerDisplayer.closeNavDrawer();
+        }
+    };
+
+    public StatisticsPresenter(StatisticsService statisticsService,
+                               StatisticsDisplayer statisticsDisplayer,
+                               NavDrawerDisplayer navDrawerDisplayer,
+                               Navigator navigator) {
         this.statisticsService = statisticsService;
         this.statisticsDisplayer = statisticsDisplayer;
+        this.navDrawerDisplayer = navDrawerDisplayer;
+        this.navigator = navigator;
     }
 
     public void startPresenting() {
@@ -27,11 +49,13 @@ public class StatisticsPresenter {
                         .compose(asData(Statistics.class))
                         .subscribe(new StatisticsObserver())
         );
+        navDrawerDisplayer.attach(navDrawerActionListener);
     }
 
     public void stopPresenting() {
         subscriptions.clear();
         subscriptions = new CompositeSubscription();
+        navDrawerDisplayer.detach();
     }
 
     private class StatisticsObserver extends DataObserver<Statistics> {
--- Architecture/TodoApp/android/core/src/test/kotlin/com/novoda/todoapp/statistics/presenter/StatisticsPresenterTest.kt
+++ Architecture/TodoApp/android/core/src/test/kotlin/com/novoda/todoapp/statistics/presenter/StatisticsPresenterTest.kt
@@ -1,6 +1,8 @@
 package com.novoda.todoapp.statistics.presenter
 
 import com.novoda.event.Event
+import com.novoda.todoapp.navigation.NavDrawerDisplayer
+import com.novoda.todoapp.navigation.Navigator
 import com.novoda.todoapp.statistics.data.model.Statistics
 import com.novoda.todoapp.statistics.displayer.StatisticsDisplayer
 import com.novoda.todoapp.statistics.service.StatisticsService
@@ -20,12 +22,16 @@ class StatisticsPresenterTest {
 
     var displayer: StatisticsDisplayer = Mockito.mock(StatisticsDisplayer::class.java)
 
-    var presenter = StatisticsPresenter(service, displayer)
+    var navDrawerDisplayer: NavDrawerDisplayer = Mockito.mock(NavDrawerDisplayer::class.java)
+
+    var navigator: Navigator = Mockito.mock(Navigator::class.java)
+
+    var presenter = StatisticsPresenter(service, displayer, navDrawerDisplayer, navigator)
 
     @Before
     fun setUp() {
         setUpService()
-        presenter = StatisticsPresenter(service, displayer)
+        presenter = StatisticsPresenter(service, displayer, navDrawerDisplayer, navigator)
     }
 
     @Test
@@ -134,6 +140,42 @@ class StatisticsPresenterTest {
         statisticsEventSubject.onNext(Event.error<Statistics>(SyncError()))
 
         Mockito.verifyNoMoreInteractions(displayer)
+    }
+
+    @Test
+    fun given_ThePresenterIsNotPresenting_on_StartPresenting_it_ShouldAttachTheNavDrawerActionListener_ToTheDisplayer() {
+        givenThePresenterIsNotPresenting()
+
+        presenter.startPresenting()
+
+        Mockito.verify(navDrawerDisplayer).attach(presenter.navDrawerActionListener)
+    }
+
+    @Test
+    fun given_ThePresenterIsPresenting_on_StopPresenting_it_ShouldDetachTheNavDrawerActionListener_FromTheDisplayer() {
+        givenThePresenterIsNotPresenting()
+
+        presenter.stopPresenting()
+
+        Mockito.verify(navDrawerDisplayer).detach()
+    }
+
+    @Test
+    fun given_ThePresenterIsPresenting_on_StatisticsNavDrawerItemSelected_it_ShouldCloseTheNavDrawer() {
+        givenThePresenterIsPresenting()
+
+        presenter.navDrawerActionListener.onStatisticsNavDrawerItemSelected()
+
+        Mockito.verify(navDrawerDisplayer).closeNavDrawer()
+    }
+
+    @Test
+    fun given_ThePresenterIsPresenting_on_ToDoListNavDrawerItemSelected_it_ShouldCloseTheNavDrawer() {
+        givenThePresenterIsPresenting()
+
+        presenter.navDrawerActionListener.onToDoListNavDrawerItemSelected()
+
+        Mockito.verify(navigator).toTasksList()
     }
 
     private fun givenThePresenterIsPresenting() {
