1445ec69c1a50bd59a7646b696534f11a67ceb58
Dorvaryn <ben@novoda.com>
2016-07-12 11:02:36 +0100

    Extract filter and rename variable for clarity

--- Architecture/TodoApp/android/core/src/main/java/com/novoda/todoapp/tasks/data/model/Tasks.java
+++ Architecture/TodoApp/android/core/src/main/java/com/novoda/todoapp/tasks/data/model/Tasks.java
@@ -130,13 +130,17 @@ public abstract class Tasks {
     }
 
     public Tasks overrideWithMostRecentActionsFrom(Tasks tasks, final long actionTimestamp) {
-        ImmutableMapWithCopy<Id, SyncedData<Task>> filter = tasks.internalMap().filter(new Predicate<Map.Entry<Id, SyncedData<Task>>>() {
+        ImmutableMapWithCopy<Id, SyncedData<Task>> moreRecentActions = tasks.internalMap().filter(isMoreRecentAction(actionTimestamp));
+        return Tasks.from(internalMap().putAll(moreRecentActions));
+    }
+
+    private Predicate<Map.Entry<Id, SyncedData<Task>>> isMoreRecentAction(final long actionTimestamp) {
+        return new Predicate<Map.Entry<Id, SyncedData<Task>>>() {
             @Override
             public boolean apply(Map.Entry<Id, SyncedData<Task>> input) {
                 return input.getValue().lastSyncAction() > actionTimestamp || actionExistsAndIsOutdatedComparedTo(input.getValue());
             }
-        });
-        return Tasks.from(internalMap().putAll(filter));
+        };
     }
 
     @Override
