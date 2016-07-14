7b5ff38aee5708190394fcb44eaf9dde09e00554
Adam Brown <adam@novoda.com>
2016-07-14 09:02:30 +0100

    adding flatten example

--- slacker/app.js
+++ slacker/app.js
@@ -5,11 +5,35 @@ const GENERAL_CHANNEL_ID = 'C029J9QTH'
 
 var latest = new Date();
 var oldest = new Date();
-oldest.setMinutes(latest.getMinutes() - 30);
+oldest.setMinutes(latest.getMinutes() - 1000);
 
 var latestEpoch = latest / 1000;
 var oldestEpoch = oldest / 1000;
 
-slack.getMessages(GENERAL_CHANNEL_ID, oldestEpoch, latestEpoch, messages => {
-  console.log(messages);
-});
+
+var foo = [
+  { user: 'adam', text: 'one'},
+  { user: 'adam', text: 'two'},
+  { user: 'carl', text: 'one'},
+  { user: 'adam', text: 'three'},
+  { user: 'carl', text: 'two'}
+];
+
+//
+// slack.getMessages(GENERAL_CHANNEL_ID, oldestEpoch, latestEpoch, messages => {
+//   var result = parse(messages);
+//
+//   console.log(result);
+//
+// });
+//
+// function parse(messages) {
+//   var mapped = messages.map(each => {
+//     return {
+//       user: each.user,
+//       text: each.text,
+//       timestamp: each.ts
+//     }
+//   });
+//   return mapped;
+// }
