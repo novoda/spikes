6ee202c78db0a2960e2869d1170565efdc524454
Adam Brown <adam@novoda.com>
2016-07-15 09:08:27 +0100

    grabbing the biggest slacker for the given time period

--- slacker/app.js
+++ slacker/app.js
@@ -7,33 +7,37 @@ var latest = new Date();
 var oldest = new Date();
 oldest.setMinutes(latest.getMinutes() - 1000);
 
-var latestEpoch = latest / 1000;
-var oldestEpoch = oldest / 1000;
+getBiggestSlacker(oldest, latest, result => {
+  console.log(result);
+});
 
+function getBiggestSlacker(oldest, latest, callback) {
+  var latestEpoch = latest / 1000;
+  var oldestEpoch = oldest / 1000;
 
-var foo = [
-  { user: 'adam', text: 'one'},
-  { user: 'adam', text: 'two'},
-  { user: 'carl', text: 'one'},
-  { user: 'adam', text: 'three'},
-  { user: 'carl', text: 'two'}
-];
+  slack.getMessages(GENERAL_CHANNEL_ID, oldestEpoch, latestEpoch, messages => {
+    var result = flatten(messages);
 
-//
-// slack.getMessages(GENERAL_CHANNEL_ID, oldestEpoch, latestEpoch, messages => {
-//   var result = parse(messages);
-//
-//   console.log(result);
-//
-// });
-//
-// function parse(messages) {
-//   var mapped = messages.map(each => {
-//     return {
-//       user: each.user,
-//       text: each.text,
-//       timestamp: each.ts
-//     }
-//   });
-//   return mapped;
-// }
+    result.sort((a, b) => {
+      return b.messages.length - a.messages.length;
+    });
+    callback(result[0]);
+  });
+}
+
+function flatten(messages) {
+  var userToMessageCount = {};
+  messages.forEach(each => {
+    if(userToMessageCount[each.user]) {
+      userToMessageCount[each.user].push({ text: each.text, timestamp: each.ts} );
+    } else {
+      userToMessageCount[each.user] = [ {text: each.text, timestamp: each.ts} ];
+    }
+  });
+  return Object.keys(userToMessageCount).map(key => {
+      return {
+        user: key,
+        messages: userToMessageCount[key]
+      }
+    });
+}
