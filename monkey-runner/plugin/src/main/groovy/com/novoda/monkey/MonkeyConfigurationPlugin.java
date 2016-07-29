93d5a44f436e4aef3f95cfb083262a7c7f5c766a
Ataul Munim <hello@ataulm.com>
2016-07-29 12:11:23 +0100

    Add MonkeyConfigurationPlugin that doesn't do anything

---
+++ monkey-runner/plugin/src/main/groovy/com/novoda/monkey/MonkeyConfigurationPlugin.java
@@ -0,0 +1,12 @@
+package com.novoda.monkey;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class MonkeyConfigurationPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project target) {
+        // TODO: configure the monkey here instead of using android-monkey.gradle
+    }
+}
