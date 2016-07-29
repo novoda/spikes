0bd8e58d181d2cdad8fcee0655d557ff9df6baad
ryanbateman <ryan.bateman@gmail.com>
2016-07-29 17:10:11 +0100

    Added small check to ensure that the message contains the word 'thank' and bump timeout back up again

--- slacker/server/thanks.js
+++ slacker/server/thanks.js
@@ -4,7 +4,7 @@ module.exports = {
 
 function thanks(channelId, messages) {
   var thanksMessages = messages.filter(each => {
-    return each.channel == channelId;
+    return (each.channel == channelId && each.text.indexOf('thank') != -1);
   });
 
   return thanksMessages[0];
