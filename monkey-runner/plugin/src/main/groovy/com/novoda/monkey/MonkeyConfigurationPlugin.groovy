376845ebd2b732bf7fe104bcdb97d585e03157fe
Daniele Bonaldo <dany.bony@gmail.com>
2016-08-01 14:10:52 +0100

    Configure task after project evaluate

--- monkey-runner/plugin/src/main/groovy/com/novoda/monkey/MonkeyConfigurationPlugin.groovy
+++ monkey-runner/plugin/src/main/groovy/com/novoda/monkey/MonkeyConfigurationPlugin.groovy
@@ -1,39 +1,33 @@
 package com.novoda.monkey
 
 import org.gradle.api.GradleException
-import org.gradle.api.NamedDomainObjectFactory
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 public class MonkeyConfigurationPlugin implements Plugin<Project> {
 
-    private static final String EXTENSION_NAME_MONKEY_RUNNER_CONFIG = 'monkeyRunner'
-    public static final String PLUGIN_NAME_ANDROID = 'com.android.application'
+    private static final String EXTENSION_NAME = 'monkeyRunner'
+    private static final String TASK_NAME = 'runMonkeyAll'
 
     @Override
     public void apply(Project project) {
         ensureAndroidPluginAppliedTo(project)
-        addMonkeyRunnerConfigExtensionTo(project)
-        configureMonkeyRunner(project)
+        MonkeyRunnerExtension extension = project.extensions.create(EXTENSION_NAME, MonkeyRunnerExtension)
+
+        project.afterEvaluate {
+            configureTask(project, extension.taskDependency)
+        }
     }
 
     private static void ensureAndroidPluginAppliedTo(Project project) {
-        boolean missingAndroidPlugin = !project.plugins.hasPlugin(PLUGIN_NAME_ANDROID)
+        boolean missingAndroidPlugin = !project.plugins.hasPlugin('com.android.application')
         if (missingAndroidPlugin) {
             throw new GradleException('monkey runner plugin can only be applied after the Android plugin')
         }
     }
 
-    private void configureMonkeyRunner(Project project) {
-        { MonkeyRunnerConfig monkeyRunnerConfig ->
-            project.afterEvaluate {
-                reallyConfigureJob(project, monkeyRunnerConfig.getTaskDependency())
-            }
-        }
-    }
-
-    private void reallyConfigureJob(Project project, String taskDependency) {
-        def runMonkeyAllTask = project.task('runMonkeyAll')
+    private void configureTask(Project project, String taskDependency) {
+        def runMonkeyAllTask = project.task(TASK_NAME)
 
         def android = project.extensions.findByName("android")
         android.command.devices().eachWithIndex { device, index ->
@@ -45,7 +39,7 @@ public class MonkeyConfigurationPlugin implements Plugin<Project> {
 
             def monkeyTask = project.task("runMonkeyDevice${index}", type: TargetedMonkey, dependsOn: taskDependency) {
                 packageName = "com.novoda.monkey"
-                events = 10000
+                events = 1000
                 deviceId = device.id
                 logFileName = 'monkey.log'
                 categories = ["android.intent.category.MONKEY"]
@@ -62,26 +56,12 @@ public class MonkeyConfigurationPlugin implements Plugin<Project> {
         }
     }
 
-    private void addMonkeyRunnerConfigExtensionTo(Project project) {
-        def container = project.container(MonkeyRunnerConfig, new NamedDomainObjectFactory<MonkeyRunnerConfig>() {
-            @Override
-            MonkeyRunnerConfig create(String name) {
-                return new MonkeyRunnerConfig()
-            }
-        })
-        project.extensions.add(EXTENSION_NAME_MONKEY_RUNNER_CONFIG, container)
-    }
+    static class MonkeyRunnerExtension {
 
+        String taskDependency
 
-    static class MonkeyRunnerConfig {
-        private final String taskDependency
-
-        MonkeyRunnerConfig(String taskDependency) {
-            this.taskDependency = taskDependency
         }
 
-        String getTaskDependency() {
-            taskDependency
         }
     }
 }
