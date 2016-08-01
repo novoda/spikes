d373672841c0ce2c63db5ec4f224bf31eff159c3
Ataul Munim <hello@ataulm.com>
2016-08-01 11:11:26 +0100

    TEMP

--- monkey-runner/plugin/src/main/groovy/com/novoda/monkey/MonkeyConfigurationPlugin.groovy
+++ monkey-runner/plugin/src/main/groovy/com/novoda/monkey/MonkeyConfigurationPlugin.groovy
@@ -1,12 +1,38 @@
-package com.novoda.monkey;
+package com.novoda.monkey
 
-import org.gradle.api.Plugin;
-import org.gradle.api.Project;
+import org.gradle.api.GradleException
+import org.gradle.api.NamedDomainObjectFactory
+import org.gradle.api.Plugin
+import org.gradle.api.Project
 
 public class MonkeyConfigurationPlugin implements Plugin<Project> {
 
+    private static final String EXTENSION_NAME_MONKEY_RUNNER_CONFIG = 'monkeyRunner'
+    public static final String PLUGIN_NAME_ANDROID = 'com.android.application'
+
     @Override
     public void apply(Project project) {
+        ensureAndroidPluginAppliedTo(project)
+        addMonkeyRunnerConfigExtensionTo(project)
+        configureMonkeyRunner(project)
+    }
+
+    private static void ensureAndroidPluginAppliedTo(Project project) {
+        boolean missingAndroidPlugin = !project.plugins.hasPlugin(PLUGIN_NAME_ANDROID)
+        if (missingAndroidPlugin) {
+            throw new GradleException('monkey runner plugin can only be applied after the Android plugin')
+        }
+    }
+
+    private void configureMonkeyRunner(Project project) {
+        { MonkeyRunnerConfig monkeyRunnerConfig ->
+            project.afterEvaluate {
+                reallyConfigureJob(project, monkeyRunnerConfig.getTaskDependency())
+            }
+        }
+    }
+
+    private void reallyConfigureJob(Project project, String taskDependency) {
         def runMonkeyAllTask = project.task('runMonkeyAll')
 
         def android = project.extensions.findByName("android")
@@ -17,7 +43,7 @@ public class MonkeyConfigurationPlugin implements Plugin<Project> {
                 deviceId = device.id
             }
 
-            def monkeyTask = project.task("runMonkeyDevice${index}", type: TargetedMonkey, dependsOn: 'installDebug') {
+            def monkeyTask = project.task("runMonkeyDevice${index}", type: TargetedMonkey, dependsOn: taskDependency) {
                 packageName = "com.novoda.monkey"
                 events = 10000
                 deviceId = device.id
@@ -33,6 +59,29 @@ public class MonkeyConfigurationPlugin implements Plugin<Project> {
             monkeyTask.dependsOn showOverlayTask
             monkeyTask.finalizedBy hideOverlay
             runMonkeyAllTask.dependsOn monkeyTask
+        }
+    }
+
+    private void addMonkeyRunnerConfigExtensionTo(Project project) {
+        def container = project.container(MonkeyRunnerConfig, new NamedDomainObjectFactory<MonkeyRunnerConfig>() {
+            @Override
+            MonkeyRunnerConfig create(String name) {
+                return new MonkeyRunnerConfig()
+            }
+        })
+        project.extensions.add(EXTENSION_NAME_MONKEY_RUNNER_CONFIG, container)
+    }
+
+
+    static class MonkeyRunnerConfig {
+        private final String taskDependency
+
+        MonkeyRunnerConfig(String taskDependency) {
+            this.taskDependency = taskDependency
+        }
+
+        String getTaskDependency() {
+            taskDependency
         }
     }
 }
