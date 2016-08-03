31072de47d02fa519cc823db3b77fd5d127bc894
Adam Brown <adam@novoda.com>
2016-08-03 22:33:34 +0100

    moving thanks to the slacker dir

---
+++ slacker/server/slacker/thanks.js
@@ -0,0 +1,27 @@
+module.exports = {
+  rule: thanks
+}
+
+function thanks(dataStore, messages) {
+  var channel = dataStore.getChannelByName('thanks');
+  var thanksMessages = messages.filter(each => {
+    return (each.channel == channel.id) && (each.text.indexOf('thank') != -1);
+  });
+
+  var randomThankYou = thanksMessages.length > 0 ? thanksMessages[0] : null;
+  return {
+    thingKey: 'thanks',
+    payload: createPayload(dataStore, randomThankYou)
+  };
+}
+
+function createPayload(dataStore, thankYou) {
+  if (thankYou) {
+    return {
+      user: dataStore.getUserById(thankYou.user),
+      thanks: thankYou
+    };
+  } else {
+    return null;
+  }
+}
