3cc9fd6bf4547b1342d9187801652dc67a8fd1aa
takecare <rui@vazteixeira.org>
2016-08-16 21:44:52 +0100

    WIP

--- slacker/server/stackoverflow.js
+++ slacker/server/stackoverflow.js
@@ -2,9 +2,26 @@ module.exports = {
   rule: stackoverflow
 }
 
+var request = require('request');
+
+function fetch() {
+  console.log('... stackoverflow FETCH')
+  // request('http://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&q=novoda&accepted=False&site=stackoverflow', 
+  //         function (error, response, body) {
+  //           if (!error && response.statusCode == 200) {
+  //               console.log(body.items);
+  //               return body.items;
+  //           } else {
+  //               console.log(error);
+  //           }
+  //       })
+}
+
 function stackoverflow() {
+  console.log('... stackoverflow rule');
+  questions = fetch();
   return {
     widgetKey: 'stackoverflow',
-    payload: ' World!'
+    payload: '!!!' // TODO number of unanswered questions
   };
 }
