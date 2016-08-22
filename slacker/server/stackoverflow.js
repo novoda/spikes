1a34ce95082c2767ac80fdcac04f7a74e0226aef
takecare <rui@vazteixeira.org>
2016-08-22 23:26:54 +0100

    add missing rank and adopt new promise system of issuing the requests and passing the response back

--- slacker/server/stackoverflow.js
+++ slacker/server/stackoverflow.js
@@ -1,29 +1,45 @@
 module.exports = {
-  rule: stackoverflow
+  rule: stackoverflow,
+  rank: 1
 }
 
 var request = require('request');
-
-function fetch() {
-  console.log('... stackoverflow FETCH')
-  // request('http://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&q=novoda&accepted=False&site=stackoverflow', 
-  //         function (error, response, body) {
-  //           if (!error && response.statusCode == 200) {
-  //               console.log(body.items);
-  //               return body.items;
-  //           } else {
-  //               console.log(error);
-  //           }
-  //       })
-}
+const SO_URL = 'http://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&q=novoda&accepted=False&site=stackoverflow';
+//const SO_URL = 'http://jsonplaceholder.typicode.com/posts/1';
 
 function stackoverflow() {
-  questions = fetch();
-  let result = function(resolve, reject) {
+  return new Promise(getQuestions).then(toNumber);
+}
+
+function getQuestions(resolve, reject) {
+  let numItems = -1;
+  request.get(
+              {
+                url: SO_URL,
+                method: 'GET',
+                gzip: true,
+                headers: {
+                  'Content-Type': 'application/json'
+                }
+              }, 
+              function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                  var jsonBody = JSON.parse(body);
+                  if (jsonBody.items) {
+                    numItems = jsonBody.items.length;
+                  }
+                } else {
+                  reject(error);
+                }
+                resolve(numItems);
+              });
+}
+
+function toNumber(data) {
+  return new Promise(function(resolve, reject) {
     resolve({
       widgetKey: 'stackoverflow',
-      payload: 'CAT'
+      payload: data
     });
-  }
-  return new Promise(result);
+  });
 }
