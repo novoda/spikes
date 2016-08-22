7bd46f400de2d7bddae811175f67a75af905af94
takecare <rui@vazteixeira.org>
2016-08-22 23:42:25 +0100

    improve the widget's look

--- slacker/public/widgets/stackoverflow.js
+++ slacker/public/widgets/stackoverflow.js
@@ -3,11 +3,12 @@ if (widgets.stackoverflow) throw "Stackoverflow already exists";
 widgets.stackoverflow = function() {
   return {
     present: function(data, element) {
-      element.innerHTML = '<h1>Hello' + data.payload + '</h1>'
+      element.innerHTML = presentQuestionCount(data.payload)
     }
   };
 }
 
+// TODO @RUI link to search
 function presentQuestionCount(data) {
   return '<div class="content-container active-image">' +
   '<div class="active-container">' +
@@ -15,7 +16,7 @@ function presentQuestionCount(data) {
     '<span class="active-channel-header">' + '&#128172;' + '</span>' +
     '<br>' +
     '<div>' +
-      '<span class="active-channel-copy"><b class="active-channel-name">#' + data.project + '</b><i> has ' + data.coverage + '% coverage</i></span>' +
+      '<span class="active-channel-copy">We have ' + data + ' unanswered StackOverflow questions!</span>' +
     '</div>' +
   '</div>' +
 '</div>';
