ebbfda7227643183d50990d60dff8296fb8c3450
takecare <rui@vazteixeira.org>
2016-08-22 23:25:56 +0100

    update widget according to new display process

--- slacker/public/widgets/stackoverflow.js
+++ slacker/public/widgets/stackoverflow.js
@@ -2,9 +2,8 @@ if (widgets.stackoverflow) throw "Stackoverflow already exists";
 
 widgets.stackoverflow = function() {
   return {
-    element: document.getElementById('stackoverflow'),
-    present: function(data) {
-      this.element.innerHTML = '<h1>Hello' + data.payload + '</h1>'
+    present: function(data, element) {
+      element.innerHTML = '<h1>Hello' + data.payload + '</h1>'
     }
   };
 }
