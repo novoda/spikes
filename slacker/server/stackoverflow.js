b4a95e9189c9e3b6fc3d9f67b10460a535772d28
takecare <rui@vazteixeira.org>
2016-08-22 23:35:25 +0100

    extract functions to improve readability

--- slacker/server/stackoverflow.js
+++ slacker/server/stackoverflow.js
@@ -5,37 +5,31 @@ module.exports = {
 
 var request = require('request');
 const SO_URL = 'http://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&q=novoda&accepted=False&site=stackoverflow';
-//const SO_URL = 'http://jsonplaceholder.typicode.com/posts/1';
 
 function stackoverflow() {
-  return new Promise(getQuestions).then(toNumber);
+  return new Promise(getQuestions).then(getCardinality);
 }
 
 function getQuestions(resolve, reject) {
   let numItems = -1;
-  request.get(
-              {
-                url: SO_URL,
-                method: 'GET',
-                gzip: true,
-                headers: {
-                  'Content-Type': 'application/json'
-                }
-              }, 
-              function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                  var jsonBody = JSON.parse(body);
-                  if (jsonBody.items) {
-                    numItems = jsonBody.items.length;
-                  }
-                } else {
-                  reject(error);
-                }
-                resolve(numItems);
-              });
+  request.get({url: SO_URL, gzip: true}, parseResponse(resolve, reject));
 }
 
-function toNumber(data) {
+function parseResponse(resolve, reject) {
+  return function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var jsonBody = JSON.parse(body);
+      if (jsonBody.items) {
+        numItems = jsonBody.items.length;
+      }
+    } else {
+      reject(error);
+    }
+    resolve(numItems);
+  }
+}
+
+function getCardinality(data) {
   return new Promise(function(resolve, reject) {
     resolve({
       widgetKey: 'stackoverflow',
