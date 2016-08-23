1372b80fdbb0adeb1099b44b593aedd957b51bd7
takecare <rui@vazteixeira.org>
2016-08-23 13:16:58 +0100

    print a question after the count

--- slacker/public/widgets/stackoverflow.js
+++ slacker/public/widgets/stackoverflow.js
@@ -3,24 +3,29 @@ if (widgets.stackoverflow) throw "Stackoverflow already exists";
 widgets.stackoverflow = function() {
   return {
     present: function(data, element) {
-      element.innerHTML = presentQuestionCount(data.payload)
+      element.innerHTML = presentQuestion(data.payload)
     }
   };
 }
 
-function presentQuestionCount(data) {
+function presentQuestion(data) {
   return '<div class="content-container active-image">' +
   '<div class="active-container">' +
     '<div class="active-filler"></div>' +
     '<span class="active-channel-header">' + '&#128172;' + '</span>' +
     '<br>' +
     '<div>' +
-      '<span class="active-channel-copy">We have ' + linkify(data) + '!</span>' +
+      '<span class="active-channel-copy">We have ' + linkifyCount(data.questions) + '!</span>' +
+      '<p class="">Here\'s one: ' + linkifyQuestion(data.questions[0]) + '</p>' +
     '</div>' +
   '</div>' +
 '</div>';
 }
 
-function linkify(data) {
-    return '<a href="' + data.url + '">' + data.count + ' unanswered questions</a>'
+function linkifyCount(questions) {
+  return '<a href="' + questions.url + '">' + questions.length + ' unanswered questions</a>'
+}
+
+function linkifyQuestion(question) {
+ return '<a href="' + question.link + '">' + question.title + '</a>' 
 }
