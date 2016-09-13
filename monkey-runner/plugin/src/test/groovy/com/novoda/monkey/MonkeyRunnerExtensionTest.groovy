b8861b6eecb6af5cd9c74120bde032bfe0c104da
Andrei Catinean <andrei@novoda.com>
2016-09-13 20:32:57 +0100

    Remove irrelevant tests

--- monkey-runner/plugin/src/test/groovy/com/novoda/monkey/MonkeyRunnerExtensionTest.groovy
+++ monkey-runner/plugin/src/test/groovy/com/novoda/monkey/MonkeyRunnerExtensionTest.groovy
@@ -3,8 +3,6 @@ package com.novoda.monkey
 import org.junit.Before
 import org.junit.Test
 
-import static com.google.common.truth.Truth.assertThat
-
 class MonkeyRunnerExtensionTest {
 
     private MonkeyRunnerExtension extension
@@ -26,26 +24,5 @@ class MonkeyRunnerExtensionTest {
         extension.taskDependency = 'someTask'
 
         extension.ensureMandatoryPropertiesPresent()
-    }
-
-    @Test
-    public void defaultEventsCountIs50000() {
-        extension.setDefaultsForOptionalProperties()
-
-        assertThat(extension.eventsCount).isEqualTo(50000)
-    }
-
-    @Test
-    public void defaultLogFileNameIsMonkeyLog() {
-        extension.setDefaultsForOptionalProperties()
-
-        assertThat(extension.logFileName).isEqualTo('monkey.log')
-    }
-
-    @Test
-    public void defaultMonkeyTrapIsTrue() {
-        extension.setDefaultsForOptionalProperties()
-
-        assertThat(extension.useMonkeyTrap).isTrue()
     }
 }
