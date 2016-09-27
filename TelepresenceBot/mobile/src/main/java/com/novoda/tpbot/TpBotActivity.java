d0c7e345c46009056ecf0aa9dd93f63270e79168
Ryan Feline <ryan.feline@novoda.com>
2016-09-27 20:31:14 +0100

    Add initial project structure for tpbot

---
+++ TelepresenceBot/mobile/src/main/java/com/novoda/tpbot/TpBotActivity.java
@@ -0,0 +1,13 @@
+package com.novoda.tpbot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TpBotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tp_bot);
+    }
+}
