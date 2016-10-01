700eff9430e1307562fae8c47eeaf1fdf073423a
Antonio Bertucci <archano@gmail.com>
2016-10-01 18:07:16 +0200

    SAP: Introduce plugin module

---
+++ static-analysis-plugin/plugin/src/main/groovy/com/novoda/staticanalysis/StaticAnalysisPlugin.groovy
@@ -0,0 +1,11 @@
+package com.novoda.staticanalysis
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class StaticAnalysisPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        // TODO
+    }
+}
