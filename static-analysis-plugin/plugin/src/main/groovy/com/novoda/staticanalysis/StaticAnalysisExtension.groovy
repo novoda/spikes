e03aa4451e77ab1e2e943cfdaa8b7731c9cee69f
Antonio Bertucci <archano@gmail.com>
2016-10-02 22:05:25 +0200

    SAP: Introduce placeholder for plugin extension

---
+++ static-analysis-plugin/plugin/src/main/groovy/com/novoda/staticanalysis/StaticAnalysisExtension.groovy
@@ -0,0 +1,4 @@
+package com.novoda.staticanalysis
+
+class StaticAnalysisExtension {
+}
--- static-analysis-plugin/plugin/src/main/groovy/com/novoda/staticanalysis/StaticAnalysisPlugin.groovy
+++ static-analysis-plugin/plugin/src/main/groovy/com/novoda/staticanalysis/StaticAnalysisPlugin.groovy
@@ -6,6 +6,6 @@ import org.gradle.api.Project
 class StaticAnalysisPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
-        // TODO
+        project.extensions.create('staticAnalysis', StaticAnalysisExtension)
     }
 }
