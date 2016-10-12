4a9730143454557480b36e3e403557711d91f35d
Alex Styl <alexanderstyl@gmail.com>
2016-10-12 20:16:46 +0100

    Trigger set Live wallpaper when opening MainActivity

--- FacesofLondon/app/src/main/java/com/novoda/wallpaper/droidcon/MainActivity.java
+++ FacesofLondon/app/src/main/java/com/novoda/wallpaper/droidcon/MainActivity.java
@@ -1,13 +1,26 @@
 package com.novoda.wallpaper.droidcon;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.WallpaperManager;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        startSetLiveWallpaperIntent();
+        finish();
+    }
+
+    private void startSetLiveWallpaperIntent() {
+        Intent intent = new Intent();
+        intent.setAction(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);
+        ComponentName wallpaper = new ComponentName(getPackageName(), LondonParallaxWallpaper.class.getCanonicalName());
+        intent.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT, wallpaper);
+        startActivityForResult(intent, 0);
     }
 }
