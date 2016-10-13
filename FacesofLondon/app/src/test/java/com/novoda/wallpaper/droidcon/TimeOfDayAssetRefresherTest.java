ca28eccc68c99be6baa6ccae7ce7e7a6ff052da3
Alex Styl <alexanderstyl@gmail.com>
2016-10-13 23:49:01 +0100

    Add tests for Dawn, Day and Dusk

--- FacesofLondon/app/src/test/java/com/novoda/wallpaper/droidcon/TimeOfDayAssetRefresherTest.java
+++ FacesofLondon/app/src/test/java/com/novoda/wallpaper/droidcon/TimeOfDayAssetRefresherTest.java
@@ -9,10 +9,8 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static com.novoda.wallpaper.droidcon.TimeOfDay.NIGHT;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static com.novoda.wallpaper.droidcon.TimeOfDay.*;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TimeOfDayAssetRefresherTest {
@@ -31,6 +29,64 @@ public class TimeOfDayAssetRefresherTest {
     public void setUp() {
         refresher = new TimeOfDayAssetRefresher(mockRender, mockTimeCalculator);
     }
+
+    @Test
+    public void givenItsDawn_whenReceivingBroadcast_thenDawnAssetsAreLoaded() {
+        givenItIs(DAWN);
+
+        refresher.onReceive(ANY_CONTEXT, ANY_INTENT);
+
+        verify(mockRender).loadAssetsFor(DAWN);
+    }
+
+    @Test
+    public void givenItsDawn_whenReceivingMultipleBroadcasts_thenDawnAssetsAreLoadedOnlyOnce() {
+        givenItIs(DAWN);
+
+        refresher.onReceive(ANY_CONTEXT, ANY_INTENT);
+        refresher.onReceive(ANY_CONTEXT, ANY_INTENT);
+
+        verify(mockRender, times(1)).loadAssetsFor(DAWN);
+    }
+
+    @Test
+    public void givenItsDay_whenReceivingBroadcast_thenDayAssetsAreLoaded() {
+        givenItIs(DAY);
+
+        refresher.onReceive(ANY_CONTEXT, ANY_INTENT);
+
+        verify(mockRender).loadAssetsFor(DAY);
+    }
+
+    @Test
+    public void givenItsDay_whenReceivingMultipleBroadcasts_thenDayAssetsAreLoadedOnlyOnce() {
+        givenItIs(DAY);
+
+        refresher.onReceive(ANY_CONTEXT, ANY_INTENT);
+        refresher.onReceive(ANY_CONTEXT, ANY_INTENT);
+
+        verify(mockRender, times(1)).loadAssetsFor(DAY);
+    }
+
+    @Test
+    public void givenItsDusk_whenReceivingBroadcast_thenDuskAssetsAreLoaded() {
+        givenItIs(DUSK);
+
+        refresher.onReceive(ANY_CONTEXT, ANY_INTENT);
+
+        verify(mockRender).loadAssetsFor(DUSK);
+    }
+
+    @Test
+    public void givenItsDusk_whenReceivingMultipleBroadcasts_thenDuskAssetsAreLoadedOnlyOnce() {
+        givenItIs(DUSK);
+
+        refresher.onReceive(ANY_CONTEXT, ANY_INTENT);
+        refresher.onReceive(ANY_CONTEXT, ANY_INTENT);
+
+        verify(mockRender, times(1)).loadAssetsFor(DUSK);
+    }
+
 
     @Test
     public void givenItsNight_whenReceivingBroadcast_thenNightAssetsAreLoaded() {
