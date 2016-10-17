c42442ac308885e99e10131c13a52d7ec877d6e6
Antonio Bertucci <archano@gmail.com>
2016-10-17 21:17:54 +0200

    SAP: Introduce simple model to collect errors and warnings count

---
+++ static-analysis-plugin/plugin/src/main/groovy/com/novoda/staticanalysis/Violations.groovy
@@ -0,0 +1,37 @@
+package com.novoda.staticanalysis;
+
+class Violations {
+    private final String toolName
+    private int errors = 0
+    private int warnings = 0
+    private List<String> reports = []
+
+    Violations(String toolName) {
+        this.toolName = toolName
+    }
+
+    public String getName() {
+        return toolName
+    }
+
+    public int getErrors() {
+        errors
+    }
+
+    public int getWarnings() {
+        warnings
+    }
+
+    public void addViolations(int errors, int warnings, String reportUrl) {
+        this.errors += errors
+        this.warnings += warnings
+        if (errors > 0 || warnings > 0) {
+            reports += reportUrl
+        }
+    }
+
+    public String getMessage() {
+        "$toolName rule violations were found ($errors errors, $warnings warnings). See the reports at:\n" +
+                "${reports.collect { "- $it" }.join('\n')}"
+    }
+}
