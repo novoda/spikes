8187c73d1ed0215f47e1abe05e2fdce111e1013b
Xavi Rigau <xavi@novoda.com>
2016-11-04 18:22:20 +0100

    Use simpler approach for the list of Components
    as per @frapontillo's suggestion

--- dashboard-all-the-things/src/app/dashboard-carousel/dashboard-carousel.component.ts
+++ dashboard-all-the-things/src/app/dashboard-carousel/dashboard-carousel.component.ts
@@ -9,18 +9,18 @@ import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ConnectableObservable, Subscription, Subscriber } from 'rxjs';
 import { ConfigService } from '../config.service';
 
-const COMPONENTS = [
-  { key: "coverage", type: SonarCoverageComponent },
-  { key: "ciWall", type: ExternalUrlComponent },
-  { key: "reviews", type: ReviewComponent },
-  { key: "stackoverflow", type: StackOverflowComponent }
-];
+const COMPONENTS = {
+  coverage: SonarCoverageComponent,
+  ciWall: ExternalUrlComponent,
+  reviews: ReviewComponent,
+  stackoverflow: StackOverflowComponent
+};
 
 @Component({
   selector: 'app-dashboard-carousel',
   templateUrl: 'dashboard-carousel.component.html',
   styleUrls: ['dashboard-carousel.component.scss'],
-  entryComponents: COMPONENTS.map((elem) => elem.type)
+  entryComponents: Object['values'](COMPONENTS)
     .filter((elem, index, arr) => arr.indexOf(elem) === index)
 })
 export class DashboardCarouselComponent implements OnInit, OnDestroy {
@@ -53,16 +53,12 @@ export class DashboardCarouselComponent implements OnInit, OnDestroy {
     if (event.widgetKey === undefined) {
       return;
     }
-    const res = this.findComponentFor(event.widgetKey);
-    if (res != undefined) {
-      this.type = res.type;
+    const type = COMPONENTS[event.widgetKey];
+    if (type != undefined) {
+      this.type = type;
       this.event = event;
     }
   };
-
-  private findComponentFor(key: string) {
-    return COMPONENTS.filter((elem, index, arr) => elem.key === key)[0];
-  }
 
   ngOnDestroy(): void {
     if (!this.configSubscription.closed) {
