50d48f7b54a5232ef8f7c20577877a844fda1301
Antonio Bertucci <archano@gmail.com>
2016-11-07 16:05:43 +0100

    Add evaluator delegate to inspect xml reports

--- static-analysis-plugin/plugin/src/test/groovy/com/novoda/test/Fixtures.groovy
+++ static-analysis-plugin/plugin/src/test/groovy/com/novoda/test/Fixtures.groovy
@@ -38,6 +38,7 @@ public final class Fixtures {
         public static final File SOURCES_WITH_HIGH_VIOLATION = new File(FIXTURES_DIR, 'findbugs/high')
         public static final File SOURCES_WITH_MEDIUM_VIOLATION = new File(FIXTURES_DIR, 'findbugs/medium')
         public static final File SOURCES_WITH_LOW_VIOLATION = new File(FIXTURES_DIR, 'findbugs/low')
+        public static final File SAMPLE_REPORT = new File(FIXTURES_DIR, 'findbugs/reports/sample.xml')
     }
 
 }
