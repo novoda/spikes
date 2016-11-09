bbe5698c519f7e3ee689a7eead45b61d17d8584a
Xavi Rigau <xavi@novoda.com>
2016-11-09 19:30:54 +0000

    Remove old dashboards since it's been moved to its own repo
    Repo can be found here: https://github.com/novoda/dashboards

--- dashboard-all-the-things/src/app/dashboards/github-contributors/team-contributor/team-contributor.component.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TeamContributorComponent } from './team-contributor.component';
-
-describe('Component: TeamContributor', () => {
-  it('creates an instance', () => {
-    let component = new TeamContributorComponent();
-    expect(component).toBeTruthy();
-  });
-});
--- dashboard-all-the-things/src/app/dashboards/sonar-coverage/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './sonar-coverage.component';
