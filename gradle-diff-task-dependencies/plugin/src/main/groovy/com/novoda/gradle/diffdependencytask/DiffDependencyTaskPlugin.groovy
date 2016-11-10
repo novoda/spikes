de93a16128516dce496f6ffe9d867e4c8d7b68cc
Francesco Pontillo <francescopontillo@gmail.com>
2016-11-10 10:18:49 +0000

    Remove Constants class

--- gradle-diff-task-dependencies/plugin/src/main/groovy/com/novoda/gradle/diffdependencytask/DiffDependencyTaskPlugin.groovy
+++ gradle-diff-task-dependencies/plugin/src/main/groovy/com/novoda/gradle/diffdependencytask/DiffDependencyTaskPlugin.groovy
@@ -6,21 +6,23 @@ import org.gradle.api.Project
 
 class DiffDependencyTaskPlugin implements Plugin<Project> {
 
+    private static String TASK_CONFIG_PROPERTY_NAME = 'diffDependency'
+
     @Override
     void apply(Project project) {
 
         ExtensionRegister.INSTANCE.run()
 
-        project.extensions.add(Constants.TASK_CONFIG_PROPERTY_NAME, new DiffDependencyTaskConfig())
+        project.extensions.add(TASK_CONFIG_PROPERTY_NAME, new DiffDependencyTaskConfig())
 
         project.extensions.add('registerConditionalDependency', { ConditionalDependency conditionalDependency ->
-            def config = project.extensions.getByName(Constants.TASK_CONFIG_PROPERTY_NAME) as DiffDependencyTaskConfig
+            def config = project.extensions.getByName(TASK_CONFIG_PROPERTY_NAME) as DiffDependencyTaskConfig
             config.conditionalDependencyRepository.add(conditionalDependency)
             project
         })
 
         project.afterEvaluate {
-            def config = project.extensions.getByName(Constants.TASK_CONFIG_PROPERTY_NAME) as DiffDependencyTaskConfig
+            def config = project.extensions.getByName(TASK_CONFIG_PROPERTY_NAME) as DiffDependencyTaskConfig
             def evaluator = new ConditionalDependencyEvaluator(
                     config.changedFilesProvider,
                     config.conditionalDependencyRepository
