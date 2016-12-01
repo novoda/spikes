9390ede3fe972b1a37d031872b999ce0cd299ce7
Ryan Feline <ryan.feline@novoda.com>
2016-12-01 09:23:05 +0000

    Add optional.

---
+++ TelepresenceBot/mobile/src/main/java/com/novoda/tpbot/support/Optional.java
@@ -0,0 +1,54 @@
+package com.novoda.tpbot.support;
+
+public final class Optional<T> {
+
+    private static final Optional ABSENT = new Optional(null);
+
+    private final T data;
+
+    public static <T> Optional<T> absent() {
+        return ABSENT;
+    }
+
+    public static <T> Optional<T> of(T data) {
+        if (data == null) {
+            throw new IllegalArgumentException("Data cannot be null.");
+        }
+        return new Optional<>(data);
+    }
+
+    private Optional(T data) {
+        this.data = data;
+    }
+
+    public boolean isPresent() {
+        return data != null;
+    }
+
+    public T get() {
+        if (!isPresent()) {
+            throw new IllegalStateException("You must check if data is present before using get()");
+        }
+        return data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Optional<?> optional = (Optional<?>) o;
+
+        return data != null ? data.equals(optional.data) : optional.data == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return data != null ? data.hashCode() : 0;
+    }
+
+}
