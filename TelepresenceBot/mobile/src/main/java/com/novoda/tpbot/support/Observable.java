b73c369a7f269c6aa6f2772905b65421a5ed1a99
Ryan Feline <ryan.feline@novoda.com>
2016-12-07 14:40:45 +0000

    Add primitive Observer / Observable.

---
+++ TelepresenceBot/mobile/src/main/java/com/novoda/tpbot/support/Observable.java
@@ -0,0 +1,74 @@
+package com.novoda.tpbot.support;
+
+import com.novoda.notils.exception.DeveloperError;
+
+import java.util.ArrayList;
+
+public abstract class Observable<T> {
+
+    private boolean changed = false;
+    private final ArrayList<Observer<T>> observers;
+
+    public Observable() {
+        observers = new ArrayList<>();
+    }
+
+    public synchronized Observable<T> addObserver(Observer<T> o) {
+        if (o == null) {
+            throw new DeveloperError("Did you forget to add an observer for this observable?");
+        }
+        if (!observers.contains(o)) {
+            observers.add(o);
+        }
+        start();
+        return this;
+    }
+
+    public synchronized void deleteObserver(Observer o) {
+        observers.remove(o);
+    }
+
+    public void notifyObservers(T arg) {
+        Observer<T>[] observersCopy;
+
+        synchronized (this) {
+            if (hasNotChanged()) {
+                return;
+            }
+
+            observersCopy = observers.toArray(new Observer[observers.size()]);
+            clearChanged();
+        }
+
+        for (int i = observersCopy.length - 1; i >= 0; i--) {
+            observersCopy[i].update(arg);
+        }
+    }
+
+    public synchronized boolean hasChanged() {
+        return changed;
+    }
+
+    public synchronized void deleteObservers() {
+        observers.clear();
+    }
+
+    private synchronized void setChanged() {
+        changed = true;
+    }
+
+    private synchronized void clearChanged() {
+        changed = false;
+    }
+
+    private synchronized boolean hasNotChanged() {
+        return !changed;
+    }
+
+    public synchronized int countObservers() {
+        return observers.size();
+    }
+
+    public abstract void start();
+
+}
