3a58c1d2aad7af1d357d8d00227b289609e41f6f
Ryan Feline <ryan.feline@novoda.com>
2016-12-08 19:24:32 +0000

    Add remaining tests for directions.

--- TelepresenceBot/core/src/test/java/com/novoda/tpbot/bot/BotPresenterTest.java
+++ TelepresenceBot/core/src/test/java/com/novoda/tpbot/bot/BotPresenterTest.java
@@ -1,5 +1,6 @@
 package com.novoda.tpbot.bot;
 
+import com.novoda.tpbot.Direction;
 import com.novoda.tpbot.Result;
 import com.novoda.tpbot.support.Observable;
 
@@ -7,6 +8,7 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
@@ -17,6 +19,7 @@ public class BotPresenterTest {
 
     private static final Result SUCCESS_RESULT = Result.from("Connection Successful!");
     private static final Result FAILURE_RESULT = Result.from(new Exception("Connection Unsuccessful"));
+    private static final Direction DIRECTION = Direction.FORWARD;
 
     @Rule
     public MockitoRule mockitoRule = MockitoJUnit.rule();
@@ -26,6 +29,7 @@ public class BotPresenterTest {
 
     @Mock
     BotView botView;
+
     private BotPresenter presenter;
 
     @Before
@@ -53,20 +57,45 @@ public class BotPresenterTest {
 
     @Test
     public void givenAlreadyPresenting_whenStopPresentingIsCalled_thenTpServiceDisconnectIsCalled() {
-        BotPresenter presenter = givenAlreadyPresenting();
+        when(tpService.connect()).thenReturn(Observable.just(SUCCESS_RESULT));
+        presenter.startPresenting();
 
         presenter.stopPresenting();
 
         verify(tpService).disconnect();
     }
 
-    private BotPresenter givenAlreadyPresenting() {
-        when(tpService.connect()).thenReturn(Observable.just(SUCCESS_RESULT));
-
-        BotPresenter presenter = new BotPresenter(tpService, botView);
+    @Test
+    public void givenAlreadyPresenting_whenStopPresentingIsCalled_thenConnectionObservableObserversAreDetached() {
+        Observable<Result> observable = Observable.just(SUCCESS_RESULT);
+        Observable<Result> spyObservable = Mockito.spy(observable);
+        when(tpService.connect()).thenReturn(spyObservable);
         presenter.startPresenting();
 
-        return presenter;
+        presenter.stopPresenting();
+
+        verify(spyObservable).detachObservers();
+    }
+
+    @Test
+    public void givenDirection_whenStartListeningForDirectionIsCalled_thenTpServiceMoveInIsCalled() {
+        when(tpService.listen()).thenReturn(Observable.just(DIRECTION));
+
+        presenter.startListeningForDirection();
+
+        verify(botView).moveIn(DIRECTION);
+    }
+
+    @Test
+    public void givenStartedListeningForDirections_whenStopPresentingIsCalled_thenDirectionObservableObserversAreDetached() {
+        Observable<Direction> observable = Observable.just(DIRECTION);
+        Observable<Direction> spyObservable = Mockito.spy(observable);
+        when(tpService.listen()).thenReturn(spyObservable);
+        presenter.startListeningForDirection();
+
+        presenter.stopPresenting();
+
+        verify(spyObservable).detachObservers();
     }
 
 }
