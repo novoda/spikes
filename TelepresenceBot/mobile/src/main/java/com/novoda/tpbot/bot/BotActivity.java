e3b251b6792cb6ca1f4f196a33bf0192dad0eb9f
Daniele Bonaldo <dany.bony@gmail.com>
2016-12-16 10:14:09 +0100

    Show toast with device name, vendor id and product id
    For every connected USB device, when selecting the item menu

--- TelepresenceBot/mobile/src/main/java/com/novoda/tpbot/bot/BotActivity.java
+++ TelepresenceBot/mobile/src/main/java/com/novoda/tpbot/bot/BotActivity.java
@@ -1,5 +1,8 @@
 package com.novoda.tpbot.bot;
 
+import android.content.Context;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,10 +13,12 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.novoda.notils.caster.Views;
+import com.novoda.notils.logger.toast.Toaster;
 import com.novoda.tpbot.Direction;
 import com.novoda.tpbot.R;
 
 import java.util.Collections;
+import java.util.HashMap;
 
 public class BotActivity extends AppCompatActivity {
 
@@ -39,4 +44,34 @@ public class BotActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.usb_devices_list_menu_item:
+                showConnectedDevices();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void showConnectedDevices() {
+        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        HashMap<String, UsbDevice> devices = manager.getDeviceList();
+        StringBuilder builder = new StringBuilder();
+        if (devices.isEmpty()) {
+            builder.append(getString(R.string.no_connected_devices));
+        } else {
+            for (UsbDevice device : devices.values()) {
+                builder.append(
+                        getString(R.string.usb_device_name_vendor_product,
+                                  device.getDeviceName(),
+                                  device.getVendorId(),
+                                  device.getProductId()
+                        )
+                );
+            }
+        }
+        Toaster.newInstance(this).popBurntToast(builder.toString());
+    }
 }
