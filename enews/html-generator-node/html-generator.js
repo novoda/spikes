e49c42584a53287050c1e72c886aea2433828d53
Adam Brown <adam@novoda.com>
2016-12-21 20:58:34 +0000

    tidying up the loading of partials and the model -
    starts a convention

--- enews/html-generator-node/html-generator.js
+++ enews/html-generator-node/html-generator.js
@@ -7,25 +7,29 @@ const Mustache = require('mustache');
 const fs = require('fs');
 
 function generateHtml(eNews) {
-  const title = fs.readFileSync('view/partial/title.mst', 'utf-8');
-  const textItem = fs.readFileSync('view/partial/text-item.mst', 'utf-8');
-  const imageItem = fs.readFileSync('view/partial/image-item.mst', 'utf-8');
-  const item = fs.readFileSync('view/partial/item.mst', 'utf-8');
-
   const template = fs.readFileSync('view/enews.mst', 'utf-8');
-  const formattedTodaysDate = moment().format('YYYY-MM-DD');
+  const partials = loadPartials();
+  const model = createModel(eNews);
+  return Mustache.render(template, model, partials);
+};
 
-  const model = {
+function loadPartials() {
+  return {
+    title: readPartial('title.mst'),
+    textItem: readPartial('text-item.mst'),
+    imageItem: readPartial('image-item.mst'),
+    item: readPartial('item.mst')
+  }
+}
+
+function readPartial(name) {
+  return fs.readFileSync(`view/partial/${name}` , 'utf-8');
+}
+
+function createModel(eNews) {
+  const formattedTodaysDate = moment().format('YYYY-MM-DD');
+  return {
     formattedTodaysDate: formattedTodaysDate,
     eNews: eNews
   };
-
-  const partials = {
-    title: title,
-    textItem: textItem,
-    imageItem: imageItem,
-    item: item
-  }
-
-  return Mustache.render(template, model, partials);
-};
+}
