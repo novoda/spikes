d4d972294af0d2e0f116f154202361f0027309ab
Eduard Bolos <eduard@novoda.com>
2016-12-31 00:39:18 +0100

    Create project

---
+++ simon-says-android-things/app/src/main/java/com/novoda/simonsaysandroidthings/HomeActivity.java
@@ -0,0 +1,12 @@
+package com.novoda.simonsaysandroidthings;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
