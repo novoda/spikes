84f9667d370a58097e0f932251bb03a741232eff
Ataul Munim <hello@ataulm.com>
2017-01-14 00:46:45 +0100

    Add TalkBack Toggler to prevent all other services being disabled

--- talkback-toggle/core/src/main/java/com/novoda/toggletalkback/TalkBackStateSettingActivity.java
+++ talkback-toggle/core/src/main/java/com/novoda/toggletalkback/TalkBackStateSettingActivity.java
@@ -1,19 +1,17 @@
 package com.novoda.toggletalkback;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
 public class TalkBackStateSettingActivity extends AppCompatActivity {
 
     static final String ACTION_ENABLE_TALKBACK = "com.novoda.toggletalkback.ENABLE_TALKBACK";
     static final String ACTION_DISABLE_TALKBACK = "com.novoda.toggletalkback.DISABLE_TALKBACK";
-
-    private static final String TALKBACK_SERVICE_NAME = "com.google.android.marvin.talkback/.TalkBackService";
-    private static final String VALUE_DISABLED = "0";
-    private static final String VALUE_ENABLED = "1";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,30 +23,33 @@ public class TalkBackStateSettingActivity extends AppCompatActivity {
             return;
         }
 
+        Toggler toggler = createTalkBackToggler();
+
         if (ACTION_ENABLE_TALKBACK.equals(intent.getAction())) {
-            updateTalkBackState(true);
+            toggler.enableTalkBack();
         } else if (ACTION_DISABLE_TALKBACK.equals(intent.getAction())) {
-            updateTalkBackState(false);
+            toggler.disableTalkBack();
         }
 
         finish();
     }
 
-    private void updateTalkBackState(boolean enableTalkBack) {
-        if (enableTalkBack) {
-            enableAccessibilityService(TALKBACK_SERVICE_NAME);
-        } else {
-            disableAccessibilityServices();
-        }
+    private Toggler createTalkBackToggler() {
+        ContentResolver contentResolver = getContentResolver();
+        Toggler.Callback togglerCallback = createTogglerCallback();
+
+        return new Toggler(contentResolver, togglerCallback);
     }
 
-    private void enableAccessibilityService(String name) {
-        Settings.Secure.putString(getContentResolver(), Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES, name);
-        Settings.Secure.putString(getContentResolver(), Settings.Secure.ACCESSIBILITY_ENABLED, VALUE_ENABLED);
+    private Toggler.Callback createTogglerCallback() {
+        return new Toggler.Callback() {
+            @Override
+            public void onSecurityExceptionThrown() {
+                String message = "Need to grant permission. Check README.md";
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                Log.w("!!!", message);
+            }
+        };
     }
 
-    private void disableAccessibilityServices() {
-        Settings.Secure.putString(getContentResolver(), Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES, "");
-        Settings.Secure.putString(getContentResolver(), Settings.Secure.ACCESSIBILITY_ENABLED, VALUE_DISABLED);
-    }
 }
