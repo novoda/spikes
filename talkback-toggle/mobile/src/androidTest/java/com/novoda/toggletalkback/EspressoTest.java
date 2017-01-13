7f7226ce47c64663eee7168a54c384aff89a4804
Ataul Munim <hello@ataulm.com>
2017-01-13 11:30:31 +0100

    Add espresso tests for movie item view

--- talkback-toggle/mobile/src/androidTest/java/com/novoda/toggletalkback/EspressoTest.java
+++ talkback-toggle/mobile/src/androidTest/java/com/novoda/toggletalkback/EspressoTest.java
@@ -9,8 +9,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
@@ -18,11 +19,31 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 public class EspressoTest {
 
     @Rule
-    public ActivityTestRule<MyActivity> activityRule = new ActivityTestRule<>(MyActivity.class);
+    public ActivityTestRule<MovieItemViewActivity> activityRule = new ActivityTestRule<>(MovieItemViewActivity.class);
 
     @Test
-    public void displaysAppBar() {
-        onView(withText(R.string.app_name)).check(matches(isDisplayed()));
+    public void clickMovieItemView() {
+        onView(withId(R.id.movie_item_view)).perform(click());
+
+        checkReactionMatches("onClick Edward Scissorhands");
+    }
+
+    @Test
+    public void clickMovieFavoriteView() {
+        onView(withId(R.id.movie_item_button_favorite)).perform(click());
+
+        checkReactionMatches("onClickFavorite Edward Scissorhands");
+    }
+
+    @Test
+    public void clickMoviePlayView() {
+        onView(withId(R.id.movie_item_button_play)).perform(click());
+
+        checkReactionMatches("onClickPlay Edward Scissorhands");
+    }
+
+    private void checkReactionMatches(String text) {
+        onView(withId(R.id.reaction_text_view)).check(matches(withText(text)));
     }
 
 }
