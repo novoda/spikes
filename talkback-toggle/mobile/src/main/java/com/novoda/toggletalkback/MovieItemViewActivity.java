3f80e1997a7a19c2afa1584af5b3a3871a267032
Ataul Munim <hello@ataulm.com>
2017-01-13 11:30:19 +0100

    Replace toast with a textview
    - difficult to assert against toast in a series of Espresso tests

--- talkback-toggle/mobile/src/main/java/com/novoda/toggletalkback/MovieItemViewActivity.java
+++ talkback-toggle/mobile/src/main/java/com/novoda/toggletalkback/MovieItemViewActivity.java
@@ -2,47 +2,37 @@ package com.novoda.toggletalkback;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import butterknife.ButterKnife;
 
 public class MovieItemViewActivity extends AppCompatActivity {
-
-    private Toast toast;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_item_view);
         MovieItemView movieItemView = ButterKnife.findById(this, R.id.movie_item_view);
+        final TextView reactionTextView = ButterKnife.findById(this, R.id.reaction_text_view);
 
         movieItemView.attachListener(new MovieItemView.Listener() {
             @Override
             public void onClick(Movie movie) {
-                toast("onClick " + movie.name);
+                reactionTextView.setText("onClick " + movie.name);
             }
 
             @Override
             public void onClickPlay(Movie movie) {
-                toast("onClickPlay " + movie.name);
+                reactionTextView.setText("onClickPlay " + movie.name);
             }
 
             @Override
             public void onClickFavorite(Movie movie) {
-                toast("onClickFavourite " + movie.name);
+                reactionTextView.setText("onClickFavorite " + movie.name);
             }
         });
 
         movieItemView.bind(new Movie("Edward Scissorhands"));
-    }
-
-    private void toast(String message) {
-        if (toast != null) {
-            toast.cancel();
-        }
-        toast = Toast.makeText(this, message, Toast.LENGTH_LONG);
-        toast.show();
     }
 
 }
