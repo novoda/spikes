4e121c06d617a765a2fe657d49069d5b3ebc7264
Tobias Heine <tobias@novoda.com>
2017-01-16 14:59:43 +0100

    Remove not needed loop

--- gradle-nonnull-plugin/plugin/src/main/groovy/com.novoda.gradle.nonnull/AndroidNonNullPlugin.groovy
+++ gradle-nonnull-plugin/plugin/src/main/groovy/com.novoda.gradle.nonnull/AndroidNonNullPlugin.groovy
@@ -5,7 +5,6 @@ import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.tasks.StopExecutionException
 import org.gradle.api.tasks.compile.JavaCompile
-import org.gradle.plugins.ide.idea.IdeaPlugin
 
 public class AndroidNonNullPlugin implements Plugin<Project> {
 
@@ -79,24 +78,23 @@ public class AndroidNonNullPlugin implements Plugin<Project> {
 
         project.sourceSets.all { sourceSet ->
             def generatedSourcesDir = new File("${project.buildDir}/generated/source/nonNull/${sourceSet.name}")
-            project.plugins.withType(IdeaPlugin) {
-                project.afterEvaluate {
-                    project.idea.module {
-                        def ancestors = getAncestors(generatedSourcesDir, project.projectDir)
 
-                        if (ancestors.contains(project.buildDir) && excludeDirs.contains(project.buildDir)) {
-                            excludeDirs -= project.buildDir
-                            // Race condition: many of these will actually be created afterwards…
-                            def subdirs = project.buildDir.listFiles({ f -> f.directory } as FileFilter)
-                            if (subdirs != null) {
-                                excludeDirs += subdirs as List
-                            }
+            project.afterEvaluate {
+                project.idea.module {
+                    def ancestors = getAncestors(generatedSourcesDir, project.projectDir)
+
+                    if (ancestors.contains(project.buildDir) && excludeDirs.contains(project.buildDir)) {
+                        excludeDirs -= project.buildDir
+                        // Race condition: many of these will actually be created afterwards…
+                        def subdirs = project.buildDir.listFiles({ f -> f.directory } as FileFilter)
+                        if (subdirs != null) {
+                            excludeDirs += subdirs as List
                         }
-                        excludeDirs -= ancestors
-
-                        sourceDirs += generatedSourcesDir
-                        generatedSourceDirs += generatedSourcesDir
                     }
+                    excludeDirs -= ancestors
+
+                    sourceDirs += generatedSourcesDir
+                    generatedSourceDirs += generatedSourcesDir
                 }
             }
         }
