dc4e59eef176435e7a6b09e56e139793d7806b40
Said Tahsin Dane <tahsin@novoda.com>
2017-01-17 12:37:18 +0100

    Remove return guards and have switch style ifs

--- gradle-nonnull-plugin/plugin/src/main/groovy/com.novoda.gradle.nonnull/AndroidNonNullPlugin.groovy
+++ gradle-nonnull-plugin/plugin/src/main/groovy/com.novoda.gradle.nonnull/AndroidNonNullPlugin.groovy
@@ -10,15 +10,11 @@ public class AndroidNonNullPlugin implements Plugin<Project> {
 
         if (project.plugins.hasPlugin("com.android.application")) {
             applyAndroid(project, project.android.applicationVariants)
-            return
-        }
-
-        if (project.plugins.hasPlugin("com.android.library")) {
+        } else if (project.plugins.hasPlugin("com.android.library")) {
             applyAndroid(project, project.android.libraryVariants)
-            return
+        } else {
+            throw new StopExecutionException("The 'android' plugin is required.")
         }
-
-        throw new StopExecutionException("The 'android' plugin is required.")
     }
 
     private static void applyAndroid(project, variants) {
