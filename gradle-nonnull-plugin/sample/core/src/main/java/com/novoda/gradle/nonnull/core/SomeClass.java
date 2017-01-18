6a6f8d36b67a78b358435f525fc8f883375c7c36
Said Tahsin Dane <tahsin@novoda.com>
2017-01-18 08:30:01 +0100

    Comment moved to the right place

--- gradle-nonnull-plugin/sample/core/src/main/java/com/novoda/gradle/nonnull/core/SomeClass.java
+++ gradle-nonnull-plugin/sample/core/src/main/java/com/novoda/gradle/nonnull/core/SomeClass.java
@@ -3,10 +3,11 @@ package com.novoda.gradle.nonnull.core;
 public class SomeClass {
 
     public SomeClass() {
+
+        // this should result in a warning in the IDEA
         setValue(null);
     }
 
-    // this should result in a warning in the IDEA
     private void setValue(Object value){
         //no-op
     }
