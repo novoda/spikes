3219d01d4024e9259657d7b64cefea743309e9c9
Ryan Feline <ryan.feline@novoda.com>
2017-01-25 16:37:25 +0000

    Create Bot specific TpService.

--- TelepresenceBot/mobile/src/main/java/com/novoda/tpbot/bot/SocketIOTpService.java
+++ TelepresenceBot/mobile/src/main/java/com/novoda/tpbot/bot/SocketIOTpService.java
@@ -5,25 +5,20 @@ import android.os.Looper;
 
 import com.novoda.tpbot.Direction;
 import com.novoda.tpbot.Result;
+import com.novoda.tpbot.support.Event;
 import com.novoda.tpbot.support.MalformedServerAddressException;
 import com.novoda.tpbot.support.Observable;
+import com.novoda.tpbot.support.SocketConnectionObservable;
 
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 
-import io.socket.client.Ack;
 import io.socket.client.IO;
 import io.socket.client.Socket;
-import io.socket.client.SocketIOException;
-
-import static io.socket.client.Socket.EVENT_CONNECT_ERROR;
-import static io.socket.emitter.Emitter.Listener;
+import io.socket.emitter.Emitter;
 
 class SocketIOTpService implements BotTpService {
-
-    private static final String EVENT_CONNECT = "join_as_human";
-    private static final String EVENT_MOVE = "move_in";
 
     private Socket socket;
     private final Handler handler;
@@ -45,56 +40,40 @@ class SocketIOTpService implements BotTpService {
             MalformedServerAddressException exceptionWithUserFacingMessage = new MalformedServerAddressException(exception);
             return Observable.just(Result.from(exceptionWithUserFacingMessage));
         }
-        return new SocketConnectionObservable();
+        return new SocketConnectionObservable(socket, handler, Event.JOIN_BOT);
     }
 
-    private class SocketConnectionObservable extends Observable<Result> {
+    @Override
+    public Observable<Direction> listen() {
+        return new DirectionObservable();
+    }
+
+    private class DirectionObservable extends Observable<Direction> {
 
         @Override
-        public Observable<Result> start() {
-            if (socket.connected()) {
-                notifyOf(Result.from("Already connected"));
+        public Observable<Direction> start() {
+            if (!socket.connected()) {
                 return this;
             }
 
-            socket.emit(EVENT_CONNECT, "", joinSuccessAcknowledgementListener);
-            socket.on(EVENT_CONNECT_ERROR, connectionTimeoutListener);
-            socket.connect();
+            socket.on(Event.DIRECTION.rawEvent(), directionListener);
             return this;
         }
 
-        private final Ack joinSuccessAcknowledgementListener = new Ack() {
+        private final Emitter.Listener directionListener = new Emitter.Listener() {
             @Override
             public void call(final Object... args) {
                 handler.post(new Runnable() {
                     @Override
                     public void run() {
                         if (args[0] != null && args[0] instanceof String) {
-                            String acknowledgment = (String) args[0];
-                            notifyOf(Result.from(acknowledgment));
+                            String rawDirection = (String) args[0];
+                            notifyOf(Direction.from(rawDirection));
                         }
                     }
                 });
             }
         };
-
-        private final Listener connectionTimeoutListener = new Listener() {
-            @Override
-            public void call(Object... args) {
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        notifyOf(Result.from(new SocketIOException("connection timeout")));
-                    }
-                });
-            }
-        };
-
-    }
-
-    @Override
-    public Observable<Direction> listen() {
-        return null;
     }
 
     @Override
