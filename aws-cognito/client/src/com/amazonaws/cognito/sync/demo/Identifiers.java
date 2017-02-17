b48f6faafb096b01817eca4c7964fe177f425d9c
Volker Leck <volker.leck@gmail.com>
2017-02-17 17:29:05 +0100

    PT-444: inline SharedPreferencesWrapper functionality

--- aws-cognito/client/src/com/amazonaws/cognito/sync/demo/Identifiers.java
+++ aws-cognito/client/src/com/amazonaws/cognito/sync/demo/Identifiers.java
@@ -2,19 +2,28 @@ package com.amazonaws.cognito.sync.demo;
 
 import android.content.SharedPreferences;
 
-import com.amazonaws.cognito.sync.demo.client.SharedPreferencesWrapper;
-
 import java.security.SecureRandom;
 
 import org.apache.commons.codec.binary.Hex;
 
 public class Identifiers {
 
+    private static final String AWS_DEVICE_UID = "AWS_DEVICE_UID";
+    private static final String AWS_DEVICE_KEY = "AWS_DEVICE_KEY";
+    private static final String FIREBASE_TOKEN = "FIREBASE_TOKEN";
+    private static final String USER_NAME = "USER_NAME";
+
     private final SharedPreferences preferences;
 
     public Identifiers(SharedPreferences preferences) {
         this.preferences = preferences;
-        registerDevice(generateUniqueDeviceId());
+        ensureUDId();
+    }
+
+    private void ensureUDId() {
+        if (getUidForDevice() == null) {
+            registerDevice(generateUniqueDeviceId());
+        }
     }
 
     private static String generateUniqueDeviceId() {
@@ -22,31 +31,42 @@ public class Identifiers {
         return new String(Hex.encodeHex(randomBytes));
     }
 
-    public void registerFirebaseToken(final String result) {
-        SharedPreferencesWrapper.registerFirebaseToken(preferences, result);
-    }
-
     public String getUidForDevice() {
-        return SharedPreferencesWrapper.getUidForDevice(preferences);
+        return preferences.getString(AWS_DEVICE_UID, null);
     }
 
     public String getKeyForDevice() {
-        return SharedPreferencesWrapper.getKeyForDevice(preferences);
-    }
-
-    public void registerUser(final String userName, final String deviceKey) {
-        SharedPreferencesWrapper.registerUser(preferences, userName, deviceKey);
-    }
-
-    private void registerDevice(final String uniqueDeviceId) {
-        SharedPreferencesWrapper.registerDevice(preferences, uniqueDeviceId);
-    }
-
-    public void wipe() {
-        SharedPreferencesWrapper.wipe(preferences);
+        return preferences.getString(AWS_DEVICE_KEY, null);
     }
 
     public String getUserName() {
-        return SharedPreferencesWrapper.getUserName(preferences);
+        return preferences.getString(USER_NAME, null);
     }
+
+    public void registerFirebaseToken(final String result) {
+        preferences.edit()
+                .putString(FIREBASE_TOKEN, result)
+                .apply();
+    }
+
+    public void registerUser(final String userName, final String deviceKey) {
+        preferences.edit()
+                .putString(USER_NAME, userName)
+                .putString(AWS_DEVICE_KEY, deviceKey)
+                .apply();
+    }
+
+    private void registerDevice(final String uniqueDeviceId) {
+        preferences.edit()
+                .putString(AWS_DEVICE_UID, uniqueDeviceId)
+                .apply();
+    }
+
+    public void wipe() {
+        preferences.edit()
+                .remove(AWS_DEVICE_KEY)
+                .remove(FIREBASE_TOKEN)
+                .apply();
+    }
+
 }
