a4b9df8669957f6b366563f95f8ea39d07b835b2
Ataul Munim <hello@ataulm.com>
2017-03-11 14:09:14 +0000

    Extract TalkBackStateSettingRequester
    - this'll allow people to use the logic in their custom rules
    - could potentially drop the rule from this repo, and move it to espresso-support
    - then this repo is just the activity and the requester, no mention of espresso

--- talkback-toggle/espresso/src/main/java/com/novoda/talkbacktoggle/TalkBackActivityTestRule.java
+++ talkback-toggle/espresso/src/main/java/com/novoda/talkbacktoggle/TalkBackActivityTestRule.java
@@ -1,38 +1,27 @@
 package com.novoda.talkbacktoggle;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.os.SystemClock;
 import android.support.test.rule.ActivityTestRule;
 
 public class TalkBackActivityTestRule<T extends Activity> extends ActivityTestRule<T> {
 
-    private static final String ENABLE_TALKBACK = "com.novoda.talkbacktoggle.ENABLE_TALKBACK";
-    private static final String DISABLE_TALKBACK = "com.novoda.talkbacktoggle.DISABLE_TALKBACK";
+    private final TalkBackStateSettingRequester talkBackStateSettingRequester;
 
     public TalkBackActivityTestRule(Class<T> activityClass) {
         super(activityClass);
+        talkBackStateSettingRequester = new TalkBackStateSettingRequester();
     }
 
     @Override
     protected void afterActivityLaunched() {
         super.afterActivityLaunched();
-        enableTalkBack(true);
+        talkBackStateSettingRequester.requestEnableTalkBack(getActivity());
     }
 
     @Override
     protected void afterActivityFinished() {
         super.afterActivityFinished();
-        enableTalkBack(false);
+        talkBackStateSettingRequester.requestDisableTalkBack(getActivity());
     }
 
-    private void enableTalkBack(boolean enable) {
-        String action = enable ? ENABLE_TALKBACK : DISABLE_TALKBACK;
-        getActivity().startActivity(new Intent(action));
-        sleepToAllowTalkBackServiceToChangeState();
-    }
-
-    private void sleepToAllowTalkBackServiceToChangeState() {
-        SystemClock.sleep(1500);
-    }
 }
