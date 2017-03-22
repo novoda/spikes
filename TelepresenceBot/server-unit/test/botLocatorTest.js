2c0e7c33680339a71f63569effb69192628c8592
Ryan Feline <ryan.feline@novoda.com>
2017-03-22 14:59:32 +0000

    Add edge case tests to botLocator.

--- TelepresenceBot/server-unit/test/botLocatorTest.js
+++ TelepresenceBot/server-unit/test/botLocatorTest.js
@@ -2,7 +2,7 @@ var should = require('should');
 var test = require('unit.js');
 var BotLocator = require("../core/botLocator.js");
 
-var botWithNoOtherSocketsInRoom = {
+var roomWithASingleBot = {
     'botId': {
         sockets: {'botId':true},
         length:1
@@ -13,10 +13,36 @@ var botWithNoOtherSocketsInRoom = {
     }
 };
 
+var roomWhereBotIsConnectedToHuman = {
+    'botId': {
+        sockets: {'botId':true, 'humanId':true},
+        length:2
+    },
+    'London': {
+        sockets: {'botId':true},
+        length:1
+    }
+};
+
+var roomContainingMultipleBotsWhereOneIsConnectedToHuman = {
+    'botId01': {
+        sockets: {'botId01':true, 'human01':true},
+        length:2
+    },
+    'botId02': {
+        sockets: {'botId02':true},
+        length:1
+    },
+    'London': {
+        sockets: {'botId01':true, 'botId02':true},
+        length:2
+    }
+};
+
 describe("BotLocator ",function() {
 
     it('Should give undefined when bot is not found in given room.', function(done){
-        var botLocator = new BotLocator(botWithNoOtherSocketsInRoom);
+        var botLocator = new BotLocator(roomWithASingleBot);
 
         var bot = botLocator.locateFirstAvailableBotIn("Unexpected Room");
 
@@ -26,13 +52,35 @@ describe("BotLocator ",function() {
         done();
     });
 
-    it('Should give bot id when bot is found in given room.', function(done){
-        var botLocator = new BotLocator(botWithNoOtherSocketsInRoom);
+    it('Should give bot id when bot room does not contain other sockets.', function(done){
+        var botLocator = new BotLocator(roomWithASingleBot);
 
         var bot = botLocator.locateFirstAvailableBotIn("London");
 
         test.string(bot)
             .is("botId");
+
+        done();
+    });
+
+    it('Should give undefined when bot room contains other sockets.', function(done){
+        var botLocator = new BotLocator(roomWhereBotIsConnectedToHuman);
+
+        var bot = botLocator.locateFirstAvailableBotIn("London");
+
+        test.value(bot)
+            .isUndefined();
+
+        done();
+    });
+
+    it('Should give first bot in room that contains multiple bots.', function(done){
+        var botLocator = new BotLocator(roomContainingMultipleBotsWhereOneIsConnectedToHuman);
+
+        var bot = botLocator.locateFirstAvailableBotIn("London");
+
+        test.string(bot)
+            .is('botId02');
 
         done();
     });
