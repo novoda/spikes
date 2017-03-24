c7c814ba443268e23c2f995258cc7c6d84d66854
Ryan Feline <ryan.feline@novoda.com>
2017-03-24 17:26:37 +0000

    Hook up socket io instance to http server.

--- TelepresenceBot/server-unit/core/testServer.js
+++ TelepresenceBot/server-unit/core/testServer.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 var port = process.env.PORT || 8080;
 
@@ -9,10 +11,18 @@ router.get('/', function(req, res) {
     res.json({ message: 'Welcome to the TelepresenceBot api' });
 });
 
-router.route('/rooms')
-    .get(function(req, res) {
-        res.sendFile(__dirname + '/json/rooms.json');
-    });
+router.route('/rooms').get(function(req, res) {
+    res.sendFile(__dirname + '/json/rooms.json');
+});
+
+router.route('/connect').get(function(req, res) {
+    var socket = io.listen(http);
+    res.send("listening");
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
 app.use('/api', router);
 
