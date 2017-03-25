3be1a433c9222e93d7f63cdb35f56cb6c7d75c79
Ryan Feline <ryan.feline@novoda.com>
2017-03-25 11:53:06 +0000

    Wait for document to load before binding events.

--- TelepresenceBot/server-unit/core/public/js/connect.js
+++ TelepresenceBot/server-unit/core/public/js/connect.js
@@ -1,19 +1,23 @@
 var intervalId;
 
-$('body').mousedown(function(event) {
-    console.log("mouseDown");
-    var message = $("input[type=button]").val();
-    intervalId = setInterval(function(){
-        sendMessage(message);
-    }, 500);
-});
-
 function sendMessage(message) {
     var socket = io();
     socket.emit('chat message', message);
 }
 
-$('body').mouseup(function(event) {
-    console.log("mouseUp");
-    clearInterval(intervalId);
+$(document).ready(function(){
+
+    $("input").mousedown(function(){
+        console.log("mouseDown");
+        var message = $(this).val();
+        intervalId = setInterval(function() {
+            sendMessage(message);
+        }, 500);
+    });
+
+    $("input").mouseup(function(){
+        console.log("mouseUp");
+        clearInterval(intervalId);
+    });
+
 });
