bb617318bef04d63a4092facfd48f5166616f047
Ryan Feline <ryan.feline@novoda.com>
2017-03-25 11:47:22 +0000

    Send message continuously as long as pressing button.

--- TelepresenceBot/server-unit/core/public/js/connect.js
+++ TelepresenceBot/server-unit/core/public/js/connect.js
@@ -1,4 +1,19 @@
+var intervalId;
+
+$('body').mousedown(function(event) {
+    console.log("mouseDown");
+    var message = $("input[type=button]").val();
+    intervalId = setInterval(function(){
+        sendMessage(message);
+    }, 500);
+});
+
 function sendMessage(message) {
     var socket = io();
     socket.emit('chat message', message);
 }
+
+$('body').mouseup(function(event) {
+    console.log("mouseUp");
+    clearInterval(intervalId);
+});
