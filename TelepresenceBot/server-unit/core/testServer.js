7282bf8c8c7e1cc3bba27100b258760c1ed2022b
Ryan Feline <ryan.feline@novoda.com>
2017-03-25 09:11:31 +0000

    Follow docs to create beginning of webpage for tpbot.

--- TelepresenceBot/server-unit/core/testServer.js
+++ TelepresenceBot/server-unit/core/testServer.js
@@ -3,21 +3,33 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-app.get('/', function(req, res) {
-    res.json({ message: 'Welcome to the TelepresenceBot api' });
-});
-
 var server = server.listen(4200, function() {
     console.log("Express server listening on port " + 4200);
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/html/index.html');
 });
 
 io.set("log level", 0);
 
 io.sockets.on("connection", function (socket) {
+    console.log('a user connected');
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+    });
+
     socket.on("echo", function (msg, callback) {
         callback = callback || function () {};
 
         socket.emit("echo", msg);
+
+        console.log("on Connection");
 
         callback(null, "Done.");
     });
