b044de5b5fe1d2dfd011f5c4661a95cf4645cbe7
Ryan Feline <ryan.feline@novoda.com>
2017-03-29 17:15:57 +0100

    Move ServerDeclarationListener to core.

--- TelepresenceBot/core/src/main/java/com/novoda/tpbot/support/ServerDeclarationListener.java
+++ TelepresenceBot/core/src/main/java/com/novoda/tpbot/support/ServerDeclarationListener.java
@@ -1,13 +1,11 @@
 package com.novoda.tpbot.support;
 
-import com.novoda.notils.logger.simple.Log;
-
 public interface ServerDeclarationListener {
 
     ServerDeclarationListener NO_OP = new ServerDeclarationListener() {
         @Override
         public void onConnect(String serverAddress) {
-            Log.w("onConnect() but no listener was set");
+            // no-op.
         }
     };
 
