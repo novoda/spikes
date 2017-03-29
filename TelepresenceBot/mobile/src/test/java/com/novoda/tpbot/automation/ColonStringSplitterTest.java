b977b071cef48fb6ea1f84ec29dac4de66f479f5
Ryan Feline <ryan.feline@novoda.com>
2017-03-29 20:31:06 +0100

    Add tests for colon splitter.

---
+++ TelepresenceBot/mobile/src/test/java/com/novoda/tpbot/automation/ColonStringSplitterTest.java
@@ -0,0 +1,40 @@
+package com.novoda.tpbot.automation;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class ColonStringSplitterTest {
+
+    private ColonStringSplitter colonStringSplitter;
+
+    @Before
+    public void setUp() {
+        colonStringSplitter = new ColonStringSplitter();
+    }
+
+    @Test
+    public void givenNullString_whenSplitting_thenReturnsEmptyStringArray() {
+        String nullString = null;
+
+        String[] result = colonStringSplitter.split(nullString);
+
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    public void givenStringContainingColons_whenSplitting_thenReturnsArraySplitByColon() {
+        String stringContainingColons = "this:string:contains:colons";
+
+        String[] result = colonStringSplitter.split(stringContainingColons);
+
+        assertThat(result).isEqualTo(new String[]{
+                "this",
+                "string",
+                "contains",
+                "colons"
+        });
+    }
+
+}
