c39d9d5a88b8ec4407b2c3e0d079c845006bc34b
Ataul Munim <hello@ataulm.com>
2017-03-30 23:00:17 +0100

    Add Sequence to represent a sequence of notes/chords

--- piano-hero/core/src/main/java/com/novoda/pianohero/Chord.java
+++ piano-hero/core/src/main/java/com/novoda/pianohero/Chord.java
@@ -25,6 +25,10 @@ public class Chord {
         return notes;
     }
 
+    public Set<Note> notes() {
+        return notes;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
