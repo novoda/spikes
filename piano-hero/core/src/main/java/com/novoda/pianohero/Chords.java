a2555d29db27a18cdae9078bac6051d568d70614
Ataul Munim <hello@ataulm.com>
2017-03-30 23:00:42 +0100

    Add two Songs with chords and notes in supported octaves

---
+++ piano-hero/core/src/main/java/com/novoda/pianohero/Chords.java
@@ -0,0 +1,12 @@
+package com.novoda.pianohero;
+
+class Chords {
+
+    static final Chord C = new Chord(Notes.C4, Notes.E4, Notes.G4);
+    static final Chord F_2ND_INV = new Chord(Notes.C4, Notes.F4, Notes.A4);
+    static final Chord G_2ND_INV = new Chord(Notes.D4, Notes.G4, Notes.B4);
+    static final Chord EM = new Chord(Notes.E4, Notes.G4, Notes.B5);
+    static final Chord AM_2ND_INV = new Chord(Notes.E4, Notes.A4, Notes.C5);
+    static final Chord G = new Chord(Notes.G4, Notes.B5, Notes.D5);
+    static final Chord C_2ND_INV = new Chord(Notes.G4, Notes.C5, Notes.E5);
+}
