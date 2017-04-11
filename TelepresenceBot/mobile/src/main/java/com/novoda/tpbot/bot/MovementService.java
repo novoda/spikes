1db1e075710ed40023ed345871016cdf9aefe48a
Ryan Feline <ryan.feline@novoda.com>
2017-04-11 13:02:51 +0100

    Fix spacing on MovementService interface.

--- TelepresenceBot/mobile/src/main/java/com/novoda/tpbot/bot/MovementService.java
+++ TelepresenceBot/mobile/src/main/java/com/novoda/tpbot/bot/MovementService.java
@@ -1,6 +1,7 @@
 package com.novoda.tpbot.bot;
 
 interface MovementService {
+
     void onPermissionGranted();
 
     void onPermissionDenied();
@@ -8,4 +9,5 @@ interface MovementService {
     void onDeviceAttached();
 
     void onDeviceDetached();
+
 }
