651fdb2900acace5d66ca7308570667bbd4998f9
amlcurran <aml.curran@gmail.com>
2017-04-21 09:34:07 +0200

    Adds a function which allows reading of PRs

--- github-firebase-sizer/functions/index.js
+++ github-firebase-sizer/functions/index.js
@@ -6,3 +6,13 @@ var functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.sizePR = functions.https.onRequest((request, response) => {
+  if (request.get('X-GitHub-Event') === 'ping') {
+    console.log('Pong!');
+    response.status(200).end();
+  } else {
+    console.log(request.body.pull_request.additions);
+    response.status(200).end();
+  }
+});
