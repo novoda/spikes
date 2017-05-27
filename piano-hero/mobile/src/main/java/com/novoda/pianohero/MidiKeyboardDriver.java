a20ca2169f4fefaf9b1f21c597d1ad50eb92e7db
Ataul Munim <hello@ataulm.com>
2017-05-27 10:17:41 +0100

    Extract SimpleUsbMidiDriver to hide noise

--- piano-hero/mobile/src/main/java/com/novoda/pianohero/MidiKeyboardDriver.java
+++ piano-hero/mobile/src/main/java/com/novoda/pianohero/MidiKeyboardDriver.java
@@ -2,13 +2,10 @@ package com.novoda.pianohero;
 
 import android.content.Context;
 import android.hardware.usb.UsbDevice;
-import android.support.annotation.NonNull;
 import android.util.Log;
 import android.util.SparseArray;
 
 import jp.kshoji.driver.midi.device.MidiInputDevice;
-import jp.kshoji.driver.midi.device.MidiOutputDevice;
-import jp.kshoji.driver.midi.util.UsbMidiDriver;
 
 interface MidiKeyboardDriver {
 
@@ -47,7 +44,7 @@ interface MidiKeyboardDriver {
         NOT_IMPLED
     }
 
-    class KeyStationMini32 extends UsbMidiDriver implements MidiKeyboardDriver {
+    class KeyStationMini32 extends SimpleUsbMidiDriver implements MidiKeyboardDriver {
         private KeyListener keyListener;
 
         private static final SparseArray<Note> NOTES_MAPPING = new SparseArray<>();
@@ -74,58 +71,8 @@ interface MidiKeyboardDriver {
             NOTES_MAPPING.put(79, Note.G5);
         }
 
-        protected KeyStationMini32(@NonNull Context context) {
+        KeyStationMini32(Context context) {
             super(context);
-        }
-
-        @Override
-        public void onDeviceAttached(@NonNull UsbDevice usbDevice) {
-            Log.d("!!!", "device attached");
-        }
-
-        @Override
-        public void onMidiInputDeviceAttached(@NonNull MidiInputDevice midiInputDevice) {
-            // not used
-        }
-
-        @Override
-        public void onMidiOutputDeviceAttached(@NonNull MidiOutputDevice midiOutputDevice) {
-            // not used
-        }
-
-        @Override
-        public void onDeviceDetached(@NonNull UsbDevice usbDevice) {
-            Log.d("!!!", "device detached");
-        }
-
-        @Override
-        public void onMidiInputDeviceDetached(@NonNull MidiInputDevice midiInputDevice) {
-            // not used
-        }
-
-        @Override
-        public void onMidiOutputDeviceDetached(@NonNull MidiOutputDevice midiOutputDevice) {
-            // not used
-        }
-
-        @Override
-        public void onMidiMiscellaneousFunctionCodes(@NonNull MidiInputDevice midiInputDevice, int i, int i1, int i2, int i3) {
-            // not used
-        }
-
-        @Override
-        public void onMidiCableEvents(@NonNull MidiInputDevice midiInputDevice, int i, int i1, int i2, int i3) {
-            // not used
-        }
-
-        @Override
-        public void onMidiSystemCommonMessage(@NonNull MidiInputDevice midiInputDevice, int i, byte[] bytes) {
-            // not used
-        }
-
-        @Override
-        public void onMidiSystemExclusive(@NonNull MidiInputDevice midiInputDevice, int i, byte[] bytes) {
-            // not used
         }
 
         @Override
@@ -133,39 +80,19 @@ interface MidiKeyboardDriver {
             this.keyListener = keyListener;
         }
 
-        /**
-         * Note-on
-         * Code Index Number : 0x9
-         *
-         * @param midiInputDevice the Object which the event sent
-         * @param cable           the cable ID 0-15
-         * @param channel         the MIDI channel number 0-15
-         * @param note            0-127
-         * @param velocity        0-127
-         */
         @Override
-        public void onMidiNoteOn(
-                @NonNull MidiInputDevice midiInputDevice,
-                int cable,
-                int channel,
-                int note,
-                int velocity) {
-            // not used
+        public void onDeviceAttached(UsbDevice usbDevice) {
+            Log.d("!!!", "device attached");
         }
 
-        /**
-         * Note-off
-         * Code Index Number : 0x8
-         *
-         * @param midiInputDevice the Object which the event sent
-         * @param cable           the cable ID 0-15
-         * @param channel         0-15
-         * @param note            0-127
-         * @param velocity        0-127
-         */
+        @Override
+        public void onDeviceDetached(UsbDevice usbDevice) {
+            Log.d("!!!", "device detached");
+        }
+
         @Override
         public void onMidiNoteOff(
-                @NonNull MidiInputDevice midiInputDevice,
+                MidiInputDevice midiInputDevice,
                 int cable,
                 int channel,
                 int note,
@@ -178,86 +105,6 @@ interface MidiKeyboardDriver {
             } else {
                 keyListener.onKeyPressed(NOTES_MAPPING.get(note));
             }
-        }
-
-        @Override
-        public void onMidiPolyphonicAftertouch(@NonNull MidiInputDevice midiInputDevice, int i, int i1, int i2, int i3) {
-            // not used
-        }
-
-        @Override
-        public void onMidiControlChange(@NonNull MidiInputDevice midiInputDevice, int i, int i1, int i2, int i3) {
-            // not used
-        }
-
-        @Override
-        public void onMidiProgramChange(@NonNull MidiInputDevice midiInputDevice, int i, int i1, int i2) {
-            // not used
-        }
-
-        @Override
-        public void onMidiChannelAftertouch(@NonNull MidiInputDevice midiInputDevice, int i, int i1, int i2) {
-            // not used
-        }
-
-        @Override
-        public void onMidiPitchWheel(@NonNull MidiInputDevice midiInputDevice, int i, int i1, int i2) {
-            // not used
-        }
-
-        @Override
-        public void onMidiSingleByte(@NonNull MidiInputDevice midiInputDevice, int i, int i1) {
-            // not used
-        }
-
-        @Override
-        public void onMidiTimeCodeQuarterFrame(@NonNull MidiInputDevice midiInputDevice, int i, int i1) {
-            // not used
-        }
-
-        @Override
-        public void onMidiSongSelect(@NonNull MidiInputDevice midiInputDevice, int i, int i1) {
-            // not used
-        }
-
-        @Override
-        public void onMidiSongPositionPointer(@NonNull MidiInputDevice midiInputDevice, int i, int i1) {
-            // not used
-        }
-
-        @Override
-        public void onMidiTuneRequest(@NonNull MidiInputDevice midiInputDevice, int i) {
-            // not used
-        }
-
-        @Override
-        public void onMidiTimingClock(@NonNull MidiInputDevice midiInputDevice, int i) {
-            // not used
-        }
-
-        @Override
-        public void onMidiStart(@NonNull MidiInputDevice midiInputDevice, int i) {
-            // not used
-        }
-
-        @Override
-        public void onMidiContinue(@NonNull MidiInputDevice midiInputDevice, int i) {
-            // not used
-        }
-
-        @Override
-        public void onMidiStop(@NonNull MidiInputDevice midiInputDevice, int i) {
-            // not used
-        }
-
-        @Override
-        public void onMidiActiveSensing(@NonNull MidiInputDevice midiInputDevice, int i) {
-            // not used
-        }
-
-        @Override
-        public void onMidiReset(@NonNull MidiInputDevice midiInputDevice, int i) {
-            // not used
         }
     }
 }
