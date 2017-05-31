aadcfcb25d343a8bdb1cf2d76cd73927477f10dd
Ataul Munim <hello@ataulm.com>
2017-05-31 11:30:36 +0100

    Remove extra spike activities with related code

--- piano-hero/core/src/main/java/com/novoda/pianohero/SongSequenceFactory.java
+++ piano-hero/core/src/main/java/com/novoda/pianohero/SongSequenceFactory.java
@@ -10,15 +10,4 @@ public class SongSequenceFactory {
                 .add(Note.E4).add(Note.D4).add(Note.D4).add(Note.E4).add(Note.D4).add(Note.C4)
                 .build();
     }
-
-    public Sequence pachelbelsCanon() {
-        return new Sequence.Builder()
-                .add(Chord.C_2ND_INV).add(Chord.G).add(Chord.AM_2ND_INV).add(Chord.EM)
-                .add(Chord.F_2ND_INV).add(Chord.C).add(Chord.F_2ND_INV).add(Chord.G_2ND_INV)
-                .add(Chord.C_2ND_INV).add(Chord.G).add(Chord.AM_2ND_INV).add(Chord.EM)
-                .add(Chord.F_2ND_INV).add(Chord.C).add(Chord.F_2ND_INV).add(Chord.G_2ND_INV)
-                .add(Chord.C_2ND_INV).add(Chord.G).add(Chord.AM_2ND_INV).add(Chord.EM)
-                .add(Chord.F_2ND_INV).add(Chord.C).add(Chord.F_2ND_INV).add(Chord.G_2ND_INV).add(Chord.C)
-                .build();
-    }
 }
