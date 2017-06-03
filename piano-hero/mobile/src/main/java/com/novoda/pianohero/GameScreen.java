c8764caaa306c2bad26cbe280eab49b7a609714d
Ataul Munim <hello@ataulm.com>
2017-06-03 18:12:09 +0100

    Hook up note hints in GameScreen

--- piano-hero/mobile/src/main/java/com/novoda/pianohero/GameScreen.java
+++ piano-hero/mobile/src/main/java/com/novoda/pianohero/GameScreen.java
@@ -6,7 +6,13 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class GameScreen extends LinearLayoutCompat implements GameMvp.View {
+
+    private static final String NEXT_NOTE_HINT_FORMAT = "Next: %s";
+
+    private final SimplePitchNotationFormatter formatter;
 
     private C4ToB5TrebleStaffWidget trebleStaffWidget;
     private View mainMenuButton;
@@ -18,6 +24,7 @@ public class GameScreen extends LinearLayoutCompat implements GameMvp.View {
     public GameScreen(Context context, AttributeSet attrs) {
         super(context, attrs);
         super.setOrientation(VERTICAL);
+        this.formatter = new SimplePitchNotationFormatter();
     }
 
     @Override
@@ -28,14 +35,34 @@ public class GameScreen extends LinearLayoutCompat implements GameMvp.View {
         mainMenuButton = findViewById(R.id.game_screen_button_main_menu);
         restartButton = findViewById(R.id.game_screen_button_restart);
         playNoteTextView = (TextView) findViewById(R.id.game_screen_text_play_note);
+        nextNoteTextView = (TextView) findViewById(R.id.game_screen_text_next_note);
         statusTextView = (TextView) findViewById(R.id.game_screen_text_status);
     }
 
     @Override
     public void showRound(RoundViewModel viewModel) {
         statusTextView.setText(viewModel.getStatusMessage());
-        trebleStaffWidget.show(viewModel.getSequence());
+
+        Sequence sequence = viewModel.getSequence();
+        playNoteTextView.setText(currentNote(sequence));
+        nextNoteTextView.setText(nextNote(sequence));
+
+        trebleStaffWidget.show(sequence);
         trebleStaffWidget.setVisibility(View.VISIBLE);
+    }
+
+    private String currentNote(Sequence sequence) {
+        Note note = sequence.get(sequence.position());
+        return formatter.format(note);
+    }
+
+    private String nextNote(Sequence sequence) {
+        if (sequence.position() + 1 < sequence.length()) {
+            String nextNote = formatter.format(sequence.get(sequence.position() + 1));
+            return String.format(Locale.US, NEXT_NOTE_HINT_FORMAT, nextNote);
+        } else {
+            return "";
+        }
     }
 
     @Override
