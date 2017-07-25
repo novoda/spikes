b23f1653d2cd06dacaa97c6abafc32839a1e0e7b
Blundell <paul@novoda.com>
2017-07-25 12:16:30 +0100

    initial commit :octocat:

---
+++ InkyPhat/app/src/main/java/com/novoda/inkyphat/MainActivity.java
@@ -0,0 +1,16 @@
+package com.novoda.inkyphat;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.PeripheralManagerService;
+import com.google.android.things.pio.SpiDevice;
+
+import java.io.IOException;
+
+public class MainActivity extends Activity {
+
+
+}
