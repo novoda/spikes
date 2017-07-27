b9e9c0677afab5baea99317ab89c09c4edc47520
Blundell <paul@novoda.com>
2017-07-27 08:54:21 +0100

    correct data

--- InkyPhat/app/src/main/java/com/novoda/inkyphat/MainActivity.java
+++ InkyPhat/app/src/main/java/com/novoda/inkyphat/MainActivity.java
@@ -66,12 +66,12 @@ public class MainActivity extends Activity {
             byte[] buffer = new byte[]{0x01};
             spiBus.write(buffer, buffer.length);
             chipCommandPin.setValue(SPI_DATA);
-            buffer = new byte[]{0x02, 0x00, 0x00, 0x00};
+            buffer = new byte[]{0x02, 0x00, 0x0A, 0x00};
             spiBus.write(buffer, buffer.length);
 
             // _BOOSTER_SOFT_START
             chipCommandPin.setValue(SPI_COMMAND);
-            buffer = new byte[]{0x02, 0x00, 0x00, 0x00};
+            buffer = new byte[]{0x06};
             spiBus.write(buffer, buffer.length);
             chipCommandPin.setValue(SPI_DATA);
             buffer = new byte[]{0x07, 0x07, 0x07};
