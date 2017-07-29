8d907e57d17c9a6889326a05620e3e4f006e6c64
Blundell <paul@novoda.com>
2017-07-29 10:48:27 +0100

    separate our Driver from the Activity

--- InkyPhat/app/src/main/java/com/novoda/inkyphat/MainActivity.java
+++ InkyPhat/app/src/main/java/com/novoda/inkyphat/MainActivity.java
@@ -2,244 +2,27 @@ package com.novoda.inkyphat;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.SystemClock;
-import android.util.Log;
-
-import com.google.android.things.pio.Gpio;
-import com.google.android.things.pio.PeripheralManagerService;
-import com.google.android.things.pio.SpiDevice;
-
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends Activity {
 
     private static final String INKY_PHAT_DISPLAY = "SPI0.0";
-    private static final boolean SPI_COMMAND = false;
-    private static final boolean SPI_DATA = true;
+    private static final String COMMAND_PIN = "BCM22";
+    private static final String RESET_PIN = "BCM27";
+    private static final String BUSY_PIN = "BCM17";
 
-    static final int WIDTH = 104;
-    static final int HEIGHT = 212;
-    private static final int NUMBER_OF_PIXEL_REGIONS = WIDTH * HEIGHT / 8;
-
-    private static final byte PANEL_SETTING = (byte) 0x00;
-    private static final byte POWER_SETTING = (byte) 0x01;
-    private static final byte POWER_OFF = (byte) 0x02;
-    private static final byte POWER_ON = (byte) 0x04;
-    private static final byte BOOSTER_SOFT_START = (byte) 0x06;
-    private static final byte DATA_START_TRANSMISSION_1 = (byte) 0x10;
-    private static final byte DATA_START_TRANSMISSION_2 = (byte) 0x13;
-    private static final byte DISPLAY_REFRESH = (byte) 0x12;
-    private static final byte OSCILLATOR_CONTROL = (byte) 0x30;
-    private static final byte VCOM_DATA_INTERVAL_SETTING = (byte) 0x50;
-    private static final byte RESOLUTION_SETTING = (byte) 0x61;
-    private static final byte VCOM_DC_SETTING = (byte) 0x82;
-    private static final byte PARTIAL_EXIT = (byte) 0x92;
-
-    private SpiDevice spiBus;
-    private Gpio chipBusyPin;
-    private Gpio chipResetPin;
-    private Gpio chipCommandPin;
-
-    public enum Palette {
-        WHITE, BLACK, RED
-    }
-
-    private Palette[][] pixelBuffer = new Palette[WIDTH][HEIGHT];
+    private InkyPhat inkyPhat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("TUT", "onCreate");
-        PeripheralManagerService service = new PeripheralManagerService();
-        try {
-            spiBus = service.openSpiDevice(INKY_PHAT_DISPLAY);
-
-            chipCommandPin = service.openGpio("BCM22");
-            chipResetPin = service.openGpio("BCM27");
-            chipBusyPin = service.openGpio("BCM17");
-
-        } catch (IOException e) {
-            throw new IllegalStateException(INKY_PHAT_DISPLAY + " connection cannot be opened.", e);
-        }
-        try {
-            spiBus.setMode(SpiDevice.MODE0);
-//            spiBus.setFrequency(1_000_000); // 1Mhz
-//            spiBus.setBitsPerWord(8);
-
-            chipCommandPin.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
-            chipCommandPin.setActiveType(Gpio.ACTIVE_HIGH);
-            chipResetPin.setDirection(Gpio.DIRECTION_OUT_INITIALLY_HIGH);
-            chipResetPin.setActiveType(Gpio.ACTIVE_HIGH);
-            chipBusyPin.setDirection(Gpio.DIRECTION_IN);
-            chipBusyPin.setActiveType(Gpio.ACTIVE_HIGH);
-        } catch (IOException e) {
-            throw new IllegalStateException(INKY_PHAT_DISPLAY + " cannot be configured.", e);
-        }
-
-        Tests.drawThreeSquares(this);
-
-        try {
-            turnDisplayOn();
-            Log.d("TUT", "display on");
-            update();
-            Log.d("TUT", "display refreshed");
-            turnDisplayOff();
-            Log.d("TUT", "display off");
-        } catch (IOException e) {
-            throw new IllegalStateException("cannot init", e);
-        }
-
-    }
-
-    void setPixel(int x, int y, Palette color) {
-        if (x > WIDTH) {
-            throw new IllegalStateException(x + " cannot be drawn. Max width is " + WIDTH);
-        }
-        if (y > HEIGHT) {
-            throw new IllegalStateException(y + " cannot be drawn. Max height is " + HEIGHT);
-        }
-        pixelBuffer[x][y] = color;
-    }
-
-    private void turnDisplayOn() throws IOException {
-        busyWait();
-
-        sendCommand(POWER_SETTING, new byte[]{0x07, 0x00, 0x0A, 0x00});
-        sendCommand(BOOSTER_SOFT_START, new byte[]{0x07, 0x07, 0x07});
-        sendCommand(POWER_ON);
-
-        busyWait();
-
-        sendCommand(PANEL_SETTING, new byte[]{(byte) 0b11001111});
-        sendCommand(VCOM_DATA_INTERVAL_SETTING, new byte[]{(byte) 0b00000111 | (byte) 0b11000000}); // Set border to white by default
-
-        sendCommand(OSCILLATOR_CONTROL, new byte[]{0x29});
-        sendCommand(RESOLUTION_SETTING, new byte[]{0x68, 0x00, (byte) 0xD4});
-        sendCommand(VCOM_DC_SETTING, new byte[]{0x0A});
-
-        sendCommand(PARTIAL_EXIT);
-    }
-
-    private void update() throws IOException {
-        byte[] black = convertPixelBufferToDisplayColor(Palette.BLACK);
-        sendCommand(DATA_START_TRANSMISSION_1, black);
-        byte[] red = convertPixelBufferToDisplayColor(Palette.RED);
-        sendCommand(DATA_START_TRANSMISSION_2, red);
-
-        sendCommand(DISPLAY_REFRESH);
-    }
-
-
-    private byte[] convertPixelBufferToDisplayColor(Palette color) {
-        Palette[] flattenedPaletteArray = flatten(pixelBuffer);
-        return mapPaletteArrayToByteArrayForDisplay(flattenedPaletteArray, color);
-//        byte[] display = new byte[NUMBER_OF_PIXEL_REGIONS];
-//
-//        byte colorByte = 0b00000000;
-//        int bitPosition = 7;
-//        int displayPosition = 0;
-//        for (int x = 0; x < WIDTH; x++) {
-//            for (int y = 0; y < HEIGHT; y++) {
-//
-//                Palette pixelColor = pixelBuffer[y][x];
-//
-//                if (pixelColor == color) {
-//                    colorByte |= 1 << bitPosition;
-//                }
-//                bitPosition--;
-//
-//                if (bitPosition == -1) {
-//                    display[displayPosition] = colorByte;
-//                    colorByte = 0b00000000;
-//                    bitPosition = 0;
-//                    displayPosition++;
-//                }
-//            }
-//        }
-//        return display;
-    }
-
-    private static Palette[] flatten(Palette[][] twoDimensionalPaletteArray) {
-        Palette[] flattenedArray = new Palette[WIDTH * HEIGHT];
-        int index = 0;
-        for (int y = 0; y < HEIGHT; y++) {
-            for (int x = 0; x < WIDTH; x++) {
-                Palette color = twoDimensionalPaletteArray[x][y];
-                flattenedArray[index++] = color;
-            }
-        }
-        return flattenedArray;
-    }
-
-    private static byte[] mapPaletteArrayToByteArrayForDisplay(Palette[] palettes, Palette choice) {
-        byte[] display = new byte[NUMBER_OF_PIXEL_REGIONS];
-        int bitPosition = 7;
-        int segment = 0;
-        byte colorByte = 0b00000000;
-        for (Palette paletteColor : palettes) {
-            if (paletteColor == choice) {
-                colorByte |= 1 << bitPosition;
-            }
-            bitPosition--;
-            if (bitPosition == -1) {
-                display[segment++] = colorByte;
-                bitPosition = 7;
-                colorByte = 0b00000000;
-            }
-        }
-        return display;
-    }
-
-    private void turnDisplayOff() throws IOException {
-        busyWait();
-
-        sendCommand(VCOM_DATA_INTERVAL_SETTING, new byte[]{0x00});
-        sendCommand(POWER_SETTING, new byte[]{0x02, 0x00, 0x00, 0x00});
-        sendCommand(POWER_OFF);
-    }
-
-    private void sendCommand(byte command) throws IOException {
-        chipCommandPin.setValue(SPI_COMMAND);
-        byte[] buffer = new byte[]{command};
-        spiBus.write(buffer, buffer.length);
-    }
-
-    private void sendCommand(byte command, byte[] data) throws IOException {
-        sendCommand(command);
-        sendData(data);
-    }
-
-    private void sendData(byte[] data) throws IOException {
-        chipCommandPin.setValue(SPI_DATA);
-        spiBus.write(data, data.length);
-    }
-
-    private long then = SystemClock.currentThreadTimeMillis();
-
-    private void busyWait() throws IOException {
-        Log.d("TUT", "Wait for the e-paper driver to be ready to receive commands/data.");
-        while (!chipBusyPin.getValue()) {
-            // wait
-            long now = SystemClock.currentThreadTimeMillis();
-            if (then + TimeUnit.SECONDS.toMillis(1) < now) {
-                Log.v("TUT", "waiting 1 second");
-                then = now;
-            }
-        }
-        Log.d("TUT", "finished waiting");
+        inkyPhat = InkyPhat.create(INKY_PHAT_DISPLAY, BUSY_PIN, RESET_PIN, COMMAND_PIN);
+        Tests.drawTwoSquares(inkyPhat);
+        inkyPhat.refresh();
     }
 
     @Override
     protected void onDestroy() {
-        try {
-            spiBus.close();
-            chipBusyPin.close();
-            chipResetPin.close();
-            chipCommandPin.close();
-        } catch (IOException e) {
-            Log.e("TUT", INKY_PHAT_DISPLAY + "connection cannot be closed, you may experience errors on next launch.", e);
-        }
+        inkyPhat.close();
         super.onDestroy();
     }
 }
--- InkyPhat/app/src/main/java/com/novoda/inkyphat/Tests.java
+++ InkyPhat/app/src/main/java/com/novoda/inkyphat/Tests.java
@@ -1,83 +1,83 @@
 package com.novoda.inkyphat;
 
-import com.novoda.inkyphat.MainActivity.Palette;
+import com.novoda.inkyphat.InkyPhat.Palette;
 
-public class Tests {
+class Tests {
 
-    public static void drawWholeScreen(MainActivity activity) {
-        for (int row = 0; row < MainActivity.HEIGHT; row++) {
-            for (int col = 0; col < MainActivity.WIDTH; col++) {
-                activity.setPixel(row, col, Palette.WHITE);
+    static void drawWholeScreen(InkyPhat inkyPhat) {
+        for (int row = 0; row < InkyPhat.HEIGHT; row++) {
+            for (int col = 0; col < InkyPhat.WIDTH; col++) {
+                inkyPhat.setPixel(row, col, Palette.WHITE);
             }
         }
     }
 
-    public static void drawThreeSquares(MainActivity activity) {
+    static void drawTwoSquares(InkyPhat inkyPhat) {
         // A square
-        activity.setPixel(3, 3, Palette.RED);
-        activity.setPixel(4, 3, Palette.BLACK);
-        activity.setPixel(5, 3, Palette.BLACK);
-        activity.setPixel(6, 3, Palette.BLACK);
-        activity.setPixel(7, 3, Palette.BLACK);
-        activity.setPixel(8, 3, Palette.BLACK);
-        activity.setPixel(9, 3, Palette.RED);
+        inkyPhat.setPixel(3, 3, Palette.BLACK);
+        inkyPhat.setPixel(4, 3, Palette.BLACK);
+        inkyPhat.setPixel(5, 3, Palette.BLACK);
+        inkyPhat.setPixel(6, 3, Palette.BLACK);
+        inkyPhat.setPixel(7, 3, Palette.BLACK);
+        inkyPhat.setPixel(8, 3, Palette.BLACK);
+        inkyPhat.setPixel(9, 3, Palette.BLACK);
 
-        activity.setPixel(3, 3, Palette.BLACK);
-        activity.setPixel(3, 4, Palette.BLACK);
-        activity.setPixel(3, 5, Palette.BLACK);
-        activity.setPixel(3, 6, Palette.BLACK);
-        activity.setPixel(3, 7, Palette.BLACK);
-        activity.setPixel(3, 8, Palette.BLACK);
-        activity.setPixel(3, 9, Palette.BLACK);
-//
-        activity.setPixel(9, 3, Palette.BLACK);
-        activity.setPixel(9, 4, Palette.BLACK);
-        activity.setPixel(9, 5, Palette.BLACK);
-        activity.setPixel(9, 6, Palette.BLACK);
-        activity.setPixel(9, 7, Palette.BLACK);
-        activity.setPixel(9, 8, Palette.BLACK);
-        activity.setPixel(9, 9, Palette.BLACK);
-//
-        activity.setPixel(3, 9, Palette.BLACK);
-        activity.setPixel(4, 9, Palette.BLACK);
-        activity.setPixel(5, 9, Palette.BLACK);
-        activity.setPixel(6, 9, Palette.BLACK);
-        activity.setPixel(7, 9, Palette.BLACK);
-        activity.setPixel(8, 9, Palette.BLACK);
-        activity.setPixel(9, 9, Palette.BLACK);
-//
-//        // A square
-        activity.setPixel(23, 23, Palette.RED);
-        activity.setPixel(24, 23, Palette.RED);
-        activity.setPixel(25, 23, Palette.RED);
-        activity.setPixel(26, 23, Palette.RED);
-        activity.setPixel(27, 23, Palette.RED);
-        activity.setPixel(28, 23, Palette.RED);
-        activity.setPixel(29, 23, Palette.RED);
+        inkyPhat.setPixel(3, 3, Palette.BLACK);
+        inkyPhat.setPixel(3, 4, Palette.BLACK);
+        inkyPhat.setPixel(3, 5, Palette.BLACK);
+        inkyPhat.setPixel(3, 6, Palette.BLACK);
+        inkyPhat.setPixel(3, 7, Palette.BLACK);
+        inkyPhat.setPixel(3, 8, Palette.BLACK);
+        inkyPhat.setPixel(3, 9, Palette.BLACK);
 
-        activity.setPixel(23, 23, Palette.RED);
-        activity.setPixel(23, 24, Palette.RED);
-        activity.setPixel(23, 25, Palette.RED);
-        activity.setPixel(23, 26, Palette.RED);
-        activity.setPixel(23, 27, Palette.RED);
-        activity.setPixel(23, 28, Palette.RED);
-        activity.setPixel(23, 29, Palette.RED);
+        inkyPhat.setPixel(9, 3, Palette.BLACK);
+        inkyPhat.setPixel(9, 4, Palette.BLACK);
+        inkyPhat.setPixel(9, 5, Palette.BLACK);
+        inkyPhat.setPixel(9, 6, Palette.BLACK);
+        inkyPhat.setPixel(9, 7, Palette.BLACK);
+        inkyPhat.setPixel(9, 8, Palette.BLACK);
+        inkyPhat.setPixel(9, 9, Palette.BLACK);
 
-        activity.setPixel(29, 23, Palette.RED);
-        activity.setPixel(29, 24, Palette.RED);
-        activity.setPixel(29, 25, Palette.RED);
-        activity.setPixel(29, 26, Palette.RED);
-        activity.setPixel(29, 27, Palette.RED);
-        activity.setPixel(29, 28, Palette.RED);
-        activity.setPixel(29, 29, Palette.RED);
+        inkyPhat.setPixel(3, 9, Palette.BLACK);
+        inkyPhat.setPixel(4, 9, Palette.BLACK);
+        inkyPhat.setPixel(5, 9, Palette.BLACK);
+        inkyPhat.setPixel(6, 9, Palette.BLACK);
+        inkyPhat.setPixel(7, 9, Palette.BLACK);
+        inkyPhat.setPixel(8, 9, Palette.BLACK);
+        inkyPhat.setPixel(9, 9, Palette.BLACK);
 
-        activity.setPixel(23, 29, Palette.RED);
-        activity.setPixel(24, 29, Palette.RED);
-        activity.setPixel(25, 29, Palette.RED);
-        activity.setPixel(26, 29, Palette.RED);
-        activity.setPixel(27, 29, Palette.RED);
-        activity.setPixel(28, 29, Palette.RED);
-        activity.setPixel(29, 29, Palette.RED);
+        // A square
+        inkyPhat.setPixel(23, 23, Palette.RED);
+        inkyPhat.setPixel(24, 23, Palette.RED);
+        inkyPhat.setPixel(25, 23, Palette.RED);
+        inkyPhat.setPixel(26, 23, Palette.RED);
+        inkyPhat.setPixel(27, 23, Palette.RED);
+        inkyPhat.setPixel(28, 23, Palette.RED);
+        inkyPhat.setPixel(29, 23, Palette.RED);
+
+        inkyPhat.setPixel(23, 23, Palette.RED);
+        inkyPhat.setPixel(23, 24, Palette.RED);
+        inkyPhat.setPixel(23, 25, Palette.RED);
+        inkyPhat.setPixel(23, 26, Palette.RED);
+        inkyPhat.setPixel(23, 27, Palette.RED);
+        inkyPhat.setPixel(23, 28, Palette.RED);
+        inkyPhat.setPixel(23, 29, Palette.RED);
+
+        inkyPhat.setPixel(29, 23, Palette.RED);
+        inkyPhat.setPixel(29, 24, Palette.RED);
+        inkyPhat.setPixel(29, 25, Palette.RED);
+        inkyPhat.setPixel(29, 26, Palette.RED);
+        inkyPhat.setPixel(29, 27, Palette.RED);
+        inkyPhat.setPixel(29, 28, Palette.RED);
+        inkyPhat.setPixel(29, 29, Palette.RED);
+
+        inkyPhat.setPixel(23, 29, Palette.RED);
+        inkyPhat.setPixel(24, 29, Palette.RED);
+        inkyPhat.setPixel(25, 29, Palette.RED);
+        inkyPhat.setPixel(26, 29, Palette.RED);
+        inkyPhat.setPixel(27, 29, Palette.RED);
+        inkyPhat.setPixel(28, 29, Palette.RED);
+        inkyPhat.setPixel(29, 29, Palette.RED);
     }
 
 }
