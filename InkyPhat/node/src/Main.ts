2b37733706abb5da10d1a6521c5ffb50b281e8ce
Adam Brown <adam@novoda.com>
2017-08-01 18:16:12 +0100

    implement spi writing

--- InkyPhat/node/src/Main.ts
+++ InkyPhat/node/src/Main.ts
@@ -1,6 +1,8 @@
 import * as spi from 'spi-device'
 import * as gpio from 'rpi-gpio'
 
+declare const Buffer
+
 enum Palette {
     BLACK,
     RED,
@@ -37,24 +39,18 @@ const busyPin = new Pin(17)
 const commandPin = new Pin(22)
 const resetPin = new Pin(27)
 
-
-
+let spiDevice
 
 
 const init = async () => {
-    const spiDevice = spi.openSync(SPI_BUS, SPI_DEVICE, { mode: MODE_0 })
+    console.log('init')
+    spiDevice = spi.openSync(SPI_BUS, SPI_DEVICE, { mode: MODE_0 })
     await gpioSetup(commandPin, gpio.DIR_LOW)
     await gpioSetup(resetPin, gpio.DIR_HIGH)
     await gpioSetup(busyPin, gpio.DIR_IN)
+
+    await turnDisplayOff()
 }
-
-
-const writeData = (payload: number[]) => {
-    // gpioSetup(commandPin, PIN_HIGH)
-    // spi transfer payload
-    spi.transferSync(null);
-}
-
 
 const gpioSetup = (pin: Pin, value: any) => {
     return new Promise((resolve, reject) => {
@@ -64,4 +60,55 @@ const gpioSetup = (pin: Pin, value: any) => {
     })
 }
 
-init().then(console.log)
+
+
+
+const turnDisplayOff = async () => {
+    console.log('turning display off')
+
+    await busyWait();
+    await sendCommand(0x50, [0x00])
+    await sendCommand(0x01, [0x02, 0x00, 0x00, 0x00])
+    await writeData(false, [0x02])
+}
+
+const sendCommand = (commandData: number, data: number[]) => {
+    writeData(false, [commandData])
+    writeData(true, data)
+}
+
+const writeData = async (commandType: boolean, data: number[]) => {
+    await gpioWrite(commandPin, commandType)
+    var message = [{
+        sendBuffer: new Buffer(data),
+        byteLength: data.length
+    }];
+    spiDevice.transferSync(message)
+}
+
+const gpioWrite = (pin: Pin, value: boolean): Promise<any> => {
+    return new Promise((resolve, reject) => {
+        gpio.write(pin.value(), value, (err) => {
+            err ? reject(err) : resolve()
+        })
+    })
+}
+
+const busyWait = async () => {
+    while (true) {
+        let result = await gpioRead(busyPin)
+        if (result) {
+            break
+        }
+    }
+}
+
+const gpioRead = (pin: Pin): Promise<boolean> => {
+    return new Promise((resolve, reject) => {
+        gpio.read(pin.value(), (err, result) => {
+            err ? reject(err) : resolve(result)
+        })
+    })
+}
+
+init().then(console.log).catch(console.log)
