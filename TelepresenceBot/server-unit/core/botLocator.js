4194d7828d24389bbccfe62eefda6412bb9fc889
Ryan Feline <ryan.feline@novoda.com>
2017-08-03 08:57:17 +0100

    Use newer js notation.

--- TelepresenceBot/server-unit/core/botLocator.js
+++ TelepresenceBot/server-unit/core/botLocator.js
@@ -1,19 +1,19 @@
 function BotLocator(rooms) {
-    this.rooms = rooms;
-}
+    return {
+        locateFirstAvailableBotIn: function(room) {
+            var botsInRoom = rooms[room];
 
-BotLocator.prototype.locateFirstAvailableBotIn = function(room) {
-    var botsInRoom = this.rooms[room];
+            if(!botsInRoom) {
+                return;
+            }
 
-    if(!botsInRoom) {
-        return;
-    }
+            for (socketId in botsInRoom.sockets) {
+                var socketsInBotRoom = rooms[socketId];
 
-    for (socketId in botsInRoom.sockets) {
-        var socketsInBotRoom = this.rooms[socketId];
-
-        if(botNotConnectedToHuman(socketsInBotRoom)) {
-            return socketId;
+                if(botNotConnectedToHuman(socketsInBotRoom)) {
+                    return socketId;
+                }
+            }
         }
     }
 }
