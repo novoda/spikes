bde3f42fb926c933dc7791fc395a92cd124811c7
Ryan Feline <ryan.feline@novoda.com>
2017-08-08 17:16:41 +0100

    Pull out collaborator that is solely responsible for requesting Usb permissions.

---
+++ TelepresenceBot/mobile/src/main/java/com/novoda/tpbot/bot/UsbPermissionRequester.java
@@ -0,0 +1,29 @@
+package com.novoda.tpbot.bot;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
+
+import static com.novoda.tpbot.bot.AndroidMovementService.ACTION_USB_PERMISSION;
+
+class UsbPermissionRequester {
+
+    private static final int REQUEST_CODE = 0;
+    private static final int NO_FLAGS = 0;
+
+    private final Context context;
+    private final UsbManager usbManager;
+
+    private UsbPermissionRequester(Context context, UsbManager usbManager) {
+        this.context = context;
+        this.usbManager = usbManager;
+    }
+
+    void requestPermission(UsbDevice usbDevice) {
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, REQUEST_CODE, new Intent(ACTION_USB_PERMISSION), NO_FLAGS);
+        usbManager.requestPermission(usbDevice, pendingIntent);
+    }
+
+}
