f351b55bebec4e003f3b10b42d2b4d652b27797d
Ataul Munim <hello@ataulm.com>
2017-08-10 12:01:33 +0100

    Initial commit for iOS-dialog-actions-for-voiceover

---
+++ dialog-actions-for-voiceover/dialog-actions-for-voiceover/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dialog-actions-for-voiceover
+//
+//  Created by Ataul Munim on 10/08/2017.
+//  Copyright © 2017 Novoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
