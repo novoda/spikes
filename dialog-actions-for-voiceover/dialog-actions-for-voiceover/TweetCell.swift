6308eb395d8edd941c8ba05bb0ee8a50e7b5fe70
Ataul Munim <hello@ataulm.com>
2017-08-10 14:54:32 +0100

    Add TableView and TweetCell

---
+++ dialog-actions-for-voiceover/dialog-actions-for-voiceover/TweetCell.swift
@@ -0,0 +1,21 @@
+//
+//  TweetCell.swift
+//  dialog-actions-for-voiceover
+//
+//  Created by Ataul Munim on 10/08/2017.
+//  Copyright © 2017 Novoda. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+    
+    var avatarImageView: UIImageView!
+    var usernameLabel: UILabel!
+    var bodyLabel: UILabel!
+    var timeLabel: UILabel!
+    var replyButton: UIButton!
+    var retweetButton: UIButton!
+    var likeButton: UIButton!
+    
+}
--- dialog-actions-for-voiceover/dialog-actions-for-voiceover/TweetsViewController.swift
+++ dialog-actions-for-voiceover/dialog-actions-for-voiceover/TweetsViewController.swift
@@ -8,10 +8,42 @@
 
 import UIKit
 
+let cellIdentifier: String = "tweetCell"
+
 class TweetsViewController: UIViewController {
+    
+    let tweetsProvider = TweetsProvider()
+    
+    var tableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(TweetCell.self, forCellReuseIdentifier: cellIdentifier)
+        let tableViewAdapter = TweetsTableViewAdapter()
+        tableView.dataSource = tableViewAdapter
+        
+        tweetsProvider.fetchTweets(completion: { tweets in
+            tableViewAdapter.update(tweets)
+            self.tableView.reloadData()
+        })
+    }
+}
+
+class TweetsTableViewAdapter: NSObject, UITableViewDataSource {
+    
+    var tweets: [Tweet] = []
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: TweetCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as! TweetCell
+        // TODO: bind tweet to view
+        return cell
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tweets.count
+    }
+    
+    func update(_ tweets: [Tweet]) {
+        self.tweets = tweets
+    }
 }
