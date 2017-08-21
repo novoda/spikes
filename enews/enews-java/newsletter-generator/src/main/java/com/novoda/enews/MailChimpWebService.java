0b3801e7ef0c5d9943e500ccedddcec5a701bbde
Paul Blundell <paul@novoda.com>
2017-08-21 22:03:17 +0100

    remove deprecated

--- enews/enews-java/newsletter-generator/src/main/java/com/novoda/enews/MailChimpWebService.java
+++ enews/enews-java/newsletter-generator/src/main/java/com/novoda/enews/MailChimpWebService.java
@@ -5,7 +5,6 @@ import retrofit2.http.*;
 
 public interface MailChimpWebService {
 
-    @Headers("Authorization: apiKey 2adce69f42e1ea8663448f2d62f77fb7-us5")
     @GET("lists")
     Call<ApiPagedChannelHistory> getLists();
 
