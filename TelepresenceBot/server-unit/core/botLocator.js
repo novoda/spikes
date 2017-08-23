56b8b9dda322b3ceca1c151084204e7b3453085b
Ryan Feline <ryan.feline@novoda.com>
2017-08-23 06:29:42 +0100

    Extract explanatory functions for botLocator.

--- TelepresenceBot/server-unit/core/botLocator.js
+++ TelepresenceBot/server-unit/core/botLocator.js
@@ -1,38 +1,35 @@
 var debug = require('debug')('test');
 
 module.exports = function (rooms) {
-    var botNotConnectedToHuman = function (socketsInBotRoom) {
-        return socketsInBotRoom.length === 1;
-    };
+
+    var roomsThatMatch = function (toMatch) {
+        return function (room) {
+            return toMatch == room;
+        };
+    }
+
+    var roomsThatContainProperty = function (property) {
+        return function (roomName) {
+            return rooms[roomName].hasOwnProperty(property);
+        };
+    }
+
+    var roomsThatAreNotAlreadyConnectedToOtherSockets = function () {
+        return function (roomName) {
+            return rooms[roomName].length == 1;
+        }
+    }
 
     return {
         locateFirstAvailableBotIn: function (roomToFind) {
             return Object.keys(rooms)
-                .filter(function (roomName) {
-                    // Get 'London' room. These rooms can only contain bots.
-                    debug("roomName == roomToFind: ", roomName, roomName == roomToFind);
-                    return roomName == roomToFind;
-                })
-                .filter(function (roomName) {
-                    // Check that it contains 'sockets' property.
-                    debug("rooms[roomName].hasOwnProperty('sockets') ", rooms[roomName].hasOwnProperty('sockets'));
-                    return rooms[roomName].hasOwnProperty('sockets');
-                })
+                .filter(roomsThatMatch(roomToFind))
+                .filter(roomsThatContainProperty('sockets'))
                 .map(function (roomName) {
-                    // Get the sockets keys.
-                    debug("Object.keys(rooms[roomName].sockets): ", Object.keys(rooms[roomName].sockets));
                     return Object.keys(rooms[roomName].sockets)
-                        .filter(function (socketKey) {
-                            // Iterate over the keys that contain property length.
-                            debug("socketKey: ", socketKey);
-                            debug("rooms[socketKey].hasOwnProperty('length'): ", rooms[socketKey].hasOwnProperty('length'));
-                            return rooms[socketKey].hasOwnProperty('length');
-                        })
-                        .filter(function (socketKey) {
-                            // If length is 1 then not connected to other clients.
-                            debug("rooms[socketKey].length == 1: ", rooms[socketKey].length == 1);
-                            return rooms[socketKey].length == 1;
-                        }).pop();
+                        .filter(roomsThatContainProperty('length'))
+                        .filter(roomsThatAreNotAlreadyConnectedToOtherSockets())
+                        .pop();
                 }).pop();
         }
     };
