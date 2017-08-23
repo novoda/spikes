f5417335f092dcf4d7559d895e7ce13e8a55fd7b
Ryan Feline <ryan.feline@novoda.com>
2017-08-23 07:51:21 +0100

    Extract explanatory functions from mover.

--- TelepresenceBot/server-unit/core/mover.js
+++ TelepresenceBot/server-unit/core/mover.js
@@ -1,12 +1,17 @@
 module.exports = function Mover(clientsAndRooms, emitter) {
+
+    var emitToRoom = function (direction) {
+        return function (room) {
+            emitter.to(room).emit('direction', direction);
+        }
+    }
+
     return {
         moveIn: function (clientId, direction) {
             var rooms = clientsAndRooms[clientId];
 
             Object.keys(rooms || {})
-                .map(function (room) {
-                    emitter.to(room).emit('direction', direction);
-                });
+                .every(emitToRoom(direction));
         }
     };
 }
