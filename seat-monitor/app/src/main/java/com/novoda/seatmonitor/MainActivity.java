65911a726ed73c727cd6277a360286d8f945a6cf
Blundell <paul@novoda.com>
2017-09-15 16:08:22 +0100

    Seat Monitor - Initial Commit

---
+++ seat-monitor/app/src/main/java/com/novoda/seatmonitor/MainActivity.java
@@ -0,0 +1,17 @@
+package com.novoda.seatmonitor;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.util.Log;
+
+import com.google.android.things.pio.I2cDevice;
+import com.google.android.things.pio.PeripheralManagerService;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends Activity {
+
+}
