8419e84dd6d2d71c1ed5abda1ac719d822f2d3e5
Paul Blundell <paul@novoda.com>
2017-09-20 21:03:46 +0100

    make lines more readable

--- seat-monitor/app/src/main/java/com/novoda/seatmonitor/MainActivity.java
+++ seat-monitor/app/src/main/java/com/novoda/seatmonitor/MainActivity.java
@@ -7,6 +7,9 @@ import android.util.Log;
 import com.novoda.loadgauge.Ads1015;
 import com.novoda.loadgauge.WiiLoadSensor;
 
+import static com.novoda.loadgauge.Ads1015.DifferentialPins.PINS_0_1;
+import static com.novoda.loadgauge.Ads1015.DifferentialPins.PINS_2_3;
+
 public class MainActivity extends Activity {
 
     private WiiLoadSensor wiiLoadSensorA;
@@ -19,10 +22,11 @@ public class MainActivity extends Activity {
 
         String i2CBus = "I2C1";
         Ads1015.Gain gain = Ads1015.Gain.ONE;
-        String alertReadyGpioPin = "GPIO23";
-        Ads1015 ads10150x48 = new Ads1015.Factory().newDifferentialComparatorInstance(i2CBus, 0x48, gain, alertReadyGpioPin, Ads1015.DifferentialPins.PINS_0_1);
+        String alertReadyPin = "GPIO23";
+        Ads1015.Factory factory = new Ads1015.Factory();
+        Ads1015 ads10150x48 = factory.newDifferentialComparatorInstance(i2CBus, 0x48, gain, alertReadyPin, PINS_0_1);
         wiiLoadSensorA = new WiiLoadSensor(ads10150x48);
-        Ads1015 ads10150x49 = new Ads1015.Factory().newDifferentialComparatorInstance(i2CBus, 0x49, gain, alertReadyGpioPin, Ads1015.DifferentialPins.PINS_2_3);
+        Ads1015 ads10150x49 = factory.newDifferentialComparatorInstance(i2CBus, 0x49, gain, alertReadyPin, PINS_2_3);
         wiiLoadSensorB = new WiiLoadSensor(ads10150x49);
 
         // TODO button to calibrate
