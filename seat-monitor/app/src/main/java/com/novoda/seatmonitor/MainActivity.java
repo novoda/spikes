eaf79b5eb902bdd17e46e7da32b6c68bee9f2aba
Blundell <paul@novoda.com>
2017-09-25 09:35:00 +0100

    try all the combo's

--- seat-monitor/app/src/main/java/com/novoda/seatmonitor/MainActivity.java
+++ seat-monitor/app/src/main/java/com/novoda/seatmonitor/MainActivity.java
@@ -10,12 +10,10 @@ import com.novoda.loadgauge.WiiLoadSensor;
 
 import java.io.IOException;
 
-import static com.novoda.loadgauge.Ads1015.DifferentialPins.PINS_0_1;
-
 public class MainActivity extends Activity {
 
     private WiiLoadSensor wiiLoadSensorA;
-//    private WiiLoadSensor wiiLoadSensorB;
+    //    private WiiLoadSensor wiiLoadSensorB;
     private Button button;
 
     @Override
@@ -27,7 +25,10 @@ public class MainActivity extends Activity {
         Ads1015.Gain gain = Ads1015.Gain.ONE;
         String alertReadyPin = "GPIO_39";
         Ads1015.Factory factory = new Ads1015.Factory();
-        Ads1015 ads10150x48 = factory.newDifferentialComparatorInstance(i2CBus, 0x48, gain, alertReadyPin, PINS_0_1);
+//        Ads1015 ads10150x48 = factory.newDifferentialComparatorInstance(i2CBus, 0x48, gain, alertReadyPin, PINS_0_1);
+//        Ads1015 ads10150x48 = factory.newDifferentialReaderInstance(i2CBus, 0x48, gain, PINS_0_1);
+//        Ads1015 ads10150x48 = factory.newSingleEndedReaderInstance(i2CBus, 0x48, gain, Ads1015.Channel.ZERO);
+        Ads1015 ads10150x48 = factory.newSingleEndedComparatorInstance(i2CBus, 0x48, gain, alertReadyPin, Ads1015.Channel.ZERO);
         wiiLoadSensorA = new WiiLoadSensor(ads10150x48);
 //        Ads1015 ads10150x49 = factory.newDifferentialComparatorInstance(i2CBus, 0x49, gain, alertReadyPin, PINS_2_3);
 //        wiiLoadSensorB = new WiiLoadSensor(ads10150x49);
@@ -45,7 +46,6 @@ public class MainActivity extends Activity {
         } catch (IOException e) {
             throw new IllegalStateException("Button FooBar", e);
         }
-
         wiiLoadSensorA.monitorForWeightChangeOver(50, sensorAWeightChangedCallback);
     }
 
