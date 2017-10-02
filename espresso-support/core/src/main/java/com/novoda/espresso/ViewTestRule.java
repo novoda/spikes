f306840b07e8cc9a2237dafba063e1933d4a545b
Brais Gabín <braisgabin@gmail.com>
2017-10-02 19:04:40 +0200

    Allow to create the View programatically

--- espresso-support/core/src/main/java/com/novoda/espresso/ViewTestRule.java
+++ espresso-support/core/src/main/java/com/novoda/espresso/ViewTestRule.java
@@ -1,11 +1,12 @@
 package com.novoda.espresso;
 
 import android.app.Instrumentation;
-import android.content.Intent;
 import android.support.annotation.LayoutRes;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 public class ViewTestRule<T extends View> extends ActivityTestRule<ViewActivity> {
 
@@ -13,6 +14,7 @@ public class ViewTestRule<T extends View> extends ActivityTestRule<ViewActivity>
 
     @LayoutRes
     private final int id;
+    private T view;
 
     public ViewTestRule(@LayoutRes int id) {
         super(ViewActivity.class);
@@ -21,10 +23,24 @@ public class ViewTestRule<T extends View> extends ActivityTestRule<ViewActivity>
     }
 
     @Override
-    protected Intent getActivityIntent() {
-        Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.putExtra(ViewActivity.EXTRA_LAYOUT_ID, id);
-        return intent;
+    protected void afterActivityLaunched() {
+        instrumentation.runOnMainSync(new Runnable() {
+            @Override
+            public void run() {
+                launchView();
+            }
+        });
+    }
+
+    protected void launchView() {
+        final ViewActivity activity = getActivity();
+        final ViewGroup parent = (ViewGroup) activity.findViewById(android.R.id.content);
+        view = createView(activity.getLayoutInflater(), parent);
+        activity.setContentView(view);
+    }
+
+    protected T createView(final LayoutInflater inflater, ViewGroup parent) {
+        return (T) inflater.inflate(id, parent, false);
     }
 
     public void runOnMainSynchronously(final Runner<T> runner) {
@@ -38,7 +54,7 @@ public class ViewTestRule<T extends View> extends ActivityTestRule<ViewActivity>
     }
 
     public T getView() {
-        return (T) getActivity().getView();
+        return view;
     }
 
     public interface Runner<T> {
