be03ec66399e7dcfc34daa789d9f6709681b9be6
Blundell <paul@novoda.com>
2017-10-03 21:08:28 +0100

    comment back in hardware
     - now that I am a MQTT god

--- seat-monitor/app/src/main/java/com/novoda/seatmonitor/MainActivity.java
+++ seat-monitor/app/src/main/java/com/novoda/seatmonitor/MainActivity.java
@@ -5,7 +5,13 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.things.contrib.driver.button.Button;
+import com.novoda.loadgauge.Ads1015;
 import com.novoda.loadgauge.WiiLoadSensor;
+
+import java.io.IOException;
+
+import static com.novoda.loadgauge.Ads1015.DifferentialPins.PINS_0_1;
+import static com.novoda.loadgauge.Ads1015.DifferentialPins.PINS_2_3;
 
 public class MainActivity extends Activity {
 
@@ -19,29 +25,29 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         Log.d("TUT", "oncreate");
 
-//        String i2CBus = "I2C1";
-//        Ads1015.Gain gain = Ads1015.Gain.ONE;
-//        String alertReadyPin = "GPIO23";
-//        Ads1015.Factory factory = new Ads1015.Factory();
-//        Ads1015 ads10150x48 = factory.newDifferentialComparatorInstance(i2CBus, 0x48, gain, alertReadyPin, PINS_0_1);
-//        wiiLoadSensorA = new WiiLoadSensor(ads10150x48);
-//        Ads1015 ads10150x49 = factory.newDifferentialComparatorInstance(i2CBus, 0x49, gain, alertReadyPin, PINS_2_3);
-//        wiiLoadSensorB = new WiiLoadSensor(ads10150x49);
-//
-//        try {
-//            button = new Button("GPIO11", Button.LogicState.PRESSED_WHEN_HIGH);
-//            button.setOnButtonEventListener(new Button.OnButtonEventListener() {
-//                @Override
-//                public void onButtonEvent(Button button, boolean pressed) {
-//                    wiiLoadSensorA.calibrateToZero();
-//                    wiiLoadSensorB.calibrateToZero();
-//                }
-//            });
-//        } catch (IOException e) {
-//            throw new IllegalStateException("Button FooBar", e);
-//        }
-//
-//        wiiLoadSensorA.monitorForWeightChangeOver(50, sensorAWeightChangedCallback);
+        String i2CBus = "I2C1";
+        Ads1015.Gain gain = Ads1015.Gain.ONE;
+        String alertReadyPin = "GPIO23";
+        Ads1015.Factory factory = new Ads1015.Factory();
+        Ads1015 ads10150x48 = factory.newDifferentialComparatorInstance(i2CBus, 0x48, gain, alertReadyPin, PINS_0_1);
+        wiiLoadSensorA = new WiiLoadSensor(ads10150x48);
+        Ads1015 ads10150x49 = factory.newDifferentialComparatorInstance(i2CBus, 0x49, gain, alertReadyPin, PINS_2_3);
+        wiiLoadSensorB = new WiiLoadSensor(ads10150x49);
+
+        try {
+            button = new Button("GPIO11", Button.LogicState.PRESSED_WHEN_HIGH);
+            button.setOnButtonEventListener(new Button.OnButtonEventListener() {
+                @Override
+                public void onButtonEvent(Button button, boolean pressed) {
+                    wiiLoadSensorA.calibrateToZero();
+                    wiiLoadSensorB.calibrateToZero();
+                }
+            });
+        } catch (IOException e) {
+            throw new IllegalStateException("Button FooBar", e);
+        }
+
+        wiiLoadSensorA.monitorForWeightChangeOver(50, sensorAWeightChangedCallback);
 
         cloudIotCoreComms = new CloudIotCoreCommunicator.Builder()
                 .withContext(this)
@@ -75,13 +81,13 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onDestroy() {
-//        wiiLoadSensorA.stopMonitoring();
-//        wiiLoadSensorB.stopMonitoring();
-//        try {
-//            button.close();
-//        } catch (IOException e) {
-//            // ignore
-//        }
+        wiiLoadSensorA.stopMonitoring();
+        wiiLoadSensorB.stopMonitoring();
+        try {
+            button.close();
+        } catch (IOException e) {
+            // ignore
+        }
         cloudIotCoreComms.disconnect();
         super.onDestroy();
     }
