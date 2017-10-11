e06b8892136309c7e8091a54abbe951217d342d5
Ryan Feline <ryan.feline@novoda.com>
2017-10-11 06:45:41 +0100

    Create a MyLooper Executor to hide android implementation of MyLooper.

--- TelepresenceBot/core/src/main/java/com/novoda/tpbot/Executor.java
+++ TelepresenceBot/core/src/main/java/com/novoda/tpbot/Executor.java
@@ -4,6 +4,12 @@ public interface Executor {
 
     void execute(Action action);
 
+    interface DelayedExecutor extends Executor {
+        void executeAfterDelay(Action action, long delay);
+
+        void destroyDelayedAction(Action action);
+    }
+
     interface Action {
         void perform();
     }
