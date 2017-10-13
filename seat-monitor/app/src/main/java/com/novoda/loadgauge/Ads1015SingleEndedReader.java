13e6c6001660030d930606adecd4678fd77c1eb3
Blundell <paul@novoda.com>
2017-10-13 23:19:21 +0100

    trying to understand bit shifting
    AND FAILING - non working code

--- seat-monitor/app/src/main/java/com/novoda/loadgauge/Ads1015SingleEndedReader.java
+++ seat-monitor/app/src/main/java/com/novoda/loadgauge/Ads1015SingleEndedReader.java
@@ -40,18 +40,29 @@ class Ads1015SingleEndedReader implements Ads1015 {
         config |= ADS1015_REG_CONFIG_OS_SINGLE;
 
         // Write config register to the ADC
-        writeRegister(ADS1015_REG_POINTER_CONFIG, (short) config);
+        writeRegister(ADS1015_REG_POINTER_CONFIG, config);
 
         // Wait for the conversion to complete
         delay(CONVERSION_DELAY);
 
         // Read the conversion results
-        // Shift 12-bit results right 4 bits for the ADS1015
-        return readRegister(ADS1015_REG_POINTER_CONVERT) >> BIT_SHIFT;
+        return readRegister(ADS1015_REG_POINTER_CONVERT);
     }
+
+//    static void writeRegister(uint8_t i2cAddress, uint8_t reg, uint16_t value) {
+//        Wire.beginTransmission(i2cAddress);
+//        i2cwrite((uint8_t) reg);
+//        i2cwrite((uint8_t) (value >> 8));
+//        i2cwrite((uint8_t) (value & 0xFF));
+//        Wire.endTransmission();
+//    }
 
     private void writeRegister(int reg, short value) {
         try {
+//            i2cBus.write(new byte[]{(byte) reg}, 1);
+//            i2cBus.write(new byte[]{(byte) (value >> 8)}, 1);
+//            i2cBus.write(new byte[]{(byte) (value & 0xFF)}, 1);
+            
             i2cBus.writeRegWord(reg, value);
         } catch (IOException e) {
             throw new IllegalStateException("Cannot write " + reg + " with value " + value, e);
@@ -62,9 +73,22 @@ class Ads1015SingleEndedReader implements Ads1015 {
         SystemClock.sleep(millis);
     }
 
-    private short readRegister(int reg) {
+//    static uint16_t readRegister(uint8_t i2cAddress, uint8_t reg) {
+//        Wire.beginTransmission(i2cAddress);
+//        i2cwrite(ADS1015_REG_POINTER_CONVERT);
+//        Wire.endTransmission();
+//        Wire.requestFrom(i2cAddress, (uint8_t) 2);
+//        return ((i2cread() << 8) | i2cread());
+//    }
+
+    private int readRegister(int reg) {
         try {
-            return i2cBus.readRegWord(reg);
+            i2cBus.write(new byte[]{(byte) reg}, 1);
+            byte[] out = new byte[2];
+            i2cBus.read(out, 2);
+            short result = (short) ((out[0] << 8) | out[1]);
+            // Shift 12-bit results right 4 bits for the ADS1015
+            return result >>> BIT_SHIFT;
         } catch (IOException e) {
             throw new IllegalStateException("Cannot read " + reg, e);
         }
