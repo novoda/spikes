616374e9ed7eb6cfc59f98c19a164960f7ab99a7
Tobias Heine <tobias@novoda.com>
2018-01-10 17:17:41 +0100

    Reuse width and height from the previous board

---
+++ game-of-life-multiplatform/game-of-life-jvm/src/main/kotlin/com/novoda/gol/presentation/AppTerminalView.kt
@@ -0,0 +1,9 @@
+package com.novoda.gol.presentation
+
+/**
+ * User: tobiasheine
+ * Date: 10.01.18
+ * Time: 16:31
+ */
+class AppTerminalView {
+}
---
+++ game-of-life-multiplatform/game-of-life-jvm/src/main/kotlin/com/novoda/gol/presentation/BoardTerminalView.kt
@@ -0,0 +1,9 @@
+package com.novoda.gol.presentation
+
+/**
+ * User: tobiasheine
+ * Date: 10.01.18
+ * Time: 16:42
+ */
+class BoardTerminalView {
+}
