26779931f82c9165f9b08f718ce8f9b6a6e41c25
Luis G. Valle <luis.gvalle@gmail.com>
2018-01-24 11:30:45 +0000

    Add dummy keyboard layout

--- keyboarddemo/app/src/main/java/com/novoda/spikes/keyboarddemo/NovodaInputMethod.kt
+++ keyboarddemo/app/src/main/java/com/novoda/spikes/keyboarddemo/NovodaInputMethod.kt
@@ -1,11 +1,52 @@
 package com.novoda.spikes.keyboarddemo
 
+import android.annotation.SuppressLint
 import android.inputmethodservice.InputMethodService
+import android.inputmethodservice.Keyboard
+import android.inputmethodservice.KeyboardView
 import android.view.KeyEvent
+import android.view.View
 import android.view.Window
+import android.view.inputmethod.CompletionInfo
 import android.widget.Toast
 
-class NovodaInputMethod : InputMethodService() {
+class NovodaInputMethod : InputMethodService(), KeyboardView.OnKeyboardActionListener {
+    override fun swipeRight() {
+
+    }
+
+    override fun onPress(p0: Int) {
+
+    }
+
+    override fun onRelease(p0: Int) {
+
+    }
+
+    override fun swipeLeft() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun swipeUp() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun swipeDown() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    override fun onKey(primaryCode: Int, keyCodes: IntArray?) {
+        when (primaryCode) {
+            Keyboard.KEYCODE_DELETE -> currentInputConnection?.deleteSurroundingText(1, 0)
+            else -> currentInputConnection?.commitText(primaryCode.toChar().toString(), 1)
+        }
+    }
+
+
+    override fun onText(p0: CharSequence?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         Toast.makeText(applicationContext, event.toString(), Toast.LENGTH_SHORT).show()
@@ -18,6 +59,20 @@ class NovodaInputMethod : InputMethodService() {
 
     override fun onConfigureWindow(win: Window?, isFullscreen: Boolean, isCandidatesOnly: Boolean) {
         super.onConfigureWindow(win, isFullscreen, isCandidatesOnly)
+    }
+
+    override fun onDisplayCompletions(completions: Array<out CompletionInfo>?) {
+        super.onDisplayCompletions(completions)
+    }
+
+    @SuppressLint("InflateParams")
+    override fun onCreateInputView(): View {
+        val inputView: KeyboardView = layoutInflater.inflate(R.layout.keyboard_layout, null) as KeyboardView
+        val keyboard = Keyboard(this, R.xml.qwerty)
+        inputView.setOnKeyboardActionListener(this)
+        inputView.keyboard = keyboard
+
+        return inputView
     }
 
 }
