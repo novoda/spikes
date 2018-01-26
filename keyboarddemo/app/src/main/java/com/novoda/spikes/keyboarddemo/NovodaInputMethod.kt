41fa4fb36bb895b991990e9a1306e7f94c9ebc8e
Luis G. Valle <luis.gvalle@gmail.com>
2018-01-26 14:31:39 +0000

    Clean up

--- keyboarddemo/app/src/main/java/com/novoda/spikes/keyboarddemo/NovodaInputMethod.kt
+++ keyboarddemo/app/src/main/java/com/novoda/spikes/keyboarddemo/NovodaInputMethod.kt
@@ -1,111 +1,58 @@
 package com.novoda.spikes.keyboarddemo
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.inputmethodservice.InputMethodService
 import android.inputmethodservice.Keyboard
 import android.inputmethodservice.KeyboardView
-import android.os.Build
-import android.support.annotation.RequiresApi
-import android.util.Log
-import android.view.KeyEvent
 import android.view.View
-import android.view.Window
-import android.view.inputmethod.CompletionInfo
-import android.view.inputmethod.EditorInfo
-import android.view.textservice.SentenceSuggestionsInfo
-import android.view.textservice.SpellCheckerSession
-import android.view.textservice.SuggestionsInfo
-import android.view.textservice.TextServicesManager
-import android.widget.Toast
-import java.util.*
 
 
-class NovodaInputMethod : InputMethodService(), KeyboardView.OnKeyboardActionListener, SpellCheckerSession.SpellCheckerSessionListener {
-    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
-    override fun onGetSentenceSuggestions(results: Array<out SentenceSuggestionsInfo>?) {
-        val sb = StringBuffer("")
-        if (results != null) {
-            for (result in results) {
-                val n = result.getSuggestionsCount()
-                for (i in 0 until n) {
-                    val m = result.getSuggestionsInfoAt(i).getSuggestionsCount()
-
-                    for (k in 0 until m) {
-                        sb.append(result.getSuggestionsInfoAt(i).getSuggestionAt(k))
-                                .append("\n")
-                    }
-                    sb.append("\n")
-                }
-            }
-        }
-        Log.d("NovodaInputMethod", sb.toString())
-        Toast.makeText(applicationContext, sb.toString(), Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onGetSuggestions(p0: Array<out SuggestionsInfo>?) {
-        val i = 0
-    }
-
-    var session: SpellCheckerSession? = null
-
+class NovodaInputMethod : InputMethodService() {
+    val SPACEBAR = 32
     var composing = StringBuilder()
-    val pirateMap = arrayOf(
-            TranslationRule("hello", "ahoy"),
-            TranslationRule("hi", "yo-ho-ho"),
-            TranslationRule("pardon me", "avast"),
-            TranslationRule("excuse me", "arrr"),
-            TranslationRule("my", "me"),
-            TranslationRule("friend", "me bucko"),
-            TranslationRule("sir", "matey"),
-            TranslationRule("madam", "proud beauty"),
-            TranslationRule("stranger", "scurvy dog"),
-            TranslationRule("officer", "foul blaggart"),
-            TranslationRule("where", "whar"),
-            TranslationRule("is", "be"),
-            TranslationRule("the", "th'"),
-            TranslationRule("you", "ye"),
-            TranslationRule("tell", "be tellin'"),
-            TranslationRule("know", "be knowin'"),
-            TranslationRule("how far", "how many leagues"),
-            TranslationRule("old", "barnacle-covered"),
-            TranslationRule("attractive", "comely"),
-            TranslationRule("happy", "grog-filled"),
-            TranslationRule("nearby", "broadside"),
-            TranslationRule("restroom", "head"),
-            TranslationRule("restaurant", "galley"),
-            TranslationRule("hotel", "fleabag inn"),
-            TranslationRule("pub", "Skull & Scuppers"),
-            TranslationRule("bank", "buried treasure"))
+    val pMap = mapOf(
+            "hello" to "ahoy",
+            "hi" to "yo-ho-ho",
+            "pardon me" to "avast",
+            "excuse me" to "arrr",
+            "my" to "me",
+            "friend" to "me bucko",
+            "sir" to "matey",
+            "madam" to "proud beauty",
+            "stranger" to "scurvy dog",
+            "officer" to "foul blaggart",
+            "where" to "whar",
+            "is" to "be",
+            "the" to "th'",
+            "you" to "ye",
+            "tell" to "be tellin'",
+            "know" to "be knowin'",
+            "how far" to "how many leagues",
+            "old" to "barnacle-covered",
+            "attractive" to "comely",
+            "happy" to "grog-filled",
+            "nearby" to "broadside",
+            "restroom" to "head",
+            "restaurant" to "galley",
+            "hotel" to "fleabag inn",
+            "pub" to "Scumm Bar",
+            "bank" to "buried treasure"
+    )
 
-    override fun swipeRight() {
+    @SuppressLint("InflateParams")
+    override fun onCreateInputView(): View {
+        val inputView: KeyboardView = layoutInflater.inflate(R.layout.keyboard_layout, null) as KeyboardView
+        inputView.keyboard = Keyboard(this, R.xml.qwerty)
+        inputView.setOnKeyboardActionListener(object : SimpleKeyboardActionListener() {
+            override fun onKey(primaryKeycode: Int, keyCodes: IntArray?) {
+                handleKeyCode(primaryKeycode)
+            }
+        })
 
+        return inputView
     }
 
-    override fun onPress(p0: Int) {
-
-    }
-
-    override fun onRelease(p0: Int) {
-
-    }
-
-    override fun swipeLeft() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun swipeUp() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun swipeDown() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-
-    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
-    override fun onKey(primaryCode: Int, keyCodes: IntArray?) {
-
+    private fun handleKeyCode(primaryCode: Int) {
         when (primaryCode) {
             Keyboard.KEYCODE_DELETE -> {
                 currentInputConnection?.deleteSurroundingText(1, 0)
@@ -113,8 +60,7 @@ class NovodaInputMethod : InputMethodService(), KeyboardView.OnKeyboardActionLis
                     composing = StringBuilder(composing.removeRange(composing.length - 1, composing.length))
                 }
             }
-            // Spacebar
-            32 -> {
+            SPACEBAR -> {
                 replacePirate(composing.toString())
                 currentInputConnection?.commitText(primaryCode.toChar().toString(), 1)
                 composing = StringBuilder()
@@ -127,66 +73,10 @@ class NovodaInputMethod : InputMethodService(), KeyboardView.OnKeyboardActionLis
     }
 
     private fun replacePirate(input: String) {
-        val rule : TranslationRule?   = pirateMap.find {
-            it.english.equals(input.trim().toLowerCase(Locale.UK))
+        pMap[input.trim()]?.apply {
+            currentInputConnection?.deleteSurroundingText(input.length, 0)
+            currentInputConnection?.commitText(this, 1)
         }
-
-        if (rule != null) {
-            currentInputConnection?.deleteSurroundingText(rule.english.length, 0)
-            currentInputConnection?.commitText(rule.pirate, 1)
-        }
-//        pirateMap
-//                .filter { it.english.equals(input.toLowerCase(Locale.UK)) }
-//                .map { currentInputConnection?.commitText(it.pirate, 1) }
     }
-
-
-    override fun onText(p0: CharSequence?) {
-        val i = 0
-    }
-
-    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
-    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        onKey(keyCode, null)
-        return false
-    }
-
-    override fun onBindInput() {
-        super.onBindInput()
-    }
-
-    override fun onConfigureWindow(win: Window?, isFullscreen: Boolean, isCandidatesOnly: Boolean) {
-        super.onConfigureWindow(win, isFullscreen, isCandidatesOnly)
-    }
-
-    override fun onDisplayCompletions(completions: Array<out CompletionInfo>?) {
-        super.onDisplayCompletions(completions)
-    }
-
-    @SuppressLint("InflateParams")
-    override fun onCreateInputView(): View {
-        val inputView: KeyboardView = layoutInflater.inflate(R.layout.keyboard_layout, null) as KeyboardView
-        val keyboard = Keyboard(this, R.xml.qwerty)
-        inputView.setOnKeyboardActionListener(this)
-        inputView.keyboard = keyboard
-
-        return inputView
-    }
-
-    override fun onFinishInput() {
-        super.onFinishInput()
-    }
-
-
-    override fun onStartInput(attribute: EditorInfo?, restarting: Boolean) {
-        super.onStartInput(attribute, restarting)
-
-        val tsm = getSystemService(Context.TEXT_SERVICES_MANAGER_SERVICE) as TextServicesManager
-
-        session = tsm.newSpellCheckerSession(null, Locale.ENGLISH, this, true)
-
-
-    }
-
 
 }
---
+++ keyboarddemo/app/src/main/java/com/novoda/spikes/keyboarddemo/SimpleKeyboardActionListener.kt
@@ -0,0 +1,39 @@
+package com.novoda.spikes.keyboarddemo
+
+import android.inputmethodservice.KeyboardView
+
+abstract class SimpleKeyboardActionListener : KeyboardView.OnKeyboardActionListener {
+    
+    override fun swipeRight() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onPress(p0: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onRelease(p0: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun swipeLeft() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun swipeUp() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun swipeDown() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onKey(primaryKeycode: Int, keyCodes: IntArray?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onText(p0: CharSequence?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
