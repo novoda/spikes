8419ad82851bb7a3599d7b0011f004c774de74c6
Tobias Heine <tobias@novoda.com>
2018-02-07 16:14:34 +0100

    Add Game of Life core kotlin implementation

---
+++ game-of-life-native/kotlin/com/novoda/gol/patterns/AbstractPattern.kt
@@ -0,0 +1,31 @@
+package com.novoda.gol.patterns
+
+import com.novoda.gol.data.CellMatrix
+
+abstract class AbstractPattern(private val cellMatrix: CellMatrix) : PatternEntity {
+
+    override fun cellAtPosition(x: Int, y: Int) = cellMatrix.cellAtPosition(x, y)
+
+    override fun getWidth() = cellMatrix.getWidth()
+
+    override fun getHeight() = cellMatrix.getHeight()
+
+    override fun getSeeds() = cellMatrix.getSeeds()
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+
+        other as AbstractPattern
+
+        if (cellMatrix != other.cellMatrix) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return cellMatrix.hashCode()
+    }
+
+
+}
