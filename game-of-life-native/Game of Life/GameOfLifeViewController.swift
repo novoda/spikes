118725c3488f6e1501740729f2b1f8a4a554c4c4
Tobias Heine <tobias@novoda.com>
2018-02-08 14:39:07 +0100

    Setup views as part of init

--- game-of-life-native/Game of Life/GameOfLifeViewController.swift
+++ game-of-life-native/Game of Life/GameOfLifeViewController.swift
@@ -11,59 +11,61 @@ import Foundation
 import KotlinGameOfLife
 
 class GameOfLifeViewController: UIViewController, KGOLAppView {
-    
-    private var appPresenter : KGOLAppPresenter?
-    private let controlButton = UIButton(frame: CGRect(x: 100, y: 100, width: 200, height: 50))
-    private let boardView = UIBoard(frame: CGRect(x: 0, y: 150, width: 300, height: 300))
-    
+
+    private var appPresenter: KGOLAppPresenter?
+    private let controlButton: UIButton
+    private let boardView: UIBoard
+
     var onControlButtonClicked: () -> KGOLStdlibUnit = {
         return KGOLStdlibUnit()
     }
-    var onPatternSelected: (KGOLPatternEntity) -> KGOLStdlibUnit = {_ in
-        return  KGOLStdlibUnit()
+    var onPatternSelected: (KGOLPatternEntity) -> KGOLStdlibUnit = { _ in
+        return KGOLStdlibUnit()
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
+        controlButton = UIButton(frame: CGRect(x: 100, y: 100, width: 200, height: 50))
+        controlButton.backgroundColor = .green
+        boardView = UIBoard(frame: CGRect(x: 0, y: 150, width: 300, height: 300))
+        boardView.backgroundColor = .blue
         super.init(coder: aDecoder)
+
+        controlButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
     }
-    
+
     func renderControlButtonLabel(controlButtonLabel: String) {
         controlButton.setTitle(controlButtonLabel, for: .normal)
     }
-    
+
     func renderPatternSelectionVisibility(visibility: Bool) {
-        
+
     }
-    
+
     func renderBoardWith(boardViewInput: KGOLBoardViewInput) {
         boardView.renderBoard(boardViewInput: boardViewInput)
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if (appPresenter == nil){
+        view.addSubview(controlButton)
+        view.addSubview(boardView)
+
+        if (appPresenter == nil) {
             let model = KGOLAppModel()
             appPresenter = KGOLAppPresenter(model: model)
-            controlButton.backgroundColor = .green
-            controlButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
-            self.view.addSubview(controlButton)
-            
-            boardView.backgroundColor = .blue
-            self.view.addSubview(boardView)
         }
-        
+
         appPresenter?.bind(view: self)
         boardView.willAppear()
     }
-    
+
     @objc func buttonAction(sender: UIButton!) {
         onControlButtonClicked()
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         appPresenter?.unbind(view: self)
         boardView.willDisAppear()
     }
-    
+
 }
