912068e52be405bf9264ced67f7cb2912b0f24c9
Tobias Heine <tobias@novoda.com>
2018-02-13 11:20:31 +0100

    Implement GameLoopImpl based on NSTimer

--- game-of-life-multiplatform/common-native/src/main/kotlin/com/novoda/gol/GameLoop.kt
+++ game-of-life-multiplatform/common-native/src/main/kotlin/com/novoda/gol/GameLoop.kt
@@ -1,16 +1,25 @@
 package com.novoda.gol
 
+import platform.Foundation.*
+
 actual class GameLoopImpl : GameLoop {
 
+    private var timer: NSTimer? = null
 
     override var onTick: () -> Unit = {}
 
     override fun startWith(intervalMs: Int) {
+        timer = NSTimer(NSDate(), 0.5, true, {
+            onTick()
+        })
+        NSRunLoop.currentRunLoop().addTimer(timer!!, NSRunLoopCommonModes)
     }
 
     override fun stop() {
+        timer?.invalidate()
+        timer = null
     }
 
-    override fun isLooping() = true
+    override fun isLooping() = timer != null
 
 }
