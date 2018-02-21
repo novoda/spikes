1e5978b4de8bb4e8941154f8b55204f89e64a288
Tobias Heine <tobias@novoda.com>
2018-02-21 14:33:33 +0100

    Consume blog posts using rss-parser

--- portfolio-diversity-dashboard-plugin/lib/data-source.js
+++ portfolio-diversity-dashboard-plugin/lib/data-source.js
@@ -1,11 +1,25 @@
-const generateViewState = (configuration) => {
-    console.log(configuration)    
-    const viewState = {
+const Rss = require('rss-parser')
+
+const parseBlogPosts = (url) => {
+    return parseRss(url).then(rss => {
+        return rss.feed.entries;
+    }).then(toItem)
+}
+
+const parseRss = (url) => {
+    return new Promise((resolve, reject) => {
+        Rss.parseURL(url, (err, result) => {
+            err ? reject(err) : resolve(result)
+        })
+    })
+}
+
+const toItem = (items) => {
+    return {
         numWebMentions: 100,
         numAndroidMentions: 200,
         numIosMentions: 300,
     }
-    return Promise.resolve(viewState)
 }
 
-module.exports = generateViewState
+module.exports = () => parseBlogPosts('https://www.novoda.com/blog/rss/')
