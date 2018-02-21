749807e550e2878e4ca0f78c4e70c53da1a6040f
Tobias Heine <tobias@novoda.com>
2018-02-21 15:17:15 +0100

    Extract mentions from blog posts

--- portfolio-diversity-dashboard-plugin/lib/data-source.js
+++ portfolio-diversity-dashboard-plugin/lib/data-source.js
@@ -7,7 +7,7 @@ const iosTags = ["iOS","XCode","swift"]
 const extractDiversityFrom = (url) => {
     return parseRss(url).then(rss => {
         return rss.feed.entries;
-    }).then(toItem)
+    }).then(toViewState)
 }
 
 const parseRss = (url) => {
@@ -18,11 +18,39 @@ const parseRss = (url) => {
     })
 }
 
-const toItem = (items) => {
+const toViewState = (items) => {
+    var androidMentions=0
+    var webMentions=0
+    var iosMentions=0
+    
+    for (var itemIndex = 0; itemIndex < items.length; itemIndex++) {
+        var item = items[itemIndex]
+        if (item.categories != null){
+            for (var categoryIndex = 0; categoryIndex < item.categories.length; categoryIndex++){
+                var category = items[itemIndex].categories[categoryIndex]
+                var isAndroidMention = (androidTags.indexOf(category) > -1);
+                var isIosMention = (iosTags.indexOf(category) > -1);
+                var isWebMention = (webTags.indexOf(category) > -1);
+
+                if (isAndroidMention){
+                    androidMentions++        
+                }
+
+                if (isIosMention){
+                    iosMentions++        
+                }
+
+                if (isWebMention){
+                    webMentions++        
+                }
+            }
+        }
+    }
+        
     return {
-        numWebMentions: 100,
-        numAndroidMentions: 200,
-        numIosMentions: 300,
+        webMentions: webMentions,
+        androidMentions: androidMentions,
+        iosMentions: iosMentions,
     }
 }
 
