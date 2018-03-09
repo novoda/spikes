4f2cde0d01196857d4c00d5b7f891b30b1263e56
Dimitrios Karavias <d.karavias@yahoo.gr>
2018-03-09 15:44:33 +0000

    IMPORTANT: This only runs on Android Studio 3.1 (current RC) or above Refactored EspressoTestExample.kt to use Page Objects.

--- TestAutomationStarter/app/src/androidTest/java/com/novoda/androidstoreexample/tests/EspressoTestExample.kt
+++ TestAutomationStarter/app/src/androidTest/java/com/novoda/androidstoreexample/tests/EspressoTestExample.kt
@@ -1,17 +1,10 @@
 package com.novoda.androidstoreexample.tests
 
-import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.action.ViewActions.click
-import android.support.test.espresso.assertion.ViewAssertions.matches
-import android.support.test.espresso.contrib.RecyclerViewActions.actionOnHolderItem
-import android.support.test.espresso.contrib.RecyclerViewActions.scrollToHolder
-import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
-import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
-import com.novoda.androidstoreexample.R
-import com.novoda.androidstoreexample.ViewMatchers
 import com.novoda.androidstoreexample.activities.MainActivity
+import com.novoda.androidstoreexample.pageobjects.MainActivityPageObject
+import com.novoda.androidstoreexample.pageobjects.ProductListPageObject
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -19,23 +12,23 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class EspressoTestExample {
 
+    private val mainActivityPageObject = MainActivityPageObject()
+    private val productListPageObject = ProductListPageObject()
+
     private val activityTestRule = ActivityTestRule<MainActivity>(MainActivity::class.java)
 
-
-    @get:Rule @JvmField
+    @get:Rule
     var activityRule: ActivityTestRule<MainActivity> = activityTestRule
 
 
     @Test
     fun firstNavigationTest() {
-        val categoryMatcher = ViewMatchers.withCategoryTitle("HATS")
 
-        onView(withId(R.id.categoryListView)).perform(scrollToHolder(categoryMatcher), actionOnHolderItem(categoryMatcher, click()))
+        mainActivityPageObject.navigateToProductList()
 
-        val productMatcher = ViewMatchers.withProductTitle("hat white")
+        productListPageObject.navigateToProductDetails()
 
-        onView(withId(R.id.productListView)).perform(scrollToHolder(productMatcher), actionOnHolderItem(productMatcher, click()))
+        productListPageObject.assertProductDetailsDisplayed()
 
-        onView(withId(R.id.productDetailDescription)).check(matches(isDisplayed()))
     }
 }
