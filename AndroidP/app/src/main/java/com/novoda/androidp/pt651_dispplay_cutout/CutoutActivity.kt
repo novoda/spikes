1c10e999413f3e74431839c8315b83dfe3846a2a
Luis G. Valle <luis.gvalle@gmail.com>
2018-03-14 17:18:52 +0000

    Clean up demo & add 'use inset' options

--- AndroidP/app/src/main/java/com/novoda/androidp/pt651_dispplay_cutout/CutoutActivity.kt
+++ AndroidP/app/src/main/java/com/novoda/androidp/pt651_dispplay_cutout/CutoutActivity.kt
@@ -1,53 +1,84 @@
 package com.novoda.androidp.pt651_dispplay_cutout
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.WindowManager
-import android.widget.Toast
+import android.widget.RadioGroup
 import com.novoda.androidp.R
 
 class CutoutActivity : AppCompatActivity() {
 
     private lateinit var linearLayout: View
     private val CUTOUT_MODE = "cutout_mode"
+    private val CUTOUT_USE_INSET = "cutout_use_inset"
     private val CUTOUT_MODE_DEFAULT = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_DEFAULT
+
+    val cutoutModeMap = mapOf(
+            R.id.rd_cutout_default to WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_DEFAULT,
+            R.id.rd_cutout_always to WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS,
+            R.id.rd_cutout_never to WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_NEVER)
+
+    val cutoutUseInsetMap = mapOf(
+            R.id.rd_cutout_yes to true,
+            R.id.rd_cutout_no to false
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cutout)
-        
-        val pref = PreferenceManager.getDefaultSharedPreferences(this)
-        val cutoutMode = pref.getInt(CUTOUT_MODE, CUTOUT_MODE_DEFAULT)
-        window.attributes.layoutInDisplayCutoutMode = cutoutMode
-
-        findViewById<View>(R.id.btn_cutout_always).setOnClickListener {
-            pref.edit().putInt(CUTOUT_MODE, WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_ALWAYS).commit()
-            restart()
-        }
-
-        findViewById<View>(R.id.btn_cutout_never).setOnClickListener {
-            pref.edit().putInt(CUTOUT_MODE, WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_NEVER).commit()
-            restart()
-        }
-
-        findViewById<View>(R.id.btn_cutout_default).setOnClickListener {
-            pref.edit().putInt(CUTOUT_MODE, WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_DEFAULT).commit()
-            restart()
-        }
         linearLayout = findViewById(R.id.main_layout)
-        linearLayout.setOnApplyWindowInsetsListener { view, windowInsets ->
+
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        setLayoutCutoutMode(pref)
+        setupTitleView(pref)
+        setupRadioButtonForCutoutMode(pref)
+        setupRadioButtonForInset(pref)
+
+    }
+
+    private fun setLayoutCutoutMode(pref: SharedPreferences) {
+        window.attributes.layoutInDisplayCutoutMode = cutoutModeMap[pref.getInt(CUTOUT_MODE, CUTOUT_MODE_DEFAULT)] ?: CUTOUT_MODE_DEFAULT
+    }
+
+    private fun setupTitleView(pref: SharedPreferences) {
+        val titleView = findViewById<View>(R.id.tv_cutout_title)
+        titleView.setOnApplyWindowInsetsListener { view, windowInsets ->
             windowInsets.displayCutout?.let {
-                Toast.makeText(this, windowInsets.displayCutout.bounds.toString(), Toast.LENGTH_SHORT).show()
+                if (isUseInsetSelected(pref)) {
+                    val safeInsetTop = windowInsets.displayCutout.safeInsetTop
+                    titleView.y = titleView.top + safeInsetTop.toFloat()
+                }
             }
 
             windowInsets
         }
+    }
 
+    private fun isUseInsetSelected(pref: SharedPreferences) = cutoutUseInsetMap[pref.getInt(CUTOUT_USE_INSET, 0)] ?: false
+
+    private fun setupRadioButtonForCutoutMode(pref: SharedPreferences) {
+        val radioGroup = findViewById<RadioGroup>(R.id.rd_cutout_mode)
+        radioGroup.check(pref.getInt(CUTOUT_MODE, R.id.rd_cutout_default))
+        radioGroup.setOnCheckedChangeListener { radioGroup, checkedId ->
+            pref.edit().putInt(CUTOUT_MODE, checkedId).commit()
+            restart()
+        }
+    }
+
+    private fun setupRadioButtonForInset(pref: SharedPreferences) {
+        val radioGroup = findViewById<RadioGroup>(R.id.rd_cutout)
+        radioGroup.check(pref.getInt(CUTOUT_USE_INSET, R.id.rd_cutout_no))
+        radioGroup.setOnCheckedChangeListener { radioGroup, checkedId ->
+            pref.edit().putInt(CUTOUT_USE_INSET, checkedId).commit()
+            restart()
+        }
     }
 
     private fun restart() {
+        linearLayout.requestLayout();
         startActivity(getIntent());
         finish();
     }
