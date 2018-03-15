52820562465d0bd1e41e82f7e005a083a8890683
Berta Devant <berta@novoda.com>
2018-03-15 13:22:24 +0100

    remove code

--- FastlaneExample/FastlaneExample/ViewController.swift
+++ FastlaneExample/FastlaneExample/ViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        button.titleLabel?.text = "Pressed"
+    }
 
 }
 
