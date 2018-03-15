01d348451af68c67045b496b9fcb0625cea0d284
Berta Devant <berta@novoda.com>
2018-03-15 11:09:51 +0100

    add fastlane project to the repo

---
+++ FastlaneExample/FastlaneExampleTests/FastlaneExampleTests.swift
@@ -0,0 +1,35 @@
+//
+//  FastlaneExampleTests.swift
+//  FastlaneExampleTests
+//
+//  Created by Berta Devant on 13/03/2018.
+//  Copyright © 2018 Berta Devant. All rights reserved.
+//
+
+import XCTest
+@testable import FastlaneExample
+
+class FastlaneExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        XCTAssertTrue(true)
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
