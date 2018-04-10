57bac326fd87696a35bcf7d384e3d7ce9c52c612
lgvalle <luis.gvalle@gmail.com>
2018-04-10 12:27:24 +0100

    Check if Camera Permissions has been granted

--- ARCore/app/src/main/java/com/novoda/spikes/arcore/MainActivity.kt
+++ ARCore/app/src/main/java/com/novoda/spikes/arcore/MainActivity.kt
@@ -20,23 +20,40 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        checkARCoreIsInstalled()
+        checkAREnvironment()
     }
 
-    private fun checkARCoreIsInstalled() {
+    private fun checkAREnvironment() {
+        if (isARCoreIsInstalled() && isCameraPermissionGranted()) {
+            Toast.makeText(this, "All good, can do AR stuff now", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // ARCore requires camera permissions to operate.
+    private fun isCameraPermissionGranted(): Boolean {
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            CameraPermissionHelper.requestCameraPermission(this)
+            return false
+        }
+        return true
+    }
+
+    private fun isARCoreIsInstalled(): Boolean {
         try {
             val requestInstall = ArCoreApk.getInstance().requestInstall(this, shouldRequestARCoreInstall)
             // We only care about 'INSTALL_REQUESTED'
             // Nothing to do if the returned value is 'INSTALLED'
             if (requestInstall == INSTALL_REQUESTED) {
                 shouldRequestARCoreInstall = false
+                return false
             }
 
         } catch (exception: Exception) {
             Log.e(TAG, "Exception creating session", exception)
             Toast.makeText(this, messageFromExceptionType(exception), Toast.LENGTH_SHORT).show()
-            return
+            return false
         }
+        return true
     }
 
     private fun messageFromExceptionType(exception: Exception): String {
@@ -47,6 +64,17 @@ class MainActivity : AppCompatActivity() {
             is UnavailableSdkTooOldException -> "Please update this app"
             is UnavailableDeviceNotCompatibleException -> "This device does not support AR"
             else -> "Failed to create AR session"
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, results: IntArray) {
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG).show()
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                CameraPermissionHelper.launchPermissionSettings(this)
+            }
+            finish()
         }
     }
 }
