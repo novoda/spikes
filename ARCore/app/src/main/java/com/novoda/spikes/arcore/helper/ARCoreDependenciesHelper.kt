c39880696dd702013eb675d94074f246696e5dc2
lgvalle <luis.gvalle@gmail.com>
2018-04-10 17:13:16 +0100

    Clean up ARCoreDependenciesHelper

--- ARCore/app/src/main/java/com/novoda/spikes/arcore/helper/ARCoreDependenciesHelper.kt
+++ ARCore/app/src/main/java/com/novoda/spikes/arcore/helper/ARCoreDependenciesHelper.kt
@@ -4,37 +4,34 @@ import android.app.Activity
 import com.google.ar.core.ArCoreApk
 import com.google.ar.core.exceptions.*
 
-class ARCoreDependenciesHelper {
-
+object ARCoreDependenciesHelper {
     class Result(val isARCoreInstalled: Boolean, val message: String)
 
-    companion object {
-        private var shouldRequestARCoreInstall = true
-        fun isARCoreIsInstalled(activity: Activity): Result {
-            try {
-                val requestInstall = ArCoreApk.getInstance().requestInstall(activity, shouldRequestARCoreInstall)
-                // We only care about 'INSTALL_REQUESTED'. Nothing to do if the returned value is 'INSTALLED'
-                if (requestInstall == ArCoreApk.InstallStatus.INSTALL_REQUESTED) {
-                    shouldRequestARCoreInstall = false
-                    return Result(false, "ARCore SDK install requested")
-                }
+    private var shouldRequestARCoreInstall = true
 
-            } catch (exception: Exception) {
-                return Result(false, messageFromExceptionType(exception))
+    fun isARCoreIsInstalled(activity: Activity): Result {
+        try {
+            val requestInstall = ArCoreApk.getInstance().requestInstall(activity, shouldRequestARCoreInstall)
+            // We only care about 'INSTALL_REQUESTED'. Nothing to do if the returned value is 'INSTALLED'
+            if (requestInstall == ArCoreApk.InstallStatus.INSTALL_REQUESTED) {
+                shouldRequestARCoreInstall = false
+                return Result(false, "ARCore SDK install requested")
             }
-            return Result(true, "ARCore SDK installed")
+
+        } catch (exception: Exception) {
+            return Result(false, messageFromExceptionType(exception))
         }
+        return Result(true, "ARCore SDK installed")
+    }
 
-        private fun messageFromExceptionType(exception: Exception): String {
-            return when (exception) {
-                is UnavailableUserDeclinedInstallationException -> "Please install ARCore"
-                is UnavailableArcoreNotInstalledException -> "Please install ARCore"
-                is UnavailableApkTooOldException -> "Please update ARCore"
-                is UnavailableSdkTooOldException -> "Please update this app"
-                is UnavailableDeviceNotCompatibleException -> "This device does not support AR"
-                else -> "Failed to create AR session"
-            }
+    private fun messageFromExceptionType(exception: Exception): String {
+        return when (exception) {
+            is UnavailableUserDeclinedInstallationException -> "Please install ARCore"
+            is UnavailableArcoreNotInstalledException -> "Please install ARCore"
+            is UnavailableApkTooOldException -> "Please update ARCore"
+            is UnavailableSdkTooOldException -> "Please update this app"
+            is UnavailableDeviceNotCompatibleException -> "This device does not support AR"
+            else -> "Failed to create AR session"
         }
     }
-    
 }
