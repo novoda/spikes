4fd104b53d6f9358b47c417e038e2557619cba67
Blundell <paul@novoda.com>
2018-04-26 21:33:04 +0100

    add back class i deleted that we need :-) sorry

---
+++ dungeon-crawler/android_things/src/main/java/com/novoda/dungeoncrawler/RunningMedian.java
@@ -0,0 +1,22 @@
+package com.novoda.dungeoncrawler;
+
+// https://playground.arduino.cc/Main/RunningMedian
+public class RunningMedian {
+    private int median;
+    private int highest;
+
+    public RunningMedian(int i) {
+    }
+
+    public void add(int a) {
+
+    }
+
+    public int getMedian() {
+        return median;
+    }
+
+    public int getHighest() {
+        return highest;
+    }
+}
