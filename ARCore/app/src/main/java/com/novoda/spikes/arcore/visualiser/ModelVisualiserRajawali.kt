d3d0014596649209a4cf433e038376903089a403
lgvalle <luis.gvalle@gmail.com>
2018-05-01 09:23:51 +0100

    WIP mtl textures

--- ARCore/app/src/main/java/com/novoda/spikes/arcore/visualiser/ModelVisualiserRajawali.kt
+++ ARCore/app/src/main/java/com/novoda/spikes/arcore/visualiser/ModelVisualiserRajawali.kt
@@ -3,13 +3,14 @@ package com.novoda.spikes.arcore.visualiser
 import android.content.Context
 import android.graphics.Color
 import android.view.MotionEvent
-import com.google.ar.core.*
+import com.google.ar.core.Anchor
 import com.novoda.spikes.arcore.R
 import com.novoda.spikes.arcore.google.helper.TapHelper
 import org.rajawali3d.Object3D
 import org.rajawali3d.lights.DirectionalLight
 import org.rajawali3d.loader.LoaderOBJ
 import org.rajawali3d.materials.Material
+import org.rajawali3d.materials.methods.DiffuseMethod
 import org.rajawali3d.materials.textures.Texture
 import org.rajawali3d.materials.textures.TextureManager
 import org.rajawali3d.math.vector.Vector3
@@ -38,26 +39,54 @@ class ModelVisualiserRajawali(
     private lateinit var droid: Object3D
 
     override fun init() {
-//        val objParser = LoaderOBJ(context.resources, textureManager, R.raw.trophy_obj) // TEXTURAS
-////        val objParser = LoaderOBJ(this, R.raw.andy)
-//        objParser.parse()
-//
-//
-//        droid = objParser.parsedObject
-////        droid = objParser.parsedObject.getChildAt(0) // TEXTURAS
-//        droid.setPosition(0.0, 0.0, -1.0)
-//        droid.material = Material().apply {
-//            addTexture(Texture("droid", R.raw.andy_texture))
-//            color = Color.BLACK
-//
-//            diffuseMethod = DiffuseMethod.Lambert()
-//            enableLighting(true)
-//            colorInfluence = 0f
-//        }
-//
-////        currentScene.addLight(mDirectionalLight)
+        droid = createTrophy()
 
-        //  Spawn droid object in front of you
+        //createDroid()
+
+        val newDroid = myClone(droid)
+        newDroid.setPosition(0.5, 0.0, -1.0)
+        newDroid.material = Material().apply {
+            //            addTexture(Texture("droid", R.raw.andy_texture))
+//            color = Color.BLACK
+            diffuseMethod = DiffuseMethod.Lambert()
+            enableLighting(true)
+            colorInfluence = 0f
+        }
+
+        renderer.currentScene.addChild(droid)
+        renderer.currentScene.addChild(newDroid)
+    }
+
+    private fun myClone(obj: Object3D): Object3D {
+        val newObj = obj.clone()
+
+        for (i in 0 until obj.numChildren) {
+            newObj.addChild(obj.getChildAt(i).clone())
+        }
+        return newObj
+    }
+
+    private fun createTrophy(): Object3D {
+        val objParser = LoaderOBJ(context.resources, textureManager, R.raw.trophy_obj) // TEXTURAS
+        objParser.parse()
+
+
+        val droid = objParser.parsedObject
+        droid.setPosition(0.0, 0.0, -1.0)
+        droid.material = Material().apply {
+            //            addTexture(Texture("droid", R.raw.andy_texture))
+//            color = Color.BLACK
+            diffuseMethod = DiffuseMethod.Lambert()
+            enableLighting(true)
+            colorInfluence = 0f
+        }
+
+        renderer.currentScene.addLight(mDirectionalLight)
+
+        return droid
+    }
+
+    private fun createDroid(): Object3D {
         val objParser = LoaderOBJ(renderer, R.raw.andy)
         objParser.parse()
 
@@ -70,7 +99,7 @@ class ModelVisualiserRajawali(
 
         }
 
-        renderer.currentScene.addChild(droid)
+        return droid
     }
 
     override fun onAnchorCreated(anchor: Anchor) {
@@ -80,7 +109,7 @@ class ModelVisualiserRajawali(
 
 
         // Spawn new droid object at anchor position
-        val newDroid = droid.clone().apply {
+        val newDroid = myClone(droid).apply {
             setPosition(translation[0].toDouble(), translation[1].toDouble(), translation[2].toDouble())
         }
         renderer.currentScene.addChild(newDroid)
