e20e92f934f42891c7ec603253bf02a3b786977f
lgvalle <luis.gvalle@gmail.com>
2018-05-02 17:15:32 +0100

    Add class to manage torch flash

--- HelloTorchIOS/HelloTorchIOS/StrobeLights.swift
+++ HelloTorchIOS/HelloTorchIOS/StrobeLights.swift
@@ -1,9 +1,74 @@
-//
-//  StrobeLights.swift
-//  HelloTorchIOS
-//
-//  Created by Luis Valle on 02/05/2018.
-//  Copyright © 2018 Novoda. All rights reserved.
-//
-
 import Foundation
+import AVFoundation
+
+class StrobeLights {
+    var counter: Int = 0
+    var timer: Timer
+    var isStrobing: Bool
+    var isLightOn: Bool
+    var frequency: Double
+    var start = DispatchTime.now()
+    var end = DispatchTime.now()
+    var active: Bool
+    var device: AVCaptureDevice
+    
+    init (){
+        self.counter = 0
+        self.timer = Timer()
+        self.isStrobing = false
+        self.isLightOn = false
+        self.frequency = 500
+        self.active = false
+        self.device = AVCaptureDevice.default(for: AVMediaType.video)!
+        do {
+            try device.lockForConfiguration()
+        }catch {
+            print("Torch could not be used")
+        }
+    }
+    
+    // Start Strobe process
+    func toggleStrobe () {
+        if (device.hasTorch == false) {
+            print( "torch unavailable")
+            return
+        }
+        
+        print("Is light on: \(self.isLightOn)")
+        if (self.isLightOn == true) {
+            self.isLightOn = false
+            self.timer.invalidate()
+            print("Turning timer off")
+            self.end = DispatchTime.now()
+            let nanoTime = end.uptimeNanoseconds - start.uptimeNanoseconds
+            let timeInterval = Double(nanoTime) / 1_000_000_000
+            print("I counted this high \(counter) in this many seconds \(timeInterval) ")
+            counter = 0
+            incrementCounter()
+        } else {
+            self.isLightOn = true
+            self.timer = Timer.scheduledTimer(timeInterval: 1/frequency, target: self, selector: #selector(incrementCounter), userInfo: nil, repeats: true)
+            print("Turning timer on")
+            self.start = DispatchTime.now()
+        }
+    }
+    
+    // Increase counter by one
+    @objc func incrementCounter () {
+        self.toggleTorch(on: false)
+        self.counter += 1
+        self.toggleTorch(on: true)
+    }
+    // Turns light on or off
+    func toggleTorch(on: Bool ) {
+        if on == true {
+            do {
+                try device.setTorchModeOn(level: 0.5)
+            }catch {
+                print("Torch could not be used")
+            }
+        } else {
+            device.torchMode = .off
+        }
+    }
+}
--- HelloTorchIOS/HelloTorchIOS/ViewController.swift
+++ HelloTorchIOS/HelloTorchIOS/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let torch = StrobeLights()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +23,8 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func toggleTorch(sender: UIButton) {
+        self.torch.toggleStrobe()
+    }
 }
 
