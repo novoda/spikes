50b64c6b61e68a95a549e26ab128a3152e255765
lgvalle <luis.gvalle@gmail.com>
2018-05-10 14:58:27 +0100

    Extract functions

--- HelloTorchIOS/HelloTorchIOS/SwitchCounter.swift
+++ HelloTorchIOS/HelloTorchIOS/SwitchCounter.swift
@@ -11,27 +11,50 @@ class SwitchCounter {
     private var end = DispatchTime.now()
     private var isCounting = false
     
-    // Start Strobe process
     func run () {
         if (isCounting) {
-            print("Turning timer off")
-            isCounting = false
-            timer.invalidate()
-            
-            end = DispatchTime.now()
-            let nanoTime = end.uptimeNanoseconds - start.uptimeNanoseconds
-            let timeInterval = Double(nanoTime) / 1_000_000_000
-            print("I counted this high \(counter) in this many seconds \(timeInterval) ")
-            counter = 0
+            stopCounting()
+            stopTimer()
+            printCounter()
+            resetCounter()
         } else {
             print("Turning timer on")
-            isCounting = true
-            timer = Timer.scheduledTimer(timeInterval: 1/frequency, target: self, selector: #selector(incrementCounter), userInfo: nil, repeats: true)
-            start = DispatchTime.now()
+            startCounting()
+            startTimer()
         }
     }
     
-    // Increase counter by one
+    fileprivate func stopCounting() {
+        isCounting = false
+    }
+    
+    fileprivate func startCounting() {
+        isCounting = true
+    }
+    
+    fileprivate func stopTimer() {
+        timer.invalidate()
+        end = DispatchTime.now()
+    }
+    
+    fileprivate func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1/frequency, target: self, selector: #selector(incrementCounter), userInfo: nil, repeats: true)
+        start = DispatchTime.now()
+    }
+    
+    fileprivate func printCounter() {
+        print("I counted this high \(counter) in this many seconds \(timeInterval()) ")
+    }
+    
+    fileprivate func timeInterval() -> Double {
+        let nanoTime = end.uptimeNanoseconds - start.uptimeNanoseconds
+        return Double(nanoTime) / 1_000_000_000
+    }
+    
+    fileprivate func resetCounter() {
+        counter = 0
+    }
+    
     @objc func incrementCounter () {
         lightsLogic.toggleStrobe()
         counter += 1
