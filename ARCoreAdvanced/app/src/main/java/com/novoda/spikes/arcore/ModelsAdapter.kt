10c0880bf0f12eb7e40d65fc06d174611b96a882
Dorvaryn <benjamin.augustin@gmail.com>
2018-05-10 11:18:43 +0100

    Rename ViewHolder

--- ARCoreAdvanced/app/src/main/java/com/novoda/spikes/arcore/ModelsAdapter.kt
+++ ARCoreAdvanced/app/src/main/java/com/novoda/spikes/arcore/ModelsAdapter.kt
@@ -9,17 +9,17 @@ import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.novoda.spikes.arcore.poly.PolyAsset
 
-class ModelsAdapter(private val layoutInflater: LayoutInflater, private val listener: Listener): RecyclerView.Adapter<FooVH>() {
+class ModelsAdapter(private val layoutInflater: LayoutInflater, private val listener: Listener): RecyclerView.Adapter<ModelVH>() {
 
     private var models = emptyList<PolyAsset>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FooVH {
-        return FooVH(layoutInflater.inflate(R.layout.model_item_view, parent, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ModelVH {
+        return ModelVH(layoutInflater.inflate(R.layout.model_item_view, parent, false))
     }
 
     override fun getItemCount() = models.size
 
-    override fun onBindViewHolder(holder: FooVH, position: Int) {
+    override fun onBindViewHolder(holder: ModelVH, position: Int) {
         val model = models[position]
         holder.modelLabelView.text = "${model.displayName} by ${model.authorName}\nSearched as ${model.representsLabel}"
         holder.itemView.setOnClickListener {
@@ -39,7 +39,7 @@ class ModelsAdapter(private val layoutInflater: LayoutInflater, private val list
 
 }
 
-class FooVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class ModelVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     val thumbnailView: ImageView = itemView.findViewById(R.id.thumbnailView)
     val modelLabelView: TextView = itemView.findViewById(R.id.modelLabelView)
