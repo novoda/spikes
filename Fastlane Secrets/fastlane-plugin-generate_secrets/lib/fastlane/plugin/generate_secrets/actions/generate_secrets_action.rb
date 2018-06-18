e5bdefc8cd1568d807e984f98e7cf144be07eb0c
amlcurran <aml.curran@gmail.com>
2018-06-18 11:07:17 +0100

    Adds plugin structure

---
+++ Fastlane Secrets/fastlane-plugin-generate_secrets/lib/fastlane/plugin/generate_secrets/actions/generate_secrets_action.rb
@@ -0,0 +1,43 @@
+require 'fastlane/action'
+require_relative '../helper/generate_secrets_helper'
+
+module Fastlane
+  module Actions
+    class GenerateSecretsAction < Action
+      def self.run(params)
+        UI.message("The generate_secrets plugin is working!")
+      end
+
+      def self.description
+        "Generate a file containing secret values from the environment"
+      end
+
+      def self.authors
+        ["amlcurran"]
+      end
+
+      def self.return_value
+        # If your method provides a return value, you can describe here what it does
+      end
+
+      def self.details
+        # Optional:
+        "Use this plugin to build a file which contains secret or confidential keys, which can be supplied by your CI environment to avoid leaking secrets!"
+      end
+
+      def self.available_options
+        [
+          # FastlaneCore::ConfigItem.new(key: :your_option,
+          #                         env_name: "GENERATE_SECRETS_YOUR_OPTION",
+          #                      description: "A description of your option",
+          #                         optional: false,
+          #                             type: String)
+        ]
+      end
+
+      def self.is_supported?(platform)
+        [:ios].include?(platform)
+      end
+    end
+  end
+end
