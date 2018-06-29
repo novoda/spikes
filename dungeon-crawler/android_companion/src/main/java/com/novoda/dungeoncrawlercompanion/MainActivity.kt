837c5cbf250186542c91d0b9700fab33f4d18664
Xavi Rigau <xavi@novoda.com>
2018-06-29 21:50:35 +0100

    Remove unnecessary fragment and views

--- dungeon-crawler/android_companion/src/main/java/com/novoda/dungeoncrawlercompanion/MainActivity.kt
+++ dungeon-crawler/android_companion/src/main/java/com/novoda/dungeoncrawlercompanion/MainActivity.kt
@@ -1,30 +1,13 @@
 package com.novoda.dungeoncrawlercompanion
 
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-        navigation.selectedItemId = R.id.navigation_game
     }
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_game -> {
-                supportFragmentManager.beginTransaction().replace(R.id.content, GameFragment()).commit()
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_about -> {
-                supportFragmentManager.beginTransaction().replace(R.id.content, AboutFragment()).commit()
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
 }
