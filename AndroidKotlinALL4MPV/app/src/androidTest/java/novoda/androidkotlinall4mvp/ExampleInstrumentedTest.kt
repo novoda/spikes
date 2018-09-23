c5ebaa32b63b32d2bfa9e866f0197501ad6a7d9e
Ferran Garriga <zegnus@gmail.com>
2018-09-23 11:44:51 +0100

    first commit

---
+++ AndroidKotlinALL4MPV/app/src/androidTest/java/novoda/androidkotlinall4mvp/ExampleInstrumentedTest.kt
@@ -0,0 +1,22 @@
+package novoda.androidkotlinall4mvp
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("novoda.androidkotlinall4mvp", appContext.packageName)
+    }
+}
