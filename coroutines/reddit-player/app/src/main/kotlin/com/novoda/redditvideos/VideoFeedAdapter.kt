0511e2835be800170e672ea280b6f1ba34767c3c
Pablo Gonzalez Alonso <pabs87@gmail.com>
2018-12-04 00:16:15 +0000

    Remove unecesary  lazy

--- coroutines/reddit-player/app/src/main/kotlin/com/novoda/redditvideos/VideoFeedAdapter.kt
+++ coroutines/reddit-player/app/src/main/kotlin/com/novoda/redditvideos/VideoFeedAdapter.kt
@@ -1,16 +1,17 @@
 package com.novoda.redditvideos
 
-import androidx.recyclerview.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.get
+import androidx.recyclerview.widget.RecyclerView
+import com.novoda.redditvideos.VideoFeedState.*
 import com.novoda.redditvideos.model.Video
-import com.novoda.redditvideos.support.view.revealDelayed
 import com.novoda.redditvideos.support.view.inflateDetached
 import com.novoda.redditvideos.support.view.load
+import com.novoda.redditvideos.support.view.revealDelayed
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_video_entry.*
 import kotlinx.android.synthetic.main.state_failure.*
@@ -18,16 +19,16 @@ import kotlinx.android.synthetic.main.state_loading.*
 
 internal class VideoFeedAdapter : RecyclerView.Adapter<VideoFeedViewHolder>() {
 
-    var state: VideoFeedState = VideoFeedState.Loading
+    var state: VideoFeedState = Loading
         set(value) {
             field = value
             notifyDataSetChanged()
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, position: Int) = when (state) {
-        is VideoFeedState.Loading -> LoadingViewHolder(parent)
-        is VideoFeedState.Idle, is VideoFeedState.LoadingWithCache -> VideoItemViewHolder(parent)
-        is VideoFeedState.Failure -> FailureViewHolder(parent)
+        is Loading -> LoadingViewHolder(parent)
+        is Idle, is VideoFeedState.LoadingWithCache -> VideoItemViewHolder(parent)
+        is Failure -> FailureViewHolder(parent)
     }
 
     override fun getItemCount() = (state as? VideoFeedState.HasVideos)?.videos?.size ?: 1
@@ -35,13 +36,13 @@ internal class VideoFeedAdapter : RecyclerView.Adapter<VideoFeedViewHolder>() {
     override fun onBindViewHolder(viewHolder: VideoFeedViewHolder, position: Int) = when (viewHolder) {
         is VideoItemViewHolder -> viewHolder.bind((state as VideoFeedState.HasVideos).videos[position])
         is LoadingViewHolder -> viewHolder.bind()
-        is FailureViewHolder -> viewHolder.bind(state as VideoFeedState.Failure)
+        is FailureViewHolder -> viewHolder.bind(state as Failure)
     }
 
-    override fun getItemViewType(position: Int) = when(state) {
-        is VideoFeedState.Loading -> 0
-        is VideoFeedState.Idle, is VideoFeedState.LoadingWithCache -> 1
-        is VideoFeedState.Failure -> 2
+    override fun getItemViewType(position: Int) = when (state) {
+        is Loading -> 0
+        is Idle, is VideoFeedState.LoadingWithCache -> 1
+        is Failure -> 2
     }
 
 }
@@ -56,14 +57,14 @@ private class LoadingViewHolder(parent: ViewGroup) : VideoFeedViewHolder(parent,
 private class VideoItemViewHolder(parent: ViewGroup) : VideoFeedViewHolder(parent, R.layout.item_video_entry)
 private class FailureViewHolder(parent: ViewGroup) : VideoFeedViewHolder(parent, R.layout.state_failure)
 
+
 private fun VideoItemViewHolder.bind(video: Video) {
     title.text = video.title
     thumbnail.load(video.thumbnail)
 }
 
-private fun FailureViewHolder.bind(failure: VideoFeedState.Failure) {
+private fun FailureViewHolder.bind(failure: Failure) {
     errorMessage.text = failure.message
-    val viewModel by lazy { ViewModelProviders.of(itemView.context as FragmentActivity).get<VideoFeedViewModel>() }
     retryButton.setOnClickListener {
         viewModel.reload()
     }
@@ -72,3 +73,6 @@ private fun FailureViewHolder.bind(failure: VideoFeedState.Failure) {
 private fun LoadingViewHolder.bind() {
     progressBar.revealDelayed()
 }
+
+private val VideoFeedViewHolder.viewModel
+    get() = ViewModelProviders.of(itemView.context as FragmentActivity).get<VideoFeedViewModel>()
