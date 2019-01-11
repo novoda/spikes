dbe4a37a1abd7b5b11a449c7295275ff7314490d
Tobias Heine <tobias@novoda.com>
2019-01-11 14:23:49 +0100

    Inject authenticated http client into GalleryBackend

---
+++ multiplatform-movies/common/src/commonMain/kotlin/com.novoda.movies.gallery/HttpClientProvider.kt
@@ -0,0 +1,27 @@
+package com.novoda.movies.gallery
+
+import io.ktor.client.HttpClient
+import io.ktor.client.HttpClientConfig
+import io.ktor.client.features.defaultRequest
+import io.ktor.client.request.parameter
+
+private const val KEY_API_SECRET: String = "api_key"
+private const val API_SECRET: String = "your themoviedb api secret" //https://developers.themoviedb.org/3
+private const val KEY_AUTH_INTERCEPTOR = "auth_interceptor"
+
+class HttpClientProvider {
+
+    fun provideAuthenticatedClient() = HttpClient {
+        authenticatedRequestsEngine()
+    }
+
+    private fun HttpClientConfig<*>.authenticatedRequestsEngine() {
+        engine {
+            install(KEY_AUTH_INTERCEPTOR) {
+                defaultRequest {
+                    parameter(KEY_API_SECRET, API_SECRET)
+                }
+            }
+        }
+    }
+}
--- multiplatform-movies/common/src/commonMain/kotlin/com.novoda.movies.gallery/KtorGalleryBackend.kt
+++ multiplatform-movies/common/src/commonMain/kotlin/com.novoda.movies.gallery/KtorGalleryBackend.kt
@@ -4,16 +4,13 @@ import io.ktor.client.HttpClient
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.features.json.serializer.KotlinxSerializer
 import io.ktor.client.request.get
-import io.ktor.client.request.parameter
 import io.ktor.client.request.url
 
 private const val BASE_URL: String = "https://api.themoviedb.org"
-private const val KEY_API_SECRET: String = "api_key"
-private const val API_SECRET: String = "your themoviedb api secret" //https://developers.themoviedb.org/3
 
-class KtorGalleryBackend : GalleryBackend {
+class KtorGalleryBackend(authenticatedClient: HttpClient) : GalleryBackend {
 
-    private val client = HttpClient {
+    private val galleryClient: HttpClient = authenticatedClient.config {
         install(JsonFeature) {
             serializer = KotlinxSerializer().apply {
                 setMapper(ApiMoviePoster::class, ApiMoviePoster.serializer())
@@ -22,10 +19,8 @@ class KtorGalleryBackend : GalleryBackend {
     }
 
     override suspend fun popularMoviesGallery(): ApiGallery {
-        return client.get {
-            url("$BASE_URL/3/movie/popular") {
-                parameter(KEY_API_SECRET, API_SECRET)
-            }
+        return galleryClient.get {
+            url("$BASE_URL/3/movie/popular")
         }
     }
 }
