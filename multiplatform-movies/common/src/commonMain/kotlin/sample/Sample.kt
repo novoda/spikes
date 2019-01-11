2a4a23008c6ff78da498cec704534e147e7493f1
Tobias Heine <tobias@novoda.com>
2019-01-11 17:03:24 +0100

    Remove unused code

--- multiplatform-movies/common/src/commonMain/kotlin/sample/Sample.kt
+++ multiplatform-movies/common/src/commonMain/kotlin/sample/Sample.kt
@@ -1,11 +1,6 @@
 package sample
 
-expect class Sample() {
-    fun checkMe(): Int
-}
-
 expect object Platform {
     val name: String
 }
 
-fun hello(): String = "Hello from ${Platform.name}"
--- multiplatform-movies/common/src/jvmMain/kotlin/sample/SampleJvm.kt
+++ multiplatform-movies/common/src/jvmMain/kotlin/sample/SampleJvm.kt
@@ -1,9 +1,5 @@
 package sample
 
-actual class Sample {
-    actual fun checkMe() = 42
-}
-
 actual object Platform {
     actual val name: String = "JVM"
 }
