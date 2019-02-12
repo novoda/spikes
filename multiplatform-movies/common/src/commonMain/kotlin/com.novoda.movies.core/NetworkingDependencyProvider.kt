7b4378525bf33b1432ebf35a0db4e828cca1f5c8
Tobias Heine <tobias@novoda.com>
2019-02-12 15:19:02 +0100

    Consume api key from BuildKonfig

--- multiplatform-movies/common/src/commonMain/kotlin/com.novoda.movies.core/NetworkingDependencyProvider.kt
+++ multiplatform-movies/common/src/commonMain/kotlin/com.novoda.movies.core/NetworkingDependencyProvider.kt
@@ -1,12 +1,12 @@
 package com.novoda.movies.core
 
+import com.novoda.movies.BuildKonfig
 import io.ktor.client.HttpClient
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.features.defaultRequest
 import io.ktor.client.request.parameter
 
 private const val KEY_API_SECRET: String = "api_key"
-private const val API_SECRET: String = "your themoviedb api secret" //https://developers.themoviedb.org/3
 private const val KEY_AUTH_INTERCEPTOR = "auth_interceptor"
 
 class NetworkingDependencyProvider {
@@ -19,7 +19,7 @@ class NetworkingDependencyProvider {
         engine {
             this@authenticatedRequestsEngine.install(KEY_AUTH_INTERCEPTOR) {
                 this@authenticatedRequestsEngine.defaultRequest {
-                    parameter(KEY_API_SECRET, API_SECRET)
+                    parameter(KEY_API_SECRET, BuildKonfig.API_KEY)
                 }
             }
         }
