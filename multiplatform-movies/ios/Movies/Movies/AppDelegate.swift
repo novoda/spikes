f0ef7904e4e8ffd223a585a4f5d8fbdbd33ad8a0
Tobias Heine <tobias@novoda.com>
2019-04-02 17:57:47 +0200

    Remove story board

--- multiplatform-movies/ios/Movies/Movies/AppDelegate.swift
+++ multiplatform-movies/ios/Movies/Movies/AppDelegate.swift
@@ -13,9 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let galleryViewController = GalleryViewController()
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = galleryViewController
+        self.window?.makeKeyAndVisible()        
         return true
     }
 
--- multiplatform-movies/ios/Movies/Movies/Gallery/GalleryViewController.swift
+++ multiplatform-movies/ios/Movies/Movies/Gallery/GalleryViewController.swift
@@ -9,30 +9,46 @@
 import UIKit
 import main
 
-class GalleryViewController: UIViewController, GalleryPresenterView {
+class GalleryViewController: UICollectionViewController, GalleryPresenterView {
     
     private let presenter = GalleryDependencyProvider(networkingDependencyProvider: NetworkingDependencyProvider()).providerPresenter()
-    private let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 21))
 
+    private var movies : [MoviePoster] = []
+    
+    override func loadView() {
+        let layout = UICollectionViewFlowLayout()
+        view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        super.loadView()        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.center = CGPoint(x: 160, y: 285)
-        label.textAlignment = .center
-        label.font = label.font.withSize(25)
-        view.addSubview(label)
         presenter.startPresenting(view: self)
     }
     
     func render(gallery: Gallery) {
-        label.text = "Gallery with \(gallery.moviePosters.count) posters"
+        movies = gallery.moviePosters
     }
     
     func renderError(message: String?) {
-        guard let errorMessage = message else {
-            label.text = "Something went wrong"
-            return 
-        }
-        label.text = errorMessage
+//        guard let errorMessage = message else {
+//            label.text = "Something went wrong"
+//            return
+//        }
+//        label.text = errorMessage
     }
 }
 
+extension GalleryViewController {
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return movies.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "movie_poster", for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+}
