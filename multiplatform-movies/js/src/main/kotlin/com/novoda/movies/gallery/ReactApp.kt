9348b4a1e3ff1075845fd6df78a0dca1c5a486df
Florian Mierzejewski <florianmski@novoda.com>
2019-04-15 16:12:31 +0200

    extract top app bar in its own component

--- multiplatform-movies/js/src/main/kotlin/com/novoda/movies/gallery/ReactApp.kt
+++ multiplatform-movies/js/src/main/kotlin/com/novoda/movies/gallery/ReactApp.kt
@@ -39,13 +39,7 @@ private class App : RComponent<RProps, AppState>(), GalleryPresenter.View {
     }
 
     override fun RBuilder.render() {
-        header(classes = "mdc-top-app-bar mdc-top-app-bar--fixed") {
-            div(classes = "mdc-top-app-bar__row") {
-                section(classes = "mdc-top-app-bar__section mdc-top-app-bar__section--align-start") {
-                    span(classes = "mdc-top-app-bar__title") { +"Movies" }
-                }
-            }
-        }
+        topAppBar("Movies")
         state.gallery?.let {
             styledDiv {
                 css {
