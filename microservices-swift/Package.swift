1279e062459e55cbdcc2fca40780ade32cc612ee
Xavi Rigau <xavi@novoda.com>
2019-10-09 16:59:09 +0200

    Attempt to use Docker compose for having a couchdb service

--- microservices-swift/Package.swift
+++ microservices-swift/Package.swift
@@ -9,11 +9,11 @@ let package = Package(
       .package(url: "https://github.com/IBM-Swift/CloudEnvironment.git", from: "9.0.0"),
       .package(url: "https://github.com/RuntimeTools/SwiftMetrics.git", from: "2.0.0"),
       .package(url: "https://github.com/IBM-Swift/Health.git", from: "1.0.0"),
+      .package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", from: "3.2.0"),
     ],
     targets: [
       .target(name: "microservices-swift", dependencies: [ .target(name: "Application"), "Kitura" , "HeliumLogger"]),
-      .target(name: "Application", dependencies: [ "Kitura", "CloudEnvironment","SwiftMetrics", "Health", 
-
+      .target(name: "Application", dependencies: [ "Kitura", "CloudEnvironment","SwiftMetrics", "Health", "CouchDB"
       ]),
 
       .testTarget(name: "ApplicationTests" , dependencies: [.target(name: "Application"), "Kitura","HeliumLogger" ])
